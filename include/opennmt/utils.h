7f8d8e7ab6c538a2394eff51c16076e091811143
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-06-14 11:30:48 +0200

    utils.cc

--- include/opennmt/utils.h
+++ include/opennmt/utils.h
@@ -1,21 +1,9 @@
 #pragma once
 
 #include <cstddef>
-#include <cstdint>
 
-#ifdef INTEL_MKL
-#  include <mkl.h>
-#endif
+namespace opennmt {
 
-namespace opennmt
-{
-
-  inline void init(size_t num_threads) {
-#ifdef WITH_MKL
-    vmlSetMode(VML_EP);
-    if (num_threads > 0)
-      mkl_set_num_threads(num_threads);
-#endif
-  }
+  void init(size_t num_threads = 0);
 
 }
