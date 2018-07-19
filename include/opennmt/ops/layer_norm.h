8d19d77736c17cda75fc9ccfdeb7e02ae75c5ad9
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-07-19 12:00:55 +0200

    Remove unnecessary copies

--- include/opennmt/ops/layer_norm.h
+++ include/opennmt/ops/layer_norm.h
@@ -22,16 +22,16 @@ namespace opennmt {
                    const StorageView& gamma,
                    const StorageView& input,
                    StorageView& output) const {
+        static thread_local StorageView tmp(input.dtype());
         size_t depth = input.dim(-1);
         size_t batch_size = input.size() / depth;
-        StorageView tmp({depth}, input.dtype());
+        tmp.resize({depth});
         output.resize_as(input);
         for (size_t i = 0; i < batch_size; ++i) {
           const auto* x = input.data<T>() + i * depth;
           auto* y = output.data<T>() + i * depth;
           auto mean = primitives::mean(x, depth);
-          primitives::copy(x, y, depth);
-          primitives::sub(mean, y, depth);
+          primitives::sub(mean, x, y, depth);
           primitives::pow(y, tmp.data<T>(), static_cast<T>(2), depth);
           auto variance = primitives::mean(tmp.data<T>(), depth);
           primitives::mul(static_cast<T>(1.0 / sqrt(variance + EPSILON)), y, depth);
--- include/opennmt/ops/softmax.h
+++ include/opennmt/ops/softmax.h
@@ -16,12 +16,12 @@ namespace opennmt {
       void compute(const StorageView& input, StorageView& output) const {
         size_t depth = input.dim(-1);
         size_t batch_size = input.size() / depth;
-        output = input;
+        output.resize_as(input);
         for (size_t i = 0; i < batch_size; ++i) {
           const auto* x = input.data<T>() + (i * depth);
           auto* y = output.data<T>() + (i * depth);
           auto max = primitives::max(x, depth);
-          primitives::sub(max, y, depth);
+          primitives::sub(max, x, y, depth);
           primitives::exp(y, y, depth);
           auto sum = primitives::sum(y, depth);
           primitives::mul(1.f / (sum + EPSILON), y, depth);
