7565d9485d32ffc9bdc36e0f489fd0583b30b00f
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-07-26 16:05:52 +0200

    Use out of place primitives for scalar operations

--- include/ctranslate2/ops/add.h
+++ include/ctranslate2/ops/add.h
@@ -14,11 +14,10 @@ namespace ctranslate2 {
     private:
       template <Device D, typename T>
       void compute(const StorageView& a, const StorageView& b, StorageView& c) const {
+        c.resize_as(a);
         if (b.is_scalar()) {
-          c = a;
-          primitives<D>::add(b.data<T>()[0], c.data<T>(), c.size());
+          primitives<D>::add(b.data<T>()[0], a.data<T>(), c.data<T>(), c.size());
         } else {
-          c.resize_as(a);
           primitives<D>::add(a.data<T>(), b.data<T>(), c.data<T>(), c.size());
         }
       }
