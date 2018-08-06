bcc3c518330069a383db7c9e6e747af121770d5c
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-08-06 18:06:49 +0200

    Merge Concat and Split CUDA impl

---
+++ src/ops/concat_split_gpu.cu
@@ -0,0 +1,132 @@
+#include "ctranslate2/ops/concat.h"
+#include "ctranslate2/ops/split.h"
+
+#include <thrust/gather.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/iterator/transform_iterator.h>
+#include <thrust/scatter.h>
+
+#include "ctranslate2/cuda/utils.h"
+
+namespace ctranslate2 {
+  namespace ops {
+
+    template <typename T>
+    struct depth_select : public thrust::unary_function<T, T> {
+      T _offset;
+      T _depth;
+      T _total_depth;
+      depth_select(size_t offset, size_t depth, size_t total_depth)
+        : _offset(offset)
+        , _depth(depth)
+        , _total_depth(total_depth) {
+      }
+      __host__ __device__
+      T operator()(const T& i) const {
+        T row = i / _depth;
+        T col = i % _depth;
+        return row * _total_depth + col + _offset;
+      }
+    };
+
+    template <typename T>
+    struct inner_dim_select : public thrust::unary_function<T, T> {
+      T _offset;
+      T _inner_dim;
+      T _outer_dim;
+      T _total_inner_dim;
+      inner_dim_select(size_t offset,
+                       size_t inner_dim,
+                       size_t outer_dim,
+                       size_t total_inner_dim)
+        : _offset(offset)
+        , _inner_dim(inner_dim)
+        , _outer_dim(outer_dim)
+        , _total_inner_dim(total_inner_dim) {
+      }
+      __host__ __device__
+      T operator()(const T& i) const {
+        T i0 = i / (_inner_dim * _outer_dim);
+        T i1 = (i / _outer_dim) % _inner_dim;
+        T i2 = i % _outer_dim;
+        return i0 * (_total_inner_dim * _outer_dim) + (i1 + _offset) * _outer_dim + i2;
+      }
+    };
+
+    template <Device D, typename T>
+    void Concat::compute(const std::vector<StorageView*>& inputs,
+                         StorageView& output) const {
+      size_t axis = _axis < 0 ? output.rank() + _axis : _axis;
+      size_t offset = 0;
+      for (const auto& x : inputs) {
+        if (axis == 0) {
+          primitives<D>::copy(x->data<T>(), output.data<T>() + offset, x->size());
+          offset += x->size();
+        } else if (axis == output.rank() - 1) {
+          auto map_ids = thrust::make_transform_iterator(
+            thrust::counting_iterator<size_t>(0),
+            depth_select<int32_t>(offset, x->dim(-1), output.dim(-1)));
+          thrust::scatter(
+            thrust::cuda::par.on(cuda::get_cuda_stream()),
+            x->data<T>(), x->data<T>() + x->size(), map_ids, output.data<T>());
+          offset += x->dim(-1);
+        } else {
+          size_t outer_dim = 1;
+          for (size_t i = axis + 1; i < output.rank(); ++i)
+            outer_dim *= output.dim(i);
+          auto map_ids = thrust::make_transform_iterator(
+            thrust::counting_iterator<size_t>(0),
+            inner_dim_select<int32_t>(offset, x->dim(axis), outer_dim, output.dim(axis)));
+          thrust::scatter(
+            thrust::cuda::par.on(cuda::get_cuda_stream()),
+            x->data<T>(), x->data<T>() + x->size(), map_ids, output.data<T>());
+          offset += x->dim(axis);
+        }
+      }
+    }
+
+    template <Device D, typename T>
+    void Split::compute(const StorageView& input,
+                        std::vector<StorageView*>& outputs) const {
+      size_t axis = _axis < 0 ? input.rank() + _axis : _axis;
+      size_t offset = 0;
+      for (auto* output : outputs) {
+        auto& x = *output;
+        if (axis == 0) { // First outer dim.
+          primitives<D>::copy(input.data<T>() + offset, x.data<T>(), x.size());
+          offset += x.size();
+        } else if (axis == input.rank() - 1) { // Last outer dim.
+          auto gather_ids = thrust::make_transform_iterator(
+            thrust::counting_iterator<size_t>(0),
+            depth_select<int32_t>(offset, x.dim(-1), input.dim(-1)));
+          thrust::gather(
+            thrust::cuda::par.on(cuda::get_cuda_stream()),
+            gather_ids, gather_ids + x.size(), input.data<T>(), x.data<T>());
+          offset += x.dim(-1);
+        } else { // Inner dim.
+          size_t outer_dim = 1;
+          for (size_t i = axis + 1; i < input.rank(); ++i)
+            outer_dim *= input.dim(i);
+          auto gather_ids = thrust::make_transform_iterator(
+            thrust::counting_iterator<size_t>(0),
+            inner_dim_select<int32_t>(offset, x.dim(axis), outer_dim, input.dim(axis)));
+          thrust::gather(
+            thrust::cuda::par.on(cuda::get_cuda_stream()),
+            gather_ids, gather_ids + x.size(), input.data<T>(), x.data<T>());
+          offset += x.dim(axis);
+        }
+      }
+    }
+
+#define DECLARE_IMPL(T)                                                 \
+    template void                                                       \
+    Concat::compute<Device::CUDA, T>(const std::vector<StorageView*>& inputs, \
+                                     StorageView& output) const;        \
+    template void                                                       \
+    Split::compute<Device::CUDA, T>(const StorageView& input,           \
+                                    std::vector<StorageView*>& outputs) const;
+
+    DECLARE_ALL_TYPES(DECLARE_IMPL)
+
+  }
+}
