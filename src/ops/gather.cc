fc4a9764bb8406807a77925212c6c37940ee2fe1
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-08-13 18:16:05 +0200

    Fuse copy operation for consecutive gather ids

--- src/ops/gather.cc
+++ src/ops/gather.cc
@@ -7,12 +7,15 @@ namespace ctranslate2 {
     void Gather::compute(const StorageView& data,
                          const StorageView& input,
                          StorageView& output) const {
+      const auto* indices = input.data<int32_t>();
       size_t copy_dim = data.stride(0);
-      for (size_t i = 0; i < input.size(); ++i) {
-        size_t index = input.data<int32_t>()[i];
+      for (size_t i = 0, merge = 1; i < input.size(); i += merge, merge = 1) {
+        size_t index = indices[i];
+        while (i + merge < input.size() && static_cast<size_t>(indices[i + merge]) == index + merge)
+          ++merge;
         const auto* src = data.index<T>({index});
         auto* dst = output.data<T>() + i * copy_dim;
-        primitives<Device::CPU>::copy(src, dst, copy_dim);
+        primitives<Device::CPU>::copy(src, dst, copy_dim * merge);
       }
     }
 
