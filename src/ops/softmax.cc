4fda0d64f9312bfe8e0cc6f2c7b27e6d6347dbc1
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-08-13 10:44:00 +0200

    Factorize CPU softmax ops

--- src/ops/softmax.cc
+++ src/ops/softmax.cc
@@ -6,8 +6,8 @@
 namespace ctranslate2 {
   namespace ops {
 
-    template <Device D, typename T>
-    void SoftMax::compute(const StorageView& input, StorageView& output) const {
+    template <typename T>
+    static void softmax(const StorageView& input, StorageView& output, bool log) {
       size_t depth = input.dim(-1);
       size_t batch_size = input.size() / depth;
       for (size_t i = 0; i < batch_size; ++i) {
@@ -17,24 +17,21 @@ namespace ctranslate2 {
         primitives<>::sub(max, x, y, depth);
         primitives<>::exp(y, y, depth);
         auto sum = primitives<>::sum(y, depth);
-        primitives<>::mul(1.f / (sum + EPSILON), y, depth);
+        if (log)
+          primitives<>::sub(std::log(sum) + max, x, y, depth);
+        else
+          primitives<>::mul(1.f / (sum + EPSILON), y, depth);
       }
     }
 
     template <Device D, typename T>
+    void SoftMax::compute(const StorageView& input, StorageView& output) const {
+      softmax<T>(input, output, false /* log */);
+    }
+
+    template <Device D, typename T>
     void LogSoftMax::compute(const StorageView& input, StorageView& output) const {
-      size_t depth = input.dim(-1);
-      size_t batch_size = input.size() / depth;
-      for (size_t i = 0; i < batch_size; ++i) {
-        const auto* x = input.data<T>() + (i * depth);
-        auto* y = output.data<T>() + (i * depth);
-        auto max = primitives<>::max(x, depth);
-        primitives<>::sub(max, x, y, depth);
-        primitives<>::exp(y, y, depth);
-        auto sum = primitives<>::sum(y, depth);
-        auto log_z = std::log(sum) + max;
-        primitives<>::sub(log_z, x, y, depth);
-      }
+      softmax<T>(input, output, true /* log */);
     }
 
 #define DECLARE_IMPL(T)                                                 \
