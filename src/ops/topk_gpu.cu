24f60836d906572c077bac4a8eca5262bbf02576
Guillaume Klein <guillaume.klein@systrangroup.com>
2018-09-20 13:54:25 +0200

    Invalidate TopK layer when the depth changes

--- src/ops/topk_gpu.cu
+++ src/ops/topk_gpu.cu
@@ -1,6 +1,7 @@
 #include "ctranslate2/ops/topk.h"
 
 #include <iostream>
+#include <memory>
 
 #include <NvInfer.h>
 
@@ -19,12 +20,59 @@ namespace ctranslate2 {
       }
     } g_logger;
 
+    class TopKLayer {
+    public:
+      TopKLayer(IBuilder* builder, int k, int depth)
+        : _depth(depth) {
+        _network = builder->createNetwork();
+        ITensor* data = _network->addInput("x",
+                                           nvinfer1::DataType::kFLOAT,
+                                           Dims{1, {depth}, {DimensionType::kCHANNEL}});
+        ITopKLayer* topk = _network->addTopK(*data, TopKOperation::kMAX, k, 1);
+        ITensor* values_t = topk->getOutput(0);
+        ITensor* indices_t = topk->getOutput(1);
+        _network->markOutput(*values_t);
+        _network->markOutput(*indices_t);
+        values_t->setName("values");
+        indices_t->setName("indices");
+        indices_t->setType(nvinfer1::DataType::kINT32);
+        _engine = builder->buildCudaEngine(*_network);
+        _execution_context = _engine->createExecutionContext();
+      }
+
+      ~TopKLayer() {
+        _network->destroy();
+        _engine->destroy();
+      }
+
+      IExecutionContext* get_execution_context() {
+        return _execution_context;
+      }
+
+      int depth() const {
+        return _depth;
+      }
+
+    private:
+      int _depth;
+      INetworkDefinition* _network;
+      ICudaEngine* _engine;
+      IExecutionContext* _execution_context;
+    };
+
     static int max_batch_size = 512;
 
     template <Device D, typename DataType, typename IndexType>
     void TopK::compute(const StorageView& x,
                        StorageView& values,
                        StorageView& indices) const {
+      static thread_local IBuilder* builder = nullptr;
+      if (!builder) {
+        builder = createInferBuilder(g_logger);
+        builder->setMaxBatchSize(max_batch_size);
+        builder->setMaxWorkspaceSize(1 << 30);
+      }
+
       int depth = x.dim(-1);
       int batch_size = x.size() / depth;
 
@@ -33,26 +81,9 @@ namespace ctranslate2 {
                                  + std::to_string(max_batch_size) + ", but got "
                                  + std::to_string(batch_size));
 
-      static thread_local IExecutionContext* context = nullptr;
-      if (context == nullptr) {
-        IBuilder* builder = createInferBuilder(g_logger);
-        builder->setMaxBatchSize(max_batch_size);
-        builder->setMaxWorkspaceSize(1 << 30);
-        INetworkDefinition* network = builder->createNetwork();
-        ITensor* data = network->addInput("x",
-                                          nvinfer1::DataType::kFLOAT,
-                                          Dims{1, {depth}, {DimensionType::kCHANNEL}});
-        ITopKLayer* topk = network->addTopK(*data, TopKOperation::kMAX, _k, 1);
-        ITensor* values_t = topk->getOutput(0);
-        ITensor* indices_t = topk->getOutput(1);
-        network->markOutput(*values_t);
-        network->markOutput(*indices_t);
-        values_t->setName("values");
-        indices_t->setName("indices");
-        indices_t->setType(nvinfer1::DataType::kINT32);
-        ICudaEngine* engine = builder->buildCudaEngine(*network);
-        context = engine->createExecutionContext();
-      }
+      static thread_local std::unique_ptr<TopKLayer> topk_layer;
+      if (!topk_layer || topk_layer->depth() != depth)
+        topk_layer.reset(new TopKLayer(builder, _k, depth));
 
       void* bindings[3] = {
         const_cast<DataType*>(x.data<DataType>()),
@@ -60,7 +91,10 @@ namespace ctranslate2 {
         indices.data<IndexType>()
       };
 
-      context->enqueue(batch_size, bindings, cuda::get_cuda_stream(), NULL);
+      topk_layer->get_execution_context()->enqueue(batch_size,
+                                                   bindings,
+                                                   cuda::get_cuda_stream(),
+                                                   NULL);
     }
 
 #define DECLARE_IMPL(T)                                                 \
