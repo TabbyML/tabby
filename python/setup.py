dbc2086f91484fabcae9dacc9bdd40443b245813
Guillaume Klein <guillaume.klein@systrangroup.com>
2019-09-25 16:14:12 +0200

    Install project to predefined destinations

--- python/setup.py
+++ python/setup.py
@@ -10,7 +10,11 @@ def _maybe_add_library_root(lib_name):
   if "%s_ROOT" % lib_name in os.environ:
     root = os.environ["%s_ROOT" % lib_name]
     include_dirs.append("%s/include" % root)
-    library_dirs.append("%s/lib" % root)
+    for lib_dir in ("lib", "lib64"):
+      path = "%s/%s" % (root, lib_dir)
+      if os.path.exists(path):
+        library_dirs.append(path)
+        break
 
 _maybe_add_library_root("BOOST")
 _maybe_add_library_root("CTRANSLATE2")
