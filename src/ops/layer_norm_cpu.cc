715ef1cc087ca6cf6b44ae9e1015e8aa3c920cdd
Guillaume Klein <guillaumekln@users.noreply.github.com>
2019-12-06 15:47:43 +0100

    Optimize LayerNorm on CPU (#51)
    * Optimize LayerNorm on CPU
    
    * Add kernel function
    
    * Pass epsilon as typed argument

--- src/ops/layer_norm_cpu.cc
+++ src/ops/layer_norm_cpu.cc
@@ -2,32 +2,53 @@
 
 #include <cmath>
 
-#define EPSILON 0.000001f
+#define EPSILON 1e-5
 
 namespace ctranslate2 {
   namespace ops {
+
+    template <typename T>
+    static void layer_norm_kernel(int64_t m,
+                                  int64_t n,
+                                  T eps,
+                                  const T* input,
+                                  const T* gamma,
+                                  const T* beta,
+                                  T* output) {
+      #pragma omp parallel for
+      for (int64_t i = 0; i < m; ++i) {
+        const auto offset = i * n;
+        const T* x = input + offset;
+        T* y = output + offset;
+        T mean = 0;  // sum(x)/n
+        T rstd = 0;  // 1/sqrt(var(x)) where var(x) = sum((x-mean)^2)/n = sum(x^2)/n - mean^2
+        for (int64_t j = 0; j < n; ++j) {
+          mean += x[j];
+          rstd += x[j] * x[j];
+        }
+        mean /= n;
+        rstd = std::max(rstd / n - mean * mean, static_cast<T>(0));
+        rstd = static_cast<T>(1) / std::sqrt(rstd + eps);
+        for (int64_t j = 0; j < n; ++j) {
+          y[j] = (x[j] - mean) * rstd * gamma[j] + beta[j];
+        }
+      }
+    }
 
     template <Device D, typename T>
     void LayerNorm::compute(const StorageView& beta,
                             const StorageView& gamma,
                             const StorageView& input,
                             StorageView& output) const {
-      size_t depth = input.dim(-1);
-      size_t batch_size = input.size() / depth;
-      StorageView tmp({batch_size, depth}, input.dtype(), input.device());
-      #pragma omp parallel for
-      for (long long i = 0; i < static_cast<long long>(batch_size); ++i) {
-        const auto* x = input.data<T>() + i * depth;
-        auto* y = output.data<T>() + i * depth;
-        auto* t = tmp.data<T>() + i * depth;
-        auto mean = primitives<>::mean(x, depth);
-        primitives<>::sub(mean, x, y, depth);
-        primitives<>::pow(y, t, static_cast<T>(2), depth);
-        auto variance = primitives<>::mean(t, depth);
-        primitives<>::mul(static_cast<T>(1.0 / sqrt(variance + EPSILON)), y, depth);
-        primitives<>::mul(gamma.data<T>(), y, depth);
-        primitives<>::add(beta.data<T>(), y, depth);
-      }
+      auto depth = input.dim(-1);
+      auto batch_size = input.size() / depth;
+      layer_norm_kernel(batch_size,
+                        depth,
+                        static_cast<T>(EPSILON),
+                        input.data<T>(),
+                        gamma.data<T>(),
+                        beta.data<T>(),
+                        output.data<T>());
     }
 
 #define DECLARE_IMPL(T)                                                 \
