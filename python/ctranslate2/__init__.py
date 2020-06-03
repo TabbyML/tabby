01e13e9debf15faf73a0dac09cbbbaf1624e316d
Guillaume Klein <guillaumekln@users.noreply.github.com>
2020-06-03 15:04:38 +0200

    Raise errors when importing the compiled translator extension (#196)

--- python/ctranslate2/__init__.py
+++ python/ctranslate2/__init__.py
@@ -1,8 +1,12 @@
 try:
     from ctranslate2.translator import Translator
     from ctranslate2.translator import contains_model
-except ImportError:
-    pass
+except ImportError as e:
+    # Allow using the Python package without the compiled translator extension.
+    if "No module named" in str(e):
+        pass
+    else:
+        raise
 
 from ctranslate2 import converters
 from ctranslate2 import specs
