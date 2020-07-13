7960d7b209b025cd6dfa990333cba1e224cf5ccf
Guillaume Klein <guillaumekln@users.noreply.github.com>
2020-07-13 13:43:06 +0200

    Fix intrinsics include in half.hpp (#238)

--- include/ctranslate2/half_float/half.hpp
+++ include/ctranslate2/half_float/half.hpp
@@ -266,9 +266,6 @@
 #if HALF_ENABLE_CPP11_HASH
 	#include <functional>
 #endif
-#if HALF_ENABLE_F16C_INTRINSICS
-	#include <immintrin.h>
-#endif
 
 
 #ifndef HALF_ENABLE_F16C_INTRINSICS
@@ -279,6 +276,10 @@
 	///
 	/// Unless predefined it will be enabled automatically when the `__F16C__` symbol is defined, which some compilers do on supporting platforms.
 	#define HALF_ENABLE_F16C_INTRINSICS __F16C__
+#endif
+
+#if HALF_ENABLE_F16C_INTRINSICS
+	#include <immintrin.h>
 #endif
 
 #ifdef HALF_DOXYGEN_ONLY
