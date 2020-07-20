115a6bd5c0ab1f0d0a10faf8ce7a2f24093869a7
Guillaume Klein <guillaume.klein@systrangroup.com>
2020-07-20 15:14:15 +0200

    Build CentOS 7 images first

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -42,14 +42,14 @@ build()
     fi
 }
 
-build Dockerfile.ubuntu ubuntu18
-build Dockerfile.ubuntu-gpu ubuntu18-gpu "--build-arg CUDA_VERSION=10.0"
-build Dockerfile.ubuntu-gpu ubuntu18-cuda10.0 "--build-arg CUDA_VERSION=10.0"
-build Dockerfile.ubuntu-gpu ubuntu18-cuda10.1 "--build-arg CUDA_VERSION=10.1"
-build Dockerfile.ubuntu-gpu ubuntu18-cuda10.2 "--build-arg CUDA_VERSION=10.2"
-
 build Dockerfile.centos7 centos7
 build Dockerfile.centos7-gpu centos7-gpu "--build-arg CUDA_VERSION=10.0"
 build Dockerfile.centos7-gpu centos7-cuda10.0 "--build-arg CUDA_VERSION=10.0"
 build Dockerfile.centos7-gpu centos7-cuda10.1 "--build-arg CUDA_VERSION=10.1"
 build Dockerfile.centos7-gpu centos7-cuda10.2 "--build-arg CUDA_VERSION=10.2"
+
+build Dockerfile.ubuntu ubuntu18
+build Dockerfile.ubuntu-gpu ubuntu18-gpu "--build-arg CUDA_VERSION=10.0"
+build Dockerfile.ubuntu-gpu ubuntu18-cuda10.0 "--build-arg CUDA_VERSION=10.0"
+build Dockerfile.ubuntu-gpu ubuntu18-cuda10.1 "--build-arg CUDA_VERSION=10.1"
+build Dockerfile.ubuntu-gpu ubuntu18-cuda10.2 "--build-arg CUDA_VERSION=10.2"
