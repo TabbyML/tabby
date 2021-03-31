7d652d6397048031a37df8272194dffbedcfea8f
Guillaume Klein <guillaumekln@users.noreply.github.com>
2021-03-31 10:06:12 +0200

    Update Intel oneAPI to 2021.2 in macOS builds (#435)

--- python/tools/prepare_build_environment_macos.sh
+++ python/tools/prepare_build_environment_macos.sh
@@ -5,12 +5,12 @@ set -x
 
 # Install OneAPI MKL and DNNL
 # See https://github.com/oneapi-src/oneapi-ci for installer URLs
-ONEAPI_INSTALLER_URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/17426/m_BaseKit_p_2021.1.0.2427_offline.dmg
+ONEAPI_INSTALLER_URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/17714/m_BaseKit_p_2021.2.0.2855_offline.dmg
 wget -q $ONEAPI_INSTALLER_URL
 hdiutil attach -noverify -noautofsck $(basename $ONEAPI_INSTALLER_URL)
 sudo /Volumes/$(basename $ONEAPI_INSTALLER_URL .dmg)/bootstrapper.app/Contents/MacOS/bootstrapper --silent --eula accept --components intel.oneapi.mac.mkl.devel:intel.oneapi.mac.dnnl
 # Fix install name and rpath of libdnnl so that delocate works correctly.
-sudo install_name_tool -id @rpath/libdnnl.2.dylib -add_rpath /opt/intel/oneapi/compiler/latest/mac/compiler/lib /opt/intel/oneapi/dnnl/latest/cpu_iomp/lib/libdnnl.2.0.dylib
+sudo install_name_tool -id @rpath/libdnnl.2.dylib -add_rpath /opt/intel/oneapi/compiler/latest/mac/compiler/lib /opt/intel/oneapi/dnnl/latest/cpu_iomp/lib/libdnnl.2.2.dylib
 
 # Install LLVM's libomp because Intel's OpenMP runtime included in MKL does
 # not ship with the header file.
