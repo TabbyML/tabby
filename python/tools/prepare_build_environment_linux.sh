3190be529d55d1e4d4ca246bd2a9cf405107bb6d
Guillaume Klein <guillaumekln@users.noreply.github.com>
2021-04-08 18:19:11 +0200

    Use yum-config-manager to register the CUDA repository (#443)

--- python/tools/prepare_build_environment_linux.sh
+++ python/tools/prepare_build_environment_linux.sh
@@ -3,15 +3,15 @@
 set -e
 set -x
 
+yum install -y yum-utils
+
 # Install CUDA 10.1, see:
 # * https://gitlab.com/nvidia/container-images/cuda/-/blob/master/dist/10.1/centos7-x86_64/base/Dockerfile
 # * https://gitlab.com/nvidia/container-images/cuda/-/blob/master/dist/10.1/centos7-x86_64/devel/Dockerfile
 CUDA_VERSION=10.1.243
 CUDA_PKG_VERSION=10-1-$CUDA_VERSION-1
 CUBLAS_PKG_VERSION=10.2.1.243-1
-curl -o cuda-repo.rpm https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-$CUDA_VERSION-1.x86_64.rpm
-rpm --install cuda-repo.rpm
-yum clean expire-cache
+yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
 yum install --setopt=obsoletes=0 -y cuda-nvcc-$CUDA_PKG_VERSION cuda-cudart-dev-$CUDA_PKG_VERSION libcublas-devel-$CUBLAS_PKG_VERSION libcublas10-$CUBLAS_PKG_VERSION
 ln -s cuda-10.1 /usr/local/cuda
 
@@ -22,7 +22,6 @@ source /opt/rh/devtoolset-8/enable
 ONEAPI_VERSION=2021.2.0
 MKL_BUILD=296
 DNNL_BUILD=228
-yum install -y yum-utils
 yum-config-manager --add-repo https://yum.repos.intel.com/oneapi
 rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 yum install -y intel-oneapi-mkl-devel-$ONEAPI_VERSION-$MKL_BUILD intel-oneapi-dnnl-devel-$ONEAPI_VERSION-$DNNL_BUILD
