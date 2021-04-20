9dd7c19d3c41647bc2e750d9d3a13d9e95988327
Guillaume Klein <guillaume.klein@systrangroup.com>
2021-04-20 11:26:35 +0200

    Remove Docker images suffixed with GPU

--- docker/build_all.sh
+++ docker/build_all.sh
@@ -43,14 +43,12 @@ build()
 }
 
 build Dockerfile.centos7 centos7
-build Dockerfile.centos7-gpu centos7-gpu "--build-arg CUDA_VERSION=10.0"
 build Dockerfile.centos7-gpu centos7-cuda10.0 "--build-arg CUDA_VERSION=10.0"
 build Dockerfile.centos7-gpu centos7-cuda10.1 "--build-arg CUDA_VERSION=10.1"
 build Dockerfile.centos7-gpu centos7-cuda10.2 "--build-arg CUDA_VERSION=10.2"
 build Dockerfile.centos7-gpu centos7-cuda11.0 "--build-arg CUDA_VERSION=11.0"
 
 build Dockerfile.ubuntu ubuntu18
-build Dockerfile.ubuntu-gpu ubuntu18-gpu "--build-arg CUDA_VERSION=10.0"
 build Dockerfile.ubuntu-gpu ubuntu18-cuda10.0 "--build-arg CUDA_VERSION=10.0"
 build Dockerfile.ubuntu-gpu ubuntu18-cuda10.1 "--build-arg CUDA_VERSION=10.1"
 build Dockerfile.ubuntu-gpu ubuntu18-cuda10.2 "--build-arg CUDA_VERSION=10.2"
