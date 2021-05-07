43dc9af366930f613a3610252fa53995e37b96ae
Guillaume Klein <guillaumekln@users.noreply.github.com>
2021-05-07 11:06:09 +0200

    Add an experimental CUDA asynchronous allocator (#472)
    * Add an experimental CUDA asynchronous allocator
    
    * Change the env variable
    
    * Minor format fix
    
    * Keep list sorted

--- src/cuda/allocator.cc
+++ src/cuda/allocator.cc
@@ -5,6 +5,7 @@
 #include "ctranslate2/utils.h"
 #include "cuda/utils.h"
 
+#include <cuda.h>
 #include <cub/util_allocator.cuh>
 
 namespace ctranslate2 {
@@ -56,13 +57,72 @@ namespace ctranslate2 {
       std::unique_ptr<cub::CachingDeviceAllocator> _allocator;
     };
 
+#if CUDA_VERSION >= 11020
+    class CudaAsyncAllocator : public Allocator {
+    public:
+      void* allocate(size_t size, int device_index) override {
+        int prev_device_index = -1;
+        if (device_index >= 0) {
+          CUDA_CHECK(cudaGetDevice(&prev_device_index));
+          CUDA_CHECK(cudaSetDevice(device_index));
+        }
+
+        void* ptr = nullptr;
+        CUDA_CHECK(cudaMallocAsync(&ptr, size, get_cuda_stream()));
+
+        if (prev_device_index >= 0) {
+          CUDA_CHECK(cudaSetDevice(prev_device_index));
+        }
+
+        return ptr;
+      }
+
+      void free(void* ptr, int device_index) override {
+        int prev_device_index = -1;
+        if (device_index >= 0) {
+          CUDA_CHECK(cudaGetDevice(&prev_device_index));
+          CUDA_CHECK(cudaSetDevice(device_index));
+        }
+
+        CUDA_CHECK(cudaFreeAsync(ptr, get_cuda_stream()));
+
+        if (prev_device_index >= 0) {
+          CUDA_CHECK(cudaSetDevice(prev_device_index));
+        }
+      }
+    };
+#endif
+
+    static std::unique_ptr<Allocator> create_allocator() {
+      const auto allocator = read_string_from_env("CT2_CUDA_ALLOCATOR", "cub_caching");
+
+      if (allocator == "cub_caching") {
+        return std::make_unique<CubCachingAllocator>();
+      } else if (allocator == "cuda_malloc_async") {
+#if CUDA_VERSION < 11020
+        throw std::runtime_error("The asynchronous CUDA allocator requires CUDA >= 11.2");
+#else
+        for (int i = 0; i < get_gpu_count(); ++i) {
+          int supported = 0;
+          CUDA_CHECK(cudaDeviceGetAttribute(&supported, cudaDevAttrMemoryPoolsSupported, i));
+          if (!supported)
+            throw std::runtime_error("Asynchronous allocation is not supported by the current GPU");
+        }
+
+        return std::make_unique<CudaAsyncAllocator>();
+#endif
+      }
+
+      throw std::invalid_argument("Invalid CUDA allocator " + allocator);
+    }
+
   }
 
   template<>
   Allocator& get_allocator<Device::CUDA>() {
     // Use 1 allocator per thread for performance.
-    static thread_local cuda::CubCachingAllocator allocator;
-    return allocator;
+    static thread_local std::unique_ptr<Allocator> allocator(cuda::create_allocator());
+    return *allocator;
   }
 
 }
