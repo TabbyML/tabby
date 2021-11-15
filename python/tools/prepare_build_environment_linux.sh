c7c95e3ada04386d4a9143f6e8f3535a7bd71839
Guillaume Klein <guillaumekln@users.noreply.github.com>
2021-11-15 10:06:59 +0100

    Use Intel OpenMP in the Linux wheel for consistency with other binaries (#621)
    * Use Intel OpenMP in the Linux wheel for consistency with other binaries
    
    * Add directory in search path
    
    * Explicitly set the build option

--- python/tools/prepare_build_environment_linux.sh
+++ python/tools/prepare_build_environment_linux.sh
@@ -19,13 +19,14 @@ DNNL_BUILD=467
 yum-config-manager --add-repo https://yum.repos.intel.com/oneapi
 rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 yum install -y intel-oneapi-mkl-devel-$ONEAPI_VERSION-$MKL_BUILD intel-oneapi-dnnl-devel-$ONEAPI_VERSION-$DNNL_BUILD
-echo "/opt/intel/oneapi/dnnl/latest/cpu_gomp/lib" > /etc/ld.so.conf.d/intel-dnnl.conf
+echo "/opt/intel/oneapi/compiler/latest/linux/compiler/lib/intel64_lin" > /etc/ld.so.conf.d/libiomp5.conf
+echo "/opt/intel/oneapi/dnnl/latest/cpu_iomp/lib" > /etc/ld.so.conf.d/intel-dnnl.conf
 ldconfig
 
 pip install "cmake==3.18.4"
 
 mkdir build-release && cd build-release
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CLI=OFF -DWITH_DNNL=ON -DOPENMP_RUNTIME=COMP -DWITH_CUDA=ON -DCUDA_DYNAMIC_LOADING=ON -DCUDA_NVCC_FLAGS="-Xfatbin=-compress-all" -DCUDA_ARCH_LIST="Common" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CLI=OFF -DWITH_DNNL=ON -DOPENMP_RUNTIME=INTEL -DWITH_CUDA=ON -DCUDA_DYNAMIC_LOADING=ON -DCUDA_NVCC_FLAGS="-Xfatbin=-compress-all" -DCUDA_ARCH_LIST="Common" ..
 make -j$(nproc) install
 cd ..
 rm -r build-release
