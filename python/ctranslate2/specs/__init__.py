1d94375a8956706aa5615c421c52d3fc768f0c35
Guillaume Klein <guillaumekln@users.noreply.github.com>
2022-03-14 09:49:20 +0100

    Make enums Activation and EmbeddingsMerge public in ctranslate2.specs (#734)

--- python/ctranslate2/specs/__init__.py
+++ python/ctranslate2/specs/__init__.py
@@ -1,3 +1,5 @@
+from ctranslate2.specs.common_spec import Activation
+from ctranslate2.specs.common_spec import EmbeddingsMerge
 from ctranslate2.specs.model_spec import LayerSpec
 from ctranslate2.specs.model_spec import ModelSpec
 from ctranslate2.specs.transformer_spec import TransformerSpec
