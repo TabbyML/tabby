3f5731a1dc9f61b63f75de4201c5fc621e5b5619
Guillaume Klein <guillaumekln@users.noreply.github.com>
2022-04-13 13:40:53 +0200

    Fix number of allocated cuRAND states (#773)

--- src/ops/multinomial_gpu.cu
+++ src/ops/multinomial_gpu.cu
@@ -26,10 +26,11 @@ namespace ctranslate2 {
       ScopedCurandStates(size_t num_states)
         : _allocator(get_allocator<Device::CUDA>())
         , _num_states(num_states)
-        , _states(static_cast<curandState*>(_allocator.allocate(num_states * sizeof (curandState))))
       {
         constexpr size_t num_init_threads = 32;
         const size_t blocks = std::max(num_states / num_init_threads, size_t(1));
+        const size_t alloc_size = blocks * num_init_threads * sizeof (curandState);
+        _states = static_cast<curandState*>(_allocator.allocate(alloc_size));
         init_curand_states_kernel<<<blocks, num_init_threads, 0, cuda::get_cuda_stream()>>>(
           _states, get_random_seed());
       }
