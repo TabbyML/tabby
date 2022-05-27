ee798d7cc00896af3b9241708c6579794488ef4c
Guillaume Klein <guillaumekln@users.noreply.github.com>
2022-05-27 16:49:34 +0200

    Make base ModelSpec classes public (#814)

--- python/ctranslate2/specs/__init__.py
+++ python/ctranslate2/specs/__init__.py
@@ -1,5 +1,10 @@
 from ctranslate2.specs.common_spec import Activation, EmbeddingsMerge
-from ctranslate2.specs.model_spec import LayerSpec, ModelSpec
+from ctranslate2.specs.model_spec import (
+    LanguageModelSpec,
+    LayerSpec,
+    ModelSpec,
+    SequenceToSequenceModelSpec,
+)
 from ctranslate2.specs.transformer_spec import (
     TransformerDecoderModelSpec,
     TransformerSpec,
