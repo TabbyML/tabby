52cb5853e3e9529e10fa79616eac982e3bf70437
Guillaume Klein <guillaumekln@users.noreply.github.com>
2022-06-30 14:32:00 +0200

    Remove unnecessary libomp installation command on macOS (#857)
    The library is already installed on the GitHub runner.

--- python/tools/prepare_build_environment_macos.sh
+++ python/tools/prepare_build_environment_macos.sh
@@ -22,10 +22,6 @@ else
     hdiutil attach -noverify -noautofsck $(basename $ONEAPI_INSTALLER_URL)
     sudo /Volumes/$(basename $ONEAPI_INSTALLER_URL .dmg)/bootstrapper.app/Contents/MacOS/bootstrapper --silent --eula accept --components intel.oneapi.mac.mkl.devel
 
-    # Install LLVM's libomp because Intel's OpenMP runtime included in MKL does
-    # not ship with the header file.
-    brew install libomp
-
 fi
 
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CLI=OFF -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_CXX_FLAGS="-Wno-unused-command-line-argument" $CMAKE_EXTRA_OPTIONS ..
