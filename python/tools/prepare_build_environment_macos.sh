ae1f84e2cdb1ce0f67568827a2a3dbbf1507e0a7
Guillaume Klein <guillaumekln@users.noreply.github.com>
2022-12-07 12:46:18 +0100

    Build macOS x64 wheels against oneDNN to support the Conv1D op (#1007)

--- python/tools/prepare_build_environment_macos.sh
+++ python/tools/prepare_build_environment_macos.sh
@@ -22,6 +22,17 @@ else
     hdiutil attach -noverify -noautofsck $(basename $ONEAPI_INSTALLER_URL)
     sudo /Volumes/$(basename $ONEAPI_INSTALLER_URL .dmg)/bootstrapper.app/Contents/MacOS/bootstrapper --silent --eula accept --components intel.oneapi.mac.mkl.devel
 
+    ONEDNN_VERSION=2.7.2
+    wget -q https://github.com/oneapi-src/oneDNN/archive/refs/tags/v${ONEDNN_VERSION}.tar.gz
+    tar xf *.tar.gz && rm *.tar.gz
+    cd oneDNN-*
+    cmake -DCMAKE_BUILD_TYPE=Release -DDNNL_LIBRARY_TYPE=STATIC -DDNNL_BUILD_EXAMPLES=OFF -DDNNL_BUILD_TESTS=OFF -DDNNL_ENABLE_WORKLOAD=INFERENCE -DDNNL_ENABLE_PRIMITIVE="CONVOLUTION;REORDER" .
+    make -j$(sysctl -n hw.physicalcpu_max) install
+    cd ..
+    rm -r oneDNN-*
+
+    CMAKE_EXTRA_OPTIONS='-DWITH_DNNL=ON'
+
 fi
 
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_CLI=OFF -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_CXX_FLAGS="-Wno-unused-command-line-argument" $CMAKE_EXTRA_OPTIONS ..
