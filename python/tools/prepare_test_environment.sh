0dddb431a5fb2a314a70050c9da4532608fcff3e
Guillaume Klein <guillaumekln@users.noreply.github.com>
2022-12-29 15:54:11 +0100

    Reduce disk usage when running Python tests in the CI (#1034)

--- python/tools/prepare_test_environment.sh
+++ python/tools/prepare_test_environment.sh
@@ -4,12 +4,14 @@ set -e
 set -x
 
 # Install test rquirements
-pip install -r python/tests/requirements.txt
+pip --no-cache-dir install -r python/tests/requirements.txt --extra-index-url https://download.pytorch.org/whl/cpu
 pip uninstall -y ctranslate2
 
 # Download test data
 curl -o transliteration-aren-all.tar.gz https://opennmt-models.s3.amazonaws.com/transliteration-aren-all.tar.gz
 tar xf transliteration-aren-all.tar.gz -C tests/data/models/
+rm transliteration-aren-all.tar.gz
 
 curl -O https://object.pouta.csc.fi/OPUS-MT-models/en-de/opus-2020-02-26.zip
 unzip opus-2020-02-26.zip -d tests/data/models/opus-mt-ende
+rm opus-2020-02-26.zip
