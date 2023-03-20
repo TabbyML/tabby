fe8e02aec612afb17a5e90b92b75d645d08ba21c
Meng Zhang <meng@tabbyml.com>
2023-03-20 16:51:28 +0800

    Add testdata for GPTNeoX

--- converter/huggingface_gptneox_convert.py
+++ converter/huggingface_gptneox_convert.py
@@ -2,12 +2,13 @@ import argparse
 import configparser
 import multiprocessing
 import os
+import shutil
 import sys
 from pathlib import Path
 
 import numpy as np
 import torch
-from transformers import GPTNeoXForCausalLM  # 4.21.1
+from transformers import GPTNeoXForCausalLM
 
 
 def get_weight_data_type(data_type):
@@ -19,51 +20,7 @@ def get_weight_data_type(data_type):
         assert False, f"Invalid weight data type {data_type}"
 
 
-def prefix_prompt_convert(args, config, weight_data_type):
-
-    saved_dir = args.saved_dir + "/%d-gpu/" % args.infer_gpu_num
-
-    prompt_in_file_list = args.prompt_in_file_list.split(",")
-
-    task_list = []
-    for idx, prompt_in_file in enumerate(prompt_in_file_list):
-        weights = torch.load(prompt_in_file)
-        task_name = prompt_in_file.split("/")[-1].split(".")[-3]
-
-        total_size = weights.nelement()
-        n_layers = config["num_hidden_layers"]
-        n_head = config["num_heads"]
-        size_per_head = config["hidden_size"] // n_head
-        prefix_prompt_len = total_size // (2 * n_layers * n_head * size_per_head)
-
-        task_list.append((task_name, prefix_prompt_len))
-        # GPT NeoX
-        weights = weights.view(
-            prefix_prompt_len, n_layers, 2, n_head, size_per_head
-        )  ## prefix_seq_len, num_layers, 2, num_heads, size_per_head
-        # weights=weights.view(prefix_prompt_len,28,2,16,256) ## prefix_seq_len, num_layers, 2, num_heads, size_per_head
-        weights = weights.permute(
-            1, 2, 3, 0, 4
-        )  ## num_layers, 2, num_heads, perfix_seq_len, size_per_head
-        local_head_num = n_head // args.infer_gpu_num
-        weights_split = torch.split(weights, local_head_num, dim=2)
-        for i in range(args.infer_gpu_num):
-            output_file_path = (
-                saved_dir
-                + "/model.prefix_prompt."
-                + task_name
-                + ".weight."
-                + str(i)
-                + ".bin"
-            )
-            weights_split[i].detach().cpu().numpy().astype(weight_data_type).tofile(
-                output_file_path
-            )
-
-    return task_list
-
-
-def split_and_convert_process(i, saved_dir, factor, key, args, config, val):
+def split_and_convert_process(saved_dir, factor, key, args, config, val):
 
     if (
         key.find("input_layernorm.weight") != -1
@@ -75,11 +32,8 @@ def split_and_convert_process(i, saved_dir, factor, key, args, config, val):
         or key.find("final_layernorm.weight") != -1
         or key.find("final_layernorm.bias") != -1
     ):
-
-        # shared weights, only need to convert the weights of rank 0
-        if i == 0:
-            saved_path = saved_dir + "/model." + key + ".bin"
-            val.tofile(saved_path)
+        saved_path = saved_dir + f"/model.{key}.bin"
+        val.tofile(saved_path)
 
     elif (
         key.find("attention.dense.weight") != -1
@@ -87,7 +41,7 @@ def split_and_convert_process(i, saved_dir, factor, key, args, config, val):
     ):
         split_vals = np.split(val, factor, axis=0)
         for j in range(factor):
-            saved_path = saved_dir + "/model." + key + ".%d.bin" % (i * factor + j)
+            saved_path = saved_dir + f"/model.{key}.{j}.bin"
             split_vals[j].tofile(saved_path)
 
     elif (
@@ -97,7 +51,7 @@ def split_and_convert_process(i, saved_dir, factor, key, args, config, val):
 
         split_vals = np.split(val, factor, axis=-1)
         for j in range(factor):
-            saved_path = saved_dir + "/model." + key + ".%d.bin" % (i * factor + j)
+            saved_path = saved_dir + f"/model.{key}.{j}.bin"
             split_vals[j].tofile(saved_path)
 
     elif key.find("attention.query_key_value.bias") != -1:
@@ -109,7 +63,7 @@ def split_and_convert_process(i, saved_dir, factor, key, args, config, val):
         split_vals = np.split(val, factor, axis=-1)
 
         for j in range(factor):
-            saved_path = saved_dir + "/model." + key + ".%d.bin" % (i * factor + j)
+            saved_path = saved_dir + f"/model.{key}.{j}.bin"
             split_vals[j].tofile(saved_path)
 
     elif key.find("attention.query_key_value.weight") != -1:
@@ -125,7 +79,7 @@ def split_and_convert_process(i, saved_dir, factor, key, args, config, val):
         split_vals = np.split(val, factor, axis=-1)
 
         for j in range(factor):
-            saved_path = saved_dir + "/model." + key + ".%d.bin" % (i * factor + j)
+            saved_path = saved_dir + f"/model.{key}.{j}.bin"
             split_vals[j].tofile(saved_path)
 
     else:
@@ -149,45 +103,30 @@ def split_and_convert(args):
     # model = torch.load(ckpt_name)
     model = GPTNeoXForCausalLM.from_pretrained(args.in_file)
     hf_config = vars(model.config)
-    if "gpt_j_residual" not in hf_config:
-        hf_config["gpt_j_residual"] = 0
 
     np_weight_data_type = get_weight_data_type(args.weight_data_type)
 
-    task_list = []
-    if args.prompt_in_file_list is not None:
-        task_list = prefix_prompt_convert(args, hf_config, np_weight_data_type)
-
     try:
         model_name = args.model_name
+        n_heads = hf_config["num_attention_heads"]
+        head_size = hf_config["hidden_size"] // n_heads
+        rotary_dim = int(head_size * hf_config["rotary_pct"])
+        use_gptj_residual = int(hf_config["use_parallel_residual"])
+
         config = configparser.ConfigParser()
         config["gptneox"] = {}
         config["gptneox"]["model_name"] = model_name
-        config["gptneox"]["head_num"] = str(hf_config["num_attention_heads"])
-        n_embd = hf_config["hidden_size"]
-        config["gptneox"]["size_per_head"] = str(
-            n_embd // hf_config["num_attention_heads"]
-        )
-        config["gptneox"]["inter_size"] = str(n_embd * 4)
+        config["gptneox"]["head_num"] = str(n_heads)
+        config["gptneox"]["size_per_head"] = str(head_size)
+        config["gptneox"]["inter_size"] = str(hf_config["intermediate_size"])
         config["gptneox"]["num_layer"] = str(hf_config["num_hidden_layers"])
-        if "rotary_dim" in hf_config:
-            rotary_dim = hf_config["rotary_dim"]
-        else:
-            rotary_dim = n_embd // hf_config["num_attention_heads"]
         config["gptneox"]["rotary_embedding"] = str(rotary_dim)
         config["gptneox"]["vocab_size"] = str(hf_config["vocab_size"])
         config["gptneox"]["start_id"] = str(hf_config["bos_token_id"])
         config["gptneox"]["end_id"] = str(hf_config["eos_token_id"])
-        config["gptneox"]["use_gptj_residual"] = str(int(hf_config["gpt_j_residual"]))
+        config["gptneox"]["use_gptj_residual"] = str(use_gptj_residual)
         config["gptneox"]["weight_data_type"] = args.weight_data_type
 
-        if len(task_list) > 0:
-            config["gptneox"]["num_tasks"] = str(len(task_list))
-            config["gptneox"]["prompt_learning_type"] = str(2)
-            for idx, (task_name, prompt_length) in enumerate(task_list):
-                config[f"task_{idx}"] = {}
-                config[f"task_{idx}"]["task_name"] = task_name
-                config[f"task_{idx}"]["prompt_length"] = str(prompt_length)
         with open((Path(saved_dir) / f"config.ini").as_posix(), "w") as configfile:
             config.write(configfile)
     except Exception as e:
@@ -211,24 +150,19 @@ def split_and_convert(args):
     torch.multiprocessing.set_start_method("spawn")
     pool = multiprocessing.Pool(args.processes)
     for name, param in model.named_parameters():
+        array = param.detach().cpu().numpy().astype(np_weight_data_type)
+        # print("input shape", name, array.shape)
         if name.find("weight") == -1 and name.find("bias") == -1:
+            print("skipped", name)
             continue
         elif name == "gpt_neox.embed_in.weight":
-            param.detach().cpu().numpy().astype(np_weight_data_type).tofile(
-                saved_dir + "model.wte.bin"
-            )
+            array.tofile(saved_dir + "model.wte.bin")
         elif name == "gpt_neox.final_layer_norm.bias":
-            param.detach().cpu().numpy().astype(np_weight_data_type).tofile(
-                saved_dir + "model.final_layernorm.bias.bin"
-            )
+            array.tofile(saved_dir + "model.final_layernorm.bias.bin")
         elif name == "gpt_neox.final_layer_norm.weight":
-            param.detach().cpu().numpy().astype(np_weight_data_type).tofile(
-                saved_dir + "model.final_layernorm.weight.bin"
-            )
+            array.tofile(saved_dir + "model.final_layernorm.weight.bin")
         elif name == "embed_out.weight":
-            param.detach().cpu().numpy().astype(np_weight_data_type).tofile(
-                saved_dir + "model.lm_head.weight.bin"
-            )
+            array.tofile(saved_dir + "model.lm_head.weight.bin")
         else:
             processed = False
             for i in range(len(ft_model_name_pattern)):
@@ -238,17 +172,12 @@ def split_and_convert(args):
                         split_and_convert_process,
                         [
                             (
-                                0,
                                 saved_dir,
                                 factor,
                                 new_name,
                                 args,
                                 vars(model.config),
-                                param.detach()
-                                .cpu()
-                                .numpy()
-                                .astype(np_weight_data_type)
-                                .T,
+                                array.T,
                             )
                         ],
                     )
@@ -256,13 +185,13 @@ def split_and_convert(args):
                     break
 
             if not processed:
-                raise Exception("Unused layer", name)
+                print("Unused layer", name)
 
     pool.close()
     pool.join()
 
     # Post-process biases if use_gptj_residual is True
-    if hf_config["gpt_j_residual"]:
+    if use_gptj_residual:
         for layer_idx in range(hf_config["n_layer"]):
             attn_bias = np.fromfile(
                 saved_dir + f"/model.layers.{layer_idx}.attention.dense.bias.bin",
@@ -289,13 +218,6 @@ if __name__ == "__main__":
         type=str,
         help="file name of input checkpoint file",
         required=True,
-    )
-    parser.add_argument(
-        "-prompt_in_file_list",
-        "-p_i_list",
-        type=str,
-        help="list of the prompt weight file path,"
-        "separate by (,). e.g. -prompt_in_file_list prefix_prompt.task0.weight,prefix_prompt.task1.weight",
     )
     parser.add_argument(
         "-trained_gpu_num",
@@ -331,4 +253,5 @@ if __name__ == "__main__":
         print("{}: {}".format(key, vars(args)[key]))
     print("========================================")
 
+    shutil.rmtree(args.saved_dir, ignore_errors=True)
     split_and_convert(args)
