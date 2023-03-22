a7124fbda1f2285405186482a8690d8db12e726f
Meng Zhang <meng@tabbyml.com>
2023-03-22 16:01:18 +0800

    Basic admin panel

--- admin/Home.py
+++ admin/Home.py
@@ -1,1 +1,17 @@
 import streamlit as st
+from utils.service_info import ServiceInfo
+
+SERVICES = [
+    ServiceInfo(label="server", url="http://server:5000"),
+    ServiceInfo(label="triton", url="http://triton:8002/metrics"),
+]
+
+
+def make_badge_markdown(x: ServiceInfo):
+    return f"![{x.label}]({x.badge_url})"
+
+
+st.markdown("## Status")
+st.markdown(" ".join(map(make_badge_markdown, SERVICES)))
+
+st.markdown("## Quality")
