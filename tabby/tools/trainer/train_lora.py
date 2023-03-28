5751912ffbc6eac02b3e347d94ff5edced47e50c
Meng Zhang <meng@tabbyml.com>
2023-03-28 15:34:12 +0800

    Remove bnb

--- tabby/tools/trainer/train_lora.py
+++ tabby/tools/trainer/train_lora.py
@@ -1,79 +1,74 @@
 import os
-import sys
-from typing import List, Optional
+from dataclasses import dataclass, field
+from typing import List
 
-import bitsandbytes as bnb
+import peft
 import torch
 import torch.nn as nn
 import transformers
-from peft import (
-    LoraConfig,
-    get_peft_model,
-    get_peft_model_state_dict,
-    prepare_model_for_int8_training,
-    set_peft_model_state_dict,
-)
 from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser
 
 from .dataset import load_dataset
 
 
-def train(
-    # model/data params
-    base_model: str = "TabbyML/J-350M",
-    data_path: str = "./alpaca_data_cleaned.json",
-    output_dir: str = "./lora-alpaca",
-    # training hyperparams
-    batch_size: int = 128,
-    micro_batch_size: int = 4,
-    num_epochs: int = 3,
-    learning_rate: float = 3e-4,
-    cutoff_len: int = 256,
-    val_set_size: int = 2000,
-    # lora hyperparams
-    lora_r: int = 8,
-    lora_alpha: int = 16,
-    lora_dropout: float = 0.05,
-    lora_target_modules: List[str] = [
-        "q_proj",
-        "v_proj",
-    ],
-    # llm hyperparams
-    train_on_inputs: bool = True,  # if False, masks out inputs in loss
-    group_by_length: bool = False,  # faster, but produces an odd training loss curve,
-    resume_from_checkpoint: str = None,  # either training checkpoint or final adapter
-):
-    gradient_accumulation_steps = batch_size // micro_batch_size
+@dataclass
+class TrainLoraArguments:
+    data_path: str = field(metadata={"help": "Dataset dir for training / eval "})
+    output_dir: str = field(metadata={"help": "Output dir for checkpoint"})
+    base_model: str = field(
+        default="TabbyML/J-350M", metadata={"help": "Base model for fine-tuning"}
+    )
 
-    device_map = "auto"
-    world_size = int(os.environ.get("WORLD_SIZE", 1))
-    ddp = world_size != 1
-    if ddp:
-        device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
-        gradient_accumulation_steps = gradient_accumulation_steps // world_size
+    batch_size: int = 128
+    micro_batch_size: int = 4
+    num_epochs: int = 3
+    learning_rate: float = 3e-4
+    cutoff_len: int = 256
+
+    # Evaluations
+    val_set_size: int = 2000
+    eval_steps: int = 200
+
+    # Lora Hyperparams
+    lora_r: int = 8
+    lora_alpha: int = 16
+    lora_dropout: float = 0.05
+    lora_target_modules: List[str] = (
+        [
+            "q_proj",
+            "v_proj",
+        ],
+    )
+    resume_from_checkpoint: str = None  # either training checkpoint or final adapter
+
+
+def parse_args() -> TrainLoraArguments:
+    parser = HfArgumentParser(TrainLoraArguments)
+    return parser.parse_args()
+
+
+def train(args: TrainLoraArguments):
+    gradient_accumulation_steps = args.batch_size // args.micro_batch_size
 
     model = AutoModelForCausalLM.from_pretrained(
-        base_model,
-        load_in_8bit=True,
-        device_map=device_map,
+        args.base_model, torch_dtype=torch.float16
     )
 
-    tokenizer = AutoTokenizer.from_pretrained(base_model)
+    tokenizer = AutoTokenizer.from_pretrained(args.base_model)
 
-    model = prepare_model_for_int8_training(model)
-
-    config = LoraConfig(
-        r=lora_r,
-        lora_alpha=lora_alpha,
-        target_modules=lora_target_modules,
-        lora_dropout=lora_dropout,
+    config = peft.LoraConfig(
+        r=args.lora_r,
+        lora_alpha=args.lora_alpha,
+        target_modules=args.lora_target_modules,
+        lora_dropout=args.lora_dropout,
         bias="none",
-        task_type="CAUSAL_LM",
+        task_type=peft.TaskType.CAUSAL_LM,
     )
-    model = get_peft_model(model, config)
+    model = peft.get_peft_model(model, config)
 
-    data = load_dataset(tokenizer, data_path, seq_length=128)
+    data = load_dataset(tokenizer, args.data_path, seq_length=args.cutoff_len)
 
+    resume_from_checkpoint = args.resume_from_checkpoint
     if resume_from_checkpoint:
         # Check the available weights and load them
         checkpoint_name = os.path.join(
@@ -88,59 +83,55 @@ def train(
         if os.path.exists(checkpoint_name):
             print(f"Restarting from {checkpoint_name}")
             adapters_weights = torch.load(checkpoint_name)
-            model = set_peft_model_state_dict(model, adapters_weights)
+            model = peft.set_peft_model_state_dict(model, adapters_weights)
         else:
             print(f"Checkpoint {checkpoint_name} not found")
 
     model.print_trainable_parameters()  # Be more transparent about the % of trainable params.
 
-    if val_set_size > 0:
-        train_val = data.train_test_split(test_size=val_set_size, shuffle=True, seed=42)
-        train_data = train_val["train"].shuffle()
-        val_data = train_val["test"].shuffle()
-    else:
-        train_data = data.shuffle()
-        val_data = None
+    train_val = data.train_test_split(
+        test_size=args.val_set_size, shuffle=True, seed=42
+    )
+    train_data = train_val["train"].shuffle()
+    val_data = train_val["test"].shuffle()
 
     trainer = transformers.Trainer(
         model=model,
         train_dataset=train_data,
         eval_dataset=val_data,
         args=transformers.TrainingArguments(
-            per_device_train_batch_size=micro_batch_size,
+            per_device_train_batch_size=args.micro_batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
             warmup_steps=100,
-            num_train_epochs=num_epochs,
-            learning_rate=learning_rate,
+            num_train_epochs=args.num_epochs,
+            learning_rate=args.learning_rate,
             fp16=True,
             logging_steps=10,
-            evaluation_strategy="steps" if val_set_size > 0 else "no",
+            evaluation_strategy="steps",
             save_strategy="steps",
-            eval_steps=200 if val_set_size > 0 else None,
-            save_steps=200,
-            output_dir=output_dir,
+            eval_steps=args.eval_steps,
+            save_steps=args.eval_steps,
+            output_dir=args.output_dir,
             save_total_limit=3,
-            load_best_model_at_end=True if val_set_size > 0 else False,
-            ddp_find_unused_parameters=False if ddp else None,
-            group_by_length=group_by_length,
+            load_best_model_at_end=True,
         ),
     )
     model.config.use_cache = False
 
     old_state_dict = model.state_dict
     model.state_dict = (
-        lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
+        lambda self, *_, **__: peft.get_peft_model_state_dict(self, old_state_dict())
     ).__get__(model, type(model))
 
-    if torch.__version__ >= "2" and sys.platform != "win32":
-        model = torch.compile(model)
+    model = torch.compile(model)
 
     trainer.train(resume_from_checkpoint=resume_from_checkpoint)
 
-    model.save_pretrained(output_dir)
+    model.save_pretrained(args.output_dir)
 
     print("\n If there's a warning about missing keys above, please disregard :)")
 
 
 if __name__ == "__main__":
-    train(data_path="/mnt/disk/Projects/tabbyml/tabby/data/dataset")
+    args = parse_args()
+    train(args)
