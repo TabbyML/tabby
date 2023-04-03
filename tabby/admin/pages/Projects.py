4361ca8bbbf3367c753606c74665a5eb8a55368c
Meng Zhang <meng@tabbyml.com>
2023-04-03 13:01:53 +0800

    feat: display dataset info in projects

--- tabby/admin/pages/Projects.py
+++ tabby/admin/pages/Projects.py
@@ -3,17 +3,35 @@ from pathlib import Path
 
 import streamlit as st
 import toml
+from datasets import load_from_disk
 from git import Repo
 
 st.set_page_config(page_title="Tabby Admin - Projects")
 
+dataset_dir = os.environ.get("DATASET_DIR", None)
 git_repositories_dir = os.environ.get("GIT_REPOSITORIES_DIR", None)
 config_file = os.environ.get("CONFIG_FILE", None)
 config = toml.load(config_file)
-data = config.get("projects", {})
+
+
+def dataset_info():
+    st.subheader("Dataset")
+    if not Path(dataset_dir).is_dir():
+        st.write("*Not populated*")
+        return
+
+    info = load_from_disk(dataset_dir)
+    st.write("Source files: ", len(info))
 
 
 def project_list():
+    data = config.get("projects", {})
+
+    if len(data) <= 0:
+        st.write("Your project list is empty")
+        st.write(f"Edit `{config_file}` to add projects")
+        return
+
     for k, v in data.items():
         st.subheader(k)
         st.write(f'Git: {v["git_url"]}')
@@ -28,8 +46,6 @@ def project_list():
         st.write(f"Status: `{sha}`")
 
 
-if len(data) > 0:
-    project_list()
-else:
-    st.subheader("Your project list is empty")
-    st.write(f"Edit `{config_file}` to add projects")
+dataset_info()
+st.write("---")
+project_list()
