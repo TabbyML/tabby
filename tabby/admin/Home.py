0021f63930bb72b3603f5adceac129d6d140ea1a
Meng Zhang <meng@tabbyml.com>
2023-04-03 17:01:44 +0800

    feat: add a few default options in admin/Home

--- tabby/admin/Home.py
+++ tabby/admin/Home.py
@@ -16,5 +16,37 @@ st.set_page_config(page_title="Tabby Admin - Home")
 
 st.markdown("## Tabby")
 st.markdown(" ".join(map(make_badge_markdown, SERVICES)))
+st.markdown("---")
 
-monaco.st_monaco()
+SNIPPETS = {
+    "Clear": "# Write some code ...",
+    "Fibonacci": "def fib(n):",
+    "Parse JSON": """def parse_json_lines(filename: str) -> List[Any]:
+    output = []
+    with open(filename, "r", encoding="utf-8") as f:
+""",
+    "Data ORM": """import birdchirp
+
+from birdchirp.model.chirp import chirp
+from birdchirp.db.mysql import MysqlDb
+
+class User:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.db = MysqlDb()
+
+    def get_avatar""",
+}
+
+
+def code_editor():
+    code = ""
+    cols = st.columns(len(SNIPPETS))
+    for col, (k, v) in zip(cols, SNIPPETS.items()):
+        with col:
+            if st.button(k):
+                code = v
+    monaco.st_monaco(key="default", code=code)
+
+
+code_editor()
