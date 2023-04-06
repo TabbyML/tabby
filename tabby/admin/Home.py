28922bf9d9eaf6a0b2c2cf2218b0c350b8fa5fef
Meng Zhang <meng@tabbyml.com>
2023-04-06 10:26:06 +0800

    refactor: extract Editor page

--- tabby/admin/Home.py
+++ tabby/admin/Home.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from components import monaco
 from utils.service_info import ServiceInfo
 
 SERVICES = [
@@ -26,37 +25,3 @@ st.set_page_config(page_title="Tabby Admin - Home")
 
 st.markdown("## Tabby")
 st.markdown(" $~$ ".join(map(make_badge_markdown, SERVICES)))
-st.markdown("---")
-
-SNIPPETS = {
-    "Clear": "# Write some code ...",
-    "Fibonacci": "def fib(n):",
-    "Parse JSON": """def parse_json_lines(filename: str) -> List[Any]:
-    output = []
-    with open(filename, "r", encoding="utf-8") as f:
-""",
-    "Data ORM": """import birdchirp
-
-from birdchirp.model.chirp import chirp
-from birdchirp.db.mysql import MysqlDb
-
-class User:
-    def __init__(self, user_id):
-        self.user_id = user_id
-        self.db = MysqlDb()
-
-    def get_avatar""",
-}
-
-
-def code_presets():
-    code = ""
-    cols = st.columns(len(SNIPPETS))
-    for col, (k, v) in zip(cols, SNIPPETS.items()):
-        with col:
-            if st.button(k):
-                code = v
-    return code
-
-
-monaco.st_monaco(key="default", code=code_presets())
