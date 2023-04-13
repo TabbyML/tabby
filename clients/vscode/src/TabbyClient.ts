a9b00b14500836a37403a83b9d8a00f09faa4cea
Zhiming Ma <codes.icy@gmail.com>
2023-04-13 15:28:30 +0800

    Update vscode use openapi codegen. (#99)

--- clients/vscode/src/TabbyClient.ts
+++ clients/vscode/src/TabbyClient.ts
@@ -3,43 +3,7 @@ import axios from "axios";
 import { sleep } from "./utils";
 import { EventEmitter } from "node:events";
 import { strict as assert } from "node:assert";
-
-const logAxios = false;
-if (logAxios) {
-  axios.interceptors.request.use((request) => {
-    console.debug("Starting Request: ", request);
-    return request;
-  });
-  axios.interceptors.response.use((response) => {
-    console.debug("Response: ", response);
-    return response;
-  });
-}
-
-export interface TabbyCompletionRequest {
-  prompt: string;
-  language?: string;
-}
-
-export interface TabbyCompletion {
-  id?: string;
-  created?: number;
-  choices?: Array<{
-    index: number;
-    text: string;
-  }>;
-}
-
-export enum EventType {
-  InlineCompletionDisplayed = "view",
-  InlineCompletionAccepted = "select",
-}
-
-export interface Event {
-  type: EventType;
-  id?: string;
-  index?: number;
-}
+import { Tabby as TabbyApi } from "./generated";
 
 export class TabbyClient extends EventEmitter {
   private static instance: TabbyClient;
@@ -50,77 +14,52 @@ export class TabbyClient extends EventEmitter {
     return TabbyClient.instance;
   }
 
-  private tabbyServerUrl: string = "";
+  private serverUrl: string = "";
   status: "connecting" | "ready" | "disconnected" = "connecting";
+  api: TabbyApi;
 
   constructor() {
     super();
 
     this.updateConfiguration();
+    this.api = new TabbyApi({ BASE: this.serverUrl });
     workspace.onDidChangeConfiguration((event) => {
       if (event.affectsConfiguration("tabby")) {
         this.updateConfiguration();
+        this.api = new TabbyApi({ BASE: this.serverUrl });
       }
     });
   }
 
   private updateConfiguration() {
     const configuration = workspace.getConfiguration("tabby");
-    this.tabbyServerUrl = configuration.get("serverUrl", "http://127.0.0.1:5000");
+    this.serverUrl = configuration.get("serverUrl", "http://127.0.0.1:5000");
+    this.serverUrl = this.serverUrl.replace(/\/$/, ''); // Remove trailing slash
     this.ping();
   }
 
-  private changeStatus(status: "connecting" | "ready" | "disconnected") {
+  public changeStatus(status: "connecting" | "ready" | "disconnected") {
     if (this.status != status) {
       this.status = status;
       this.emit("statusChanged", status);
     }
   }
 
-  private async ping(tries: number = 0) {
+  private async ping(tries: number = 0): Promise<boolean> {
     try {
-      const response = await axios.get(`${this.tabbyServerUrl}/`);
+      const response = await axios.get(`${this.serverUrl}/`);
       assert(response.status == 200);
       this.changeStatus("ready");
+      return true;
     } catch (e) {
       if (tries > 5) {
         this.changeStatus("disconnected");
-        return;
+        return false;
       }
       this.changeStatus("connecting");
       const pingRetryDelay = 1000;
       await sleep(pingRetryDelay);
-      this.ping(tries + 1);
-    }
-  }
-
-  public async getCompletion(request: TabbyCompletionRequest): Promise<TabbyCompletion | null> {
-    if (this.status == "disconnected") {
-      this.ping();
-    }
-    try {
-      const response = await axios.post<TabbyCompletion>(`${this.tabbyServerUrl}/v1/completions`, request);
-      assert(response.status == 200);
-      return response.data;
-    } catch (e) {
-      this.ping();
-      return null;
-    }
-  }
-
-  public async postEvent(event: Event) {
-    if (this.status == "disconnected") {
-      this.ping();
-    }
-    try {
-      const response = await axios.post(`${this.tabbyServerUrl}/v1/events`, {
-        type: event.type,
-        completion_id: event.id,
-        choice_index: event.index,
-      });
-      assert(response.status == 200);
-    } catch (e) {
-      this.ping();
+      return this.ping(tries + 1);
     }
   }
 }
