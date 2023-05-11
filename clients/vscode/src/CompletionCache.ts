75c82fa7c610f730dcf3b54548d28d6157741635
Zhiming Ma <codes.icy@gmail.com>
2023-05-11 22:55:34 +0800

    VSCode extensions: add completion cache. (#128)

---
+++ clients/vscode/src/CompletionCache.ts
@@ -0,0 +1,75 @@
+import { LinkedList } from "linked-list-typescript";
+import { CompletionResponse, Choice } from "./generated";
+
+type Range = {
+  start: number;
+  end: number;
+};
+
+export type CompletionCacheEntry = {
+  documentId: any;
+  promptRange: Range;
+  prompt: string;
+  completion: CompletionResponse;
+};
+
+export class CompletionCache {
+  public static cacheSize = 10;
+  private cache = new LinkedList<CompletionCacheEntry>();
+
+  constructor() {}
+
+  private evict() {
+    while (this.cache.length > CompletionCache.cacheSize) {
+      this.cache.removeTail();
+    }
+  }
+
+  private pop(entry: CompletionCacheEntry) {
+    this.cache.remove(entry);
+    this.cache.prepend(entry);
+  }
+
+  public add(entry: CompletionCacheEntry) {
+    this.evict();
+    this.cache.prepend(entry);
+  }
+
+  public findCompatible(documentId: any, text: string, cursor: number): CompletionResponse | null {
+    let hit: { entry: CompletionCacheEntry; compatibleChoices: Choice[] } | null = null;
+    for (const entry of this.cache) {
+      if (entry.documentId !== documentId) {
+        continue;
+      }
+      // Check if text in prompt range has not changed
+      if (text.slice(entry.promptRange.start, entry.promptRange.end) !== entry.prompt) {
+        continue;
+      }
+      // Filter choices that start with inputed text after prompt
+      const compatibleChoices = entry.completion.choices
+        .filter((choice) => choice.text.startsWith(text.slice(entry.promptRange.end, cursor)))
+        .map((choice) => {
+          return {
+            index: choice.index,
+            text: choice.text.substring(cursor - entry.promptRange.end),
+          };
+        });
+      if (compatibleChoices.length > 0) {
+        hit = {
+          entry,
+          compatibleChoices,
+        };
+        break;
+      }
+    }
+    if (hit) {
+      this.pop(hit.entry);
+      return {
+        id: hit.entry.completion.id,
+        created: hit.entry.completion.created,
+        choices: hit.compatibleChoices,
+      };
+    }
+    return null;
+  }
+}
