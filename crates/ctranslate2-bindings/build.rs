06cf34a0077596f7c7c99195e7b6363bb5e80847
Meng Zhang <meng@tabbyml.com>
2023-05-26 21:34:31 -0700

    support static linking of ctranslate2 (#148)
    * support static linking of ctranslate2
    
    * update
    
    * remove submodule rust-cxx-cmake-bridge
    
    * support alwayslink with whole-archive
    
    * update
    
    * move export_libs
    
    * update docker config
    
    * update ctranslate2
    
    * remove
    
    * update
    
    * update build.rs
    
    * parse external libs
    
    * cleanup
    
    * add cargo fmt

--- crates/ctranslate2-bindings/build.rs
+++ crates/ctranslate2-bindings/build.rs
@@ -1,21 +1,15 @@
 use cmake::Config;
+use rust_cxx_cmake_bridge::read_cmake_generated;
 
 fn main() {
-    let mut config = Config::new("CTranslate2");
+    let mut config = Config::new(".");
     config
         .define("CMAKE_BUILD_TYPE", "Release")
         .define("BUILD_CLI", "OFF")
-        .define("BUILD_SHARED_LIBS", "ON")
-        .define("CMAKE_INSTALL_RPATH_USE_LINK_PATH", "ON");
+        .define("CMAKE_INSTALL_RPATH_USE_LINK_PATH", "ON")
+        .define("BUILD_SHARED_LIBS", "OFF");
 
-    if cfg!(target_os = "macos") {
-        config
-            .define("CMAKE_OSX_ARCHITECTURES", "arm64")
-            .define("WITH_ACCELERATE", "ON")
-            .define("WITH_MKL", "OFF")
-            .define("OPENMP_RUNTIME", "NONE")
-            .define("WITH_RUY", "ON");
-    } else if cfg!(target_os = "linux") {
+    if cfg!(target_os = "linux") {
         config
             .define("WITH_CUDA", "ON")
             .define("WITH_CUDNN", "ON")
@@ -24,16 +18,26 @@ fn main() {
             .define("OPENMP_RUNTIME", "COMP")
             .cxxflag("-msse4.1")
             .define("CUDA_NVCC_FLAGS", "-Xfatbin=-compress-all")
-            .define("CUDA_ARCH_LIST", "Common");
-    }
+            .define("CUDA_ARCH_LIST", "Common")
+    } else if cfg!(target_os = "macos") {
+        config
+            .define("CMAKE_OSX_ARCHITECTURES", "arm64")
+            .define("WITH_ACCELERATE", "ON")
+            .define("WITH_MKL", "OFF")
+            .define("OPENMP_RUNTIME", "NONE")
+            .define("WITH_RUY", "ON")
+    } else {
+        panic!("Invalid target")
+    };
 
     let dst = config.build();
 
-    println!(
-        "cargo:rustc-link-search=native={}",
-        dst.join("lib").display()
-    );
-    println!("cargo:rustc-link-lib=ctranslate2");
+    // Read static lib from generated deps.
+    let cmake_generated_libs_str = std::fs::read_to_string(
+        &format!("/{}/build/cmake_generated_libs", dst.display()).to_string(),
+    )
+    .unwrap();
+    read_cmake_generated(&cmake_generated_libs_str);
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=include/ctranslate2.h");
