fea645248e28bf9f57a5771283aa3d6ed6947576
Meng Zhang <meng@tabbyml.com>
2023-06-06 14:12:04 -0700

    add stop regexp

--- crates/ctranslate2-bindings/src/lib.rs
+++ crates/ctranslate2-bindings/src/lib.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use tokenizers::tokenizer::Tokenizer;
 use tokio_util::sync::CancellationToken;
 
@@ -67,16 +68,21 @@ pub struct TextInferenceOptions {
 
     #[builder(default = "1.0")]
     sampling_temperature: f32,
+
+    #[builder(default = "vec!()")]
+    stop_words: Vec<String>
 }
 
 pub struct InferenceContext {
+    stop_regexp: Regex,
     cancel: CancellationToken,
     output_text: String
 }
 
 impl InferenceContext {
-    fn new(cancel: CancellationToken) -> Self {
-        InferenceContext { cancel, output_text: "".to_owned() }
+    fn new(stop_words: Vec<String>, cancel: CancellationToken) -> Self {
+        let stop_regexp = Regex::new(stop_words.join("|").as_ref()).unwrap();
+        InferenceContext { stop_regexp, cancel, output_text: "".to_owned() }
     }
 }
 
@@ -108,7 +114,7 @@ impl TextInferenceEngine {
         let cancel_for_inference = cancel.clone();
         let _guard = cancel.drop_guard();
 
-        let context = InferenceContext::new(cancel_for_inference);
+        let context = InferenceContext::new(options.stop_words, cancel_for_inference);
         let output_tokens = tokio::task::spawn_blocking(move || {
             let context = Box::new(context);
             engine.inference(
@@ -136,10 +142,14 @@ impl TextInferenceEngine {
 }
 
 fn inference_callback(context: &mut InferenceContext, _step: usize, _token_id: u32, token: String) -> bool {
-    context.output_text.push_str(&token);
     if context.cancel.is_cancelled() {
         true
     } else {
-        false
+        context.output_text.push_str(&token);
+        if let Some(_) = context.stop_regexp.find(&context.output_text) {
+            true
+        } else {
+            false
+        }
     }
 }
