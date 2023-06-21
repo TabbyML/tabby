ddaef3e0c9f3228e0041944e4eabfe3f4fabbf95
Meng Zhang <meng@tabbyml.com>
2023-06-21 16:33:24 -0700

    add document.rs

--- crates/tabby-common/src/lib.rs
+++ crates/tabby-common/src/lib.rs
@@ -1,3 +1,16 @@
 pub mod config;
 pub mod events;
 pub mod path;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Document {
+    pub git_url: String,
+    pub filepath: String,
+    pub content: String,
+    pub language: String,
+    pub max_line_length: usize,
+    pub avg_line_length: f32,
+    pub alphanum_fraction: f32,
+}
