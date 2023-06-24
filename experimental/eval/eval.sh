c126bc3c858801d57d626379f10f5cd187bf1f43
Meng Zhang <meng@tabbyml.com>
2023-06-24 15:42:22 -0700

    update

--- experimental/eval/eval.sh
+++ experimental/eval/eval.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -ex
 
-if [ -z "$TABBY_ROOT"]; then
-  export TABBY_ROOT="$HOME/.tabby"
-fi
+docker-compose up -d
 
-papermill main.ipynb ./reports.ipynb -r filepattern "$TABBY_ROOT/dataset/*.jsonl"
+while ! curl -X POST http://localhost:8080/v1/health; do
+  echo "server not ready, waiting..."
+  sleep 5
+done
+
+papermill main.ipynb ./reports.ipynb -r filepattern "./tabby/dataset/*.jsonl"
 
 jupyter nbconvert reports.ipynb --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags remove --to html
+
+echo done
