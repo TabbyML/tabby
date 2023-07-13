ae60ca5ca223f7aec2ed48bc9a1e40e217d59d44
Meng Zhang <meng@tabbyml.com>
2023-07-13 11:26:32 +0800

    feat: load tantivy index in prompt builder

--- crates/tabby/src/serve/completions/prompt.rs
+++ crates/tabby/src/serve/completions/prompt.rs
@@ -1,14 +1,28 @@
 use strfmt::strfmt;
+use tabby_common::path::index_dir;
+use tantivy::{Index, ReloadPolicy, IndexReader, Searcher};
+use tracing::warn;
 
 use super::Segments;
 
 pub struct PromptBuilder {
     prompt_template: Option<String>,
+    reader: Option<IndexReader>
 }
 
 impl PromptBuilder {
     pub fn new(prompt_template: Option<String>) -> Self {
-        PromptBuilder { prompt_template }
+        let index = Index::open_in_dir(index_dir());
+        let reader = index.and_then(|index| index.reader_builder().reload_policy(ReloadPolicy::OnCommit).try_into());
+
+        if let Err(err) = &reader {
+            warn!("Failed to open index in {:?}: {:?}", index_dir(), err);
+        }
+
+        PromptBuilder {
+            prompt_template,
+            reader: reader.ok(),
+        }
     }
 
     fn build_prompt(&self, prefix: String, suffix: String) -> String {
@@ -19,8 +33,8 @@ impl PromptBuilder {
         }
     }
 
-    pub fn build(&self, _language: &str, segments: Segments) -> String {
-        let segments = self.rewrite(segments);
+    pub fn build(&self, language: &str, segments: Segments) -> String {
+        let segments = self.rewrite(language, segments);
         if let Some(suffix) = segments.suffix {
             self.build_prompt(segments.prefix, suffix)
         } else {
@@ -28,7 +42,16 @@ impl PromptBuilder {
         }
     }
 
-    fn rewrite(&self, segments: Segments) -> Segments {
+    fn rewrite(&self, language: &str, segments: Segments) -> Segments {
+        if let Some(reader) = &self.reader {
+            self.rewrite_with_index(reader.searcher(), language, segments)
+        } else {
+            segments
+        }
+    }
+
+    fn rewrite_with_index(&self, searcher: Searcher, language: &str, segments: Segments) -> Segments {
+        // FIXME(meng): implement this.
         segments
     }
 }
