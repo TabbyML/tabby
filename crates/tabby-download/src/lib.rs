57c811b30f1e8891b3c2e818119d85ac17b089af
Meng Zhang <meng@tabbyml.com>
2023-08-02 14:30:35 +0800

    fix: improve download logging (#325)
    * Suggest use `-it` so docker run generate progress bar of downloading information properly
    
    * add info! log for model download

--- crates/tabby-download/src/lib.rs
+++ crates/tabby-download/src/lib.rs
@@ -11,6 +11,7 @@ use tokio_retry::{
     strategy::{jitter, ExponentialBackoff},
     Retry,
 };
+use tracing::info;
 
 impl CacheInfo {
     async fn download(
@@ -61,6 +62,8 @@ pub async fn download_model(model_id: &str, prefer_local_file: bool) -> Result<(
         // Local path, no need for downloading.
         return Ok(());
     }
+
+    info!("Start downloading model `{}`", model_id);
 
     let mut cache_info = CacheInfo::from(model_id).await;
 
