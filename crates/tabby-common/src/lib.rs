6ea9251c16c0dbefa31adf7245d9779004b7f835
Meng Zhang <meng@tabbyml.com>
2023-08-09 15:11:20 +0800

    feat: add anonymous usage tracker

--- crates/tabby-common/src/lib.rs
+++ crates/tabby-common/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod config;
 pub mod events;
 pub mod path;
+pub mod usage;
 
 use std::{
     fs::File,
---
+++ crates/tabby-common/src/usage.rs
@@ -0,0 +1,58 @@
+use std::{
+    collections::HashMap,
+    fs::{self},
+};
+
+use lazy_static::lazy_static;
+use reqwest::Client;
+use uuid::Uuid;
+
+use crate::path::usage_id_file;
+
+struct UsageTracker {
+    id: String,
+    client: reqwest::Client,
+}
+
+impl UsageTracker {
+    fn new() -> Self {
+        if fs::metadata(usage_id_file()).is_err() {
+            // usage id file doesn't exists.
+            let id = Uuid::new_v4().to_string();
+            std::fs::write(usage_id_file(), id).expect("Failed to create usage id");
+        }
+
+        let id = fs::read_to_string(usage_id_file()).expect("Failed to read usage id");
+        let client = Client::new();
+
+        Self { id, client }
+    }
+
+    async fn capture(&self, event: &str) {
+        let params = HashMap::from([("distinctId", self.id.as_ref()), ("event", event)]);
+        self.client
+            .post("https://app.tabbyml.com/api/usage")
+            .json(&params)
+            .send()
+            .await
+            .ok();
+    }
+}
+
+lazy_static! {
+    static ref TRACKER: UsageTracker = UsageTracker::new();
+}
+
+pub async fn capture(event: &str) {
+    TRACKER.capture(event).await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::capture;
+
+    #[tokio::test]
+    async fn it_fire_event() {
+        capture("UsageTest").await
+    }
+}
