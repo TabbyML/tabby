2026b4dd0ecc38bc806b45855c1eeaa593c540c3
vodkaslime <646329483@qq.com>
2023-08-15 23:22:03 +0800

    feat: add architecture/cpu info to health api response [TAB-162] (#355)
    * feat: add architecture, cpu and gpu info to health command
    
    * chore: fix
    
    * chore: fix
    
    * chore: fix
    
    * chore: fix lint
    
    * chore: fix lint
    
    * chore: remove gpu
    
    * chore: resolve comments
    
    * chore: resolve comments
    
    * Update health.rs
    
    ---------
    
    Co-authored-by: Meng Zhang <meng@tabbyml.com>

--- crates/tabby/src/serve/health.rs
+++ crates/tabby/src/serve/health.rs
@@ -1,7 +1,8 @@
-use std::sync::Arc;
+use std::{env::consts::ARCH, sync::Arc};
 
 use axum::{extract::State, Json};
 use serde::{Deserialize, Serialize};
+use sysinfo::{CpuExt, System, SystemExt};
 use utoipa::ToSchema;
 
 #[derive(Serialize, Deserialize, ToSchema, Clone, Debug)]
@@ -9,14 +10,30 @@ pub struct HealthState {
     model: String,
     device: String,
     compute_type: String,
+    arch: String,
+    cpu_info: String,
+    cpu_count: usize,
 }
 
 impl HealthState {
     pub fn new(args: &super::ServeArgs) -> Self {
+        let mut sys = System::new_all();
+        sys.refresh_cpu();
+        let cpus = sys.cpus();
+        let cpu_info = if cpus.len() > 0 {
+            let cpu = &cpus[0];
+            cpu.brand().to_string()
+        } else {
+            "unknown".to_string()
+        };
+
         Self {
             model: args.model.clone(),
             device: args.device.to_string(),
             compute_type: args.compute_type.to_string(),
+            arch: ARCH.to_string(),
+            cpu_info,
+            cpu_count: cpus.len(),
         }
     }
 }
