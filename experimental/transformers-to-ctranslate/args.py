3b1fcf25d48d5c9e086193871bda2dd5d112f7bd
vodkaslime <646329483@qq.com>
2023-08-17 11:56:15 +0800

    chore: resolve comments

--- experimental/transformers-to-ctranslate/args.py
+++ experimental/transformers-to-ctranslate/args.py
@@ -14,23 +14,6 @@ def make_parser():
         ),
     )
     parser.add_argument(
-        "--activation_scales",
-        help=(
-            "Path to the pre-computed activation scales. Models may "
-            "use them to rescale some weights to smooth the intermediate activations "
-            "and improve the quantization accuracy. See "
-            "https://github.com/mit-han-lab/smoothquant."
-        ),
-    )
-    parser.add_argument(
-        "--copy_files",
-        nargs="+",
-        help=(
-            "List of filenames to copy from the Hugging Face model to the converted "
-            "model directory."
-        ),
-    )
-    parser.add_argument(
         "--revision",
         help="Revision of the model to download from the Hugging Face Hub.",
     )
@@ -43,12 +26,6 @@ def make_parser():
         "--trust_remote_code",
         action="store_true",
         help="Allow converting models using custom code.",
-    )
-
-    parser.add_argument(
-        "--tokenizer_name",
-        required=True,
-        help="Tokenizer name of the model.",
     )
 
     Converter.declare_arguments(parser)
--- experimental/transformers-to-ctranslate/main.py
+++ experimental/transformers-to-ctranslate/main.py
@@ -1,13 +1,35 @@
-import os
 from args import make_parser
+import json
+import os
 
 from dotenv import load_dotenv
 from ctranslate2.converters.transformers import TransformersConverter
 from huggingface_hub import snapshot_download
 from transformers.convert_slow_tokenizers_checkpoints_to_fast import convert_slow_checkpoint_to_fast
-from huggingface_hub import login, HfApi
 
-if __name__ == "__main__":
+class InvalidConvertionException(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+def convert_tokenizer():
+    # Infer tokenizer name
+    if not os.path.isfile('tokenizer_config.json'):
+        raise InvalidConvertionException("cannot find tokenizer_config.json, unable to infer tokenizer name")
+
+    data = {}
+    with open('tokenizer_config.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    tokenizer_name = data["tokenizer_class"]
+
+    # Start to convert
+    convert_slow_checkpoint_to_fast(
+        tokenizer_name=tokenizer_name,
+        checkpoint_name='./',
+        dump_path='./',
+        force_download=False,
+    )
+
+def main():
     # Set up args
     parser = make_parser()
 
@@ -20,18 +42,15 @@ if __name__ == "__main__":
     model_path = snapshot_download(
         repo_id=args.model,
         cache_dir=args.output_dir,
-        force_download=True
+        force_download=False,
     )
     convert_output_dir = os.path.join(model_path, "ctranslate2")
 
     # Convert model into ctranslate
     converter = TransformersConverter(
-        args.model,
-        activation_scales=args.activation_scales,
-        copy_files=args.copy_files,
+        model_name_or_path=args.model,
         load_as_float16=args.quantization in ("float16", "int8_float16"),
         revision=args.revision,
-        low_cpu_mem_usage=args.low_cpu_mem_usage,
         trust_remote_code=args.trust_remote_code,
     )
     converter.convert(
@@ -42,20 +61,11 @@ if __name__ == "__main__":
     )
 
     # Convert model with fast tokenizer
-    os.chdir(model_path)
-    convert_slow_checkpoint_to_fast(
-        tokenizer_name=args.tokenizer_name,
-        checkpoint_name='./',
-        dump_path='./',
-        force_download=False,
-    )
+    if not os.path.exists(os.path.join(model_path, "tokenizer2.json")):
+        os.chdir(model_path)
+        convert_tokenizer()
+    else:
+        print("found tokenizer.json, skipping tokenizer conversion")
 
-    # Upload folder to huggingface
-    token = os.getenv("HF_TOKEN")
-    login(token)
-    api = HfApi()
-    api.upload_folder(
-        folder_path=model_path,
-        repo_id=args.model,
-        repo_type='model',
-    )
+if __name__ == "__main__":
+    main()
