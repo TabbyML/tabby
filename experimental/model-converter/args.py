b4381acfbf2afa6cc5d6e39d59449a28127a9893
vodkaslime <646329483@qq.com>
2023-08-17 22:29:20 +0800

    feat: add model-converter [TAB-153] (#357)
    * feat: add transformers-to-ctranslate
    
    * chore: resolve comments
    
    * chore: fix
    
    * chore: remove dotenv
    
    * chore: resolve comments
    
    * chore: lint
    
    * chore: change dir name

---
+++ experimental/model-converter/args.py
@@ -0,0 +1,27 @@
+import argparse
+
+
+def make_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--model",
+        required=True,
+        help=(
+            "Name of the pretrained model to download, "
+            "or path to a directory containing the pretrained model."
+        ),
+    )
+    parser.add_argument("--output_dir", required=True, help="Output model directory.")
+    parser.add_argument(
+        "--inference_mode",
+        required=True,
+        choices=["causallm", "seq2seq"],
+        help="Model inference mode. ",
+    )
+    parser.add_argument(
+        "--prompt_template", default=None, help="prompt template for fim"
+    )
+
+    return parser
