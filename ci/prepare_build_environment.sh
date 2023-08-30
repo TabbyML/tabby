054aefaf15690cda7cfdf9f1eed88e2fc725e70c
Meng Zhang <meng@tabbyml.com>
2023-08-30 18:45:05 +0800

    chore: add linux static build (#379)
    * chore: add linux static build
    
    * add touch
    
    * update build env
    
    * add sudo
    
    * fix: protobuf ubuntu target

--- ci/prepare_build_environment.sh
+++ ci/prepare_build_environment.sh
@@ -3,3 +3,7 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   brew install protobuf
 fi
+
+if [[ "$OSTYPE" == "linux"* ]]; then
+  sudo apt-get -y install protobuf-compiler
+fi
--- crates/tabby/build.rs
+++ crates/tabby/build.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // touch
     EmitBuilder::builder().all_build().all_git().emit()?;
     Ok(())
 }
