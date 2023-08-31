c44a9c719576df0aa3c18aefb52db4cabcd01316
Meng Zhang <meng@tabbyml.com>
2023-08-31 09:06:36 +0800

    fix: correct git_describe in /health (#383)
    * fix: add missing Version component in OpenAPI definition
    
    * fix: allow tag / dirty in git describe

--- crates/tabby/build.rs
+++ crates/tabby/build.rs
@@ -4,6 +4,10 @@ use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // touch
-    EmitBuilder::builder().all_build().all_git().emit()?;
+    EmitBuilder::builder()
+        .all_build()
+        .all_git()
+        .git_describe(true, true, None)
+        .emit()?;
     Ok(())
 }
