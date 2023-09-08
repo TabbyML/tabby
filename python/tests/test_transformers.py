3e3cae2c0771998c25e8c1d8892fd68d45728518
Guillaume Klein <guillaumekln@users.noreply.github.com>
2023-09-08 15:40:42 +0200

    Merge BERT and DistilBERT test functions (#1461)

--- python/tests/test_transformers.py
+++ python/tests/test_transformers.py
@@ -276,63 +276,23 @@ def test_transformers_marianmt_disable_unk(
 @pytest.mark.parametrize(
     "model_name",
     [
+        "bert-base-uncased",
         "distilbert-base-uncased",
         "distilbert-base-cased-distilled-squad",
         "typeform/distilbert-base-uncased-mnli",
     ],
 )
-def test_transformers_distilbert(clear_transformers_cache, tmp_dir, device, model_name):
+def test_transformers_encoder(clear_transformers_cache, tmp_dir, device, model_name):
     import torch
     import transformers
 
     text = ["Hello world!", "Hello, my dog is cute"]
 
-    model = transformers.DistilBertModel.from_pretrained(model_name)
-    tokenizer = transformers.DistilBertTokenizer.from_pretrained(model_name)
-
-    inputs = tokenizer(text, return_tensors="pt", padding=True)
-
-    inputs.to(device)
-    model.to(device)
-
-    with torch.no_grad():
-        outputs = model(**inputs)
-
-    mask = inputs.attention_mask.unsqueeze(-1).cpu().numpy()
-    ref_last_hidden_state = outputs.last_hidden_state.cpu().numpy()
-
-    converter = ctranslate2.converters.TransformersConverter(model_name)
-    output_dir = str(tmp_dir.join("ctranslate2_model"))
-    output_dir = converter.convert(output_dir)
-
-    encoder = ctranslate2.Encoder(output_dir, device=device)
-
-    ids = [tokenizer(t).input_ids for t in text]
-    outputs = encoder.forward_batch(ids)
-
-    last_hidden_state = _to_numpy(outputs.last_hidden_state, device)
-    assert last_hidden_state.shape == ref_last_hidden_state.shape
-
-    last_hidden_state *= mask
-    ref_last_hidden_state *= mask
-    np.testing.assert_array_almost_equal(
-        last_hidden_state, ref_last_hidden_state, decimal=5
-    )
-
-
-@test_utils.only_on_linux
-@test_utils.on_available_devices
-def test_transformers_bert(clear_transformers_cache, tmp_dir, device):
-    import torch
-    import transformers
-
-    text = ["Hello world!", "Hello, my dog is cute"]
-
-    model_name = "bert-base-uncased"
-    model = transformers.BertModel.from_pretrained(model_name)
+    model = transformers.AutoModel.from_pretrained(model_name)
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
 
     inputs = tokenizer(text, return_tensors="pt", padding=True)
+
     inputs.to(device)
     model.to(device)
 
@@ -341,7 +301,11 @@ def test_transformers_bert(clear_transformers_cache, tmp_dir, device):
 
     mask = inputs.attention_mask.unsqueeze(-1).cpu().numpy()
     ref_last_hidden_state = outputs.last_hidden_state.cpu().numpy()
-    ref_pooler_output = outputs.pooler_output.cpu().numpy()
+    ref_pooler_output = (
+        outputs.pooler_output.cpu().numpy()
+        if hasattr(outputs, "pooler_output")
+        else None
+    )
 
     converter = ctranslate2.converters.TransformersConverter(model_name)
     output_dir = str(tmp_dir.join("ctranslate2_model"))
@@ -361,9 +325,12 @@ def test_transformers_bert(clear_transformers_cache, tmp_dir, device):
         last_hidden_state, ref_last_hidden_state, decimal=5
     )
 
-    pooler_output = _to_numpy(outputs.pooler_output, device)
-    assert pooler_output.shape == ref_pooler_output.shape
-    np.testing.assert_array_almost_equal(pooler_output, ref_pooler_output, decimal=5)
+    if ref_pooler_output is not None:
+        pooler_output = _to_numpy(outputs.pooler_output, device)
+        assert pooler_output.shape == ref_pooler_output.shape
+        np.testing.assert_array_almost_equal(
+            pooler_output, ref_pooler_output, decimal=5
+        )
 
 
 def _to_numpy(storage, device):
