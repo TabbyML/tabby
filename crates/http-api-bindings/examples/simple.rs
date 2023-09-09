d76ff293b59b9354cf7026d5b49cca55942fb526
Meng Zhang <meng@tabbyml.com>
2023-09-09 11:03:49 +0800

    add examples/simple.rs

---
+++ crates/http-api-bindings/examples/simple.rs
@@ -0,0 +1,15 @@
+use std::env;
+
+use http_api_bindings::vertex_ai::VertexAIEngine;
+use tabby_inference::{TextGeneration, TextGenerationOptions, TextGenerationOptionsBuilder};
+
+#[tokio::main]
+async fn main() {
+    let api_endpoint = env::var("API_ENDPOINT").expect("API_ENDPOINT not set");
+    let authorization = env::var("AUTHORIZATION").expect("AUTHORIZATION not set");
+    let engine = VertexAIEngine::create(&api_endpoint, &authorization);
+
+    let options = TextGenerationOptionsBuilder::default().build().unwrap();
+    let text = engine.generate("def fib(n)", options).await;
+    println!("text: {}", text);
+}
