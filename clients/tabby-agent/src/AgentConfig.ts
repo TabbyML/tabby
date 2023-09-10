12a37e2330b11a72814634340229dc532fe73638
Zhiming Ma <codes.icy@gmail.com>
2023-09-10 10:25:23 +0800

    refactor(agent): Move data path from ~/.tabby/agent to ~/.tabby-client/agent. Add config.toml template. (#420)

--- clients/tabby-agent/src/AgentConfig.ts
+++ clients/tabby-agent/src/AgentConfig.ts
@@ -66,6 +66,49 @@ export const defaultAgentConfig: AgentConfig = {
   },
 };
 
+const configTomlTemplate = `## Tabby agent configuration file
+
+## You can uncomment any block to enable settings.
+## Configurations in this file has lower priority than in IDE settings.
+
+## Server
+## You can set the server endpoint and request timeout here.
+# [server]
+# endpoint = "http://localhost:8080" # http or https URL
+# requestTimeout = 30000 # ms
+
+## You can add custom request headers, e.g. for authentication.
+# [server.requestHeaders]
+# Authorization = "Bearer eyJhbGciOiJ..........."
+
+## Completion
+## You can set the prompt context to send to the server for completion.
+# [completion.prompt]
+# maxPrefixLines = 20
+# maxSuffixLines = 20
+
+## You can set the debounce mode for auto completion requests when typing.
+# [completion.debounce]
+# mode = "adaptive" # or "fixed"
+# interval = 250 # ms, only used when mode is "fixed"
+
+## You can set the timeout for completion requests.
+# [completion.timeout]
+# auto = 5000 # ms, for auto completion when typing
+# manually = 30000 # ms, for manually triggered completion
+
+## Logs
+## You can set the log level here. The log file is located at ~/.tabby-client/agent/logs/.
+# [logs]
+# level = "silent" # or "error" or "debug"
+
+## Anonymous usage tracking
+## You can disable anonymous usage tracking here.
+# [anonymousUsageTracking]
+# disable = false # set to true to disable
+
+`;
+
 export const userAgentConfig = isBrowser
   ? null
   : (() => {
@@ -95,7 +138,19 @@ export const userAgentConfig = isBrowser
             this.data = toml.parse(fileContent);
             super.emit("updated", this.data);
           } catch (error) {
-            this.logger.error({ error }, "Failed to load config file");
+            if (error.code === "ENOENT") {
+              await this.createTemplate();
+            } else {
+              this.logger.error({ error }, "Failed to load config file");
+            }
+          }
+        }
+
+        async createTemplate() {
+          try {
+            await fs.outputFile(this.filepath, configTomlTemplate);
+          } catch (error) {
+            this.logger.error({ error }, "Failed to create config template file");
           }
         }
 
@@ -108,6 +163,6 @@ export const userAgentConfig = isBrowser
         }
       }
 
-      const configFile = require("path").join(require("os").homedir(), ".tabby", "agent", "config.toml");
+      const configFile = require("path").join(require("os").homedir(), ".tabby-client", "agent", "config.toml");
       return new ConfigFile(configFile);
     })();
