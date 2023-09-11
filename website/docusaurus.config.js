c3d5e9aa3070826ef0f067291d92138b619558ac
Meng Zhang <meng@tabbyml.com>
2023-09-11 12:33:33 +0800

    docs: update domain name

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://tabbyml.github.io',
+  url: 'https://tabby.tabbyml.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tabby',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
