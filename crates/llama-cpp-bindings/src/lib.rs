76e929e5dad51007c99c36d814501f99087dc0ad
Meng Zhang <meng@tabbyml.com>
2023-09-27 12:03:29 -0700

    support streaming in llama.cpp

--- crates/llama-cpp-bindings/src/lib.rs
+++ crates/llama-cpp-bindings/src/lib.rs
@@ -1,12 +1,17 @@
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
+use async_stream::stream;
 use async_trait::async_trait;
 use derive_builder::Builder;
 use ffi::create_engine;
+use futures::{
+    lock::Mutex,
+    pin_mut,
+    stream::{BoxStream, StreamExt},
+};
 use stop_words::StopWords;
 use tabby_inference::{TextGeneration, TextGenerationOptions};
 use tokenizers::tokenizer::Tokenizer;
-use tokio_util::sync::CancellationToken;
 
 #[cxx::bridge(namespace = "llama")]
 mod ffi {
@@ -35,7 +40,7 @@ pub struct LlamaEngineOptions {
 }
 
 pub struct LlamaEngine {
-    engine: Arc<Mutex<cxx::SharedPtr<ffi::TextInferenceEngine>>>,
+    engine: Mutex<cxx::SharedPtr<ffi::TextInferenceEngine>>,
     tokenizer: Arc<Tokenizer>,
     stop_words: StopWords,
 }
@@ -43,7 +48,7 @@ pub struct LlamaEngine {
 impl LlamaEngine {
     pub fn create(options: LlamaEngineOptions) -> Self {
         LlamaEngine {
-            engine: Arc::new(Mutex::new(create_engine(&options.model_path))),
+            engine: Mutex::new(create_engine(&options.model_path)),
             tokenizer: Arc::new(Tokenizer::from_file(&options.tokenizer_path).unwrap()),
             stop_words: StopWords::default(),
         }
@@ -53,51 +58,56 @@ impl LlamaEngine {
 #[async_trait]
 impl TextGeneration for LlamaEngine {
     async fn generate(&self, prompt: &str, options: TextGenerationOptions) -> String {
-        let cancel = CancellationToken::new();
-        let cancel_for_inference = cancel.clone();
-        let _guard = cancel.drop_guard();
+        let s = self.generate_stream(prompt, options).await;
+        pin_mut!(s);
 
+        let mut text = "".to_owned();
+        while let Some(value) = s.next().await {
+            text += &value;
+        }
+
+        text
+    }
+
+    async fn generate_stream(
+        &self,
+        prompt: &str,
+        options: TextGenerationOptions,
+    ) -> BoxStream<String> {
         let prompt = prompt.to_owned();
-        let engine = self.engine.clone();
         let mut stop_condition = self
             .stop_words
             .create_condition(self.tokenizer.clone(), options.stop_words);
 
-        let output_ids = tokio::task::spawn_blocking(move || {
-            let engine = engine.lock().unwrap();
+        let s = stream! {
+            let engine = self.engine.lock().await;
             let eos_token = engine.eos_token();
 
             let mut next_token_id = engine.start(&prompt, options.max_input_length);
             if next_token_id == eos_token {
-                return Vec::new();
-            }
+                yield "".to_owned();
+            } else {
+                let mut n_remains = options.max_decoding_length - 1;
 
-            let mut n_remains = options.max_decoding_length - 1;
-            let mut output_ids = vec![next_token_id];
+                while n_remains > 0 {
+                    next_token_id = engine.step(next_token_id);
+                    if next_token_id == eos_token {
+                        break;
+                    }
 
-            while n_remains > 0 {
-                if cancel_for_inference.is_cancelled() {
-                    // The token was cancelled
-                    break;
+                    if stop_condition.next_token(next_token_id) {
+                        break;
+                    }
+
+                    let text = self.tokenizer.decode(&[next_token_id], true).unwrap();
+                    yield text;
+                    n_remains -= 1;
                 }
-
-                next_token_id = engine.step(next_token_id);
-                if next_token_id == eos_token {
-                    break;
-                }
-
-                if stop_condition.next_token(next_token_id) {
-                    break;
-                }
-                output_ids.push(next_token_id);
-                n_remains -= 1;
             }
 
             engine.end();
-            output_ids
-        })
-        .await
-        .expect("Inference failed");
-        self.tokenizer.decode(&output_ids, true).unwrap()
+        };
+
+        Box::pin(s)
     }
 }
