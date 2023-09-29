bfee87487ca95c01d3bbe18b205d45febf77fdfa
Meng Zhang <meng@tabbyml.com>
2023-09-29 14:49:28 -0700

    update

--- clients/tabby-playground/app/layout.tsx
+++ clients/tabby-playground/app/layout.tsx
@@ -19,11 +19,6 @@ export const metadata: Metadata = {
     { media: '(prefers-color-scheme: light)', color: 'white' },
     { media: '(prefers-color-scheme: dark)', color: 'black' }
   ],
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png'
-  }
 }
 
 interface RootLayoutProps {
--- clients/tabby-playground/next.config.js
+++ clients/tabby-playground/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: '/playground',
   output: "export",
   images: {
     unoptimized: true
