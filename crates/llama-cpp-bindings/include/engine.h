a7ec501db1f59403c906ae7a48adcdbf3144a186
Meng Zhang <meng@tabbyml.com>
2023-09-30 07:29:57 -0700

    refactor: cleanup llama cpp implementations to fix warnings

--- crates/llama-cpp-bindings/include/engine.h
+++ crates/llama-cpp-bindings/include/engine.h
@@ -9,12 +9,12 @@ class TextInferenceEngine {
  public:
   virtual ~TextInferenceEngine();
 
-  virtual void start(rust::Slice<const uint32_t> input_token_ids) const = 0;
-  virtual uint32_t step() const = 0;
-  virtual void end() const = 0;
+  virtual void start(rust::Slice<const uint32_t> input_token_ids) = 0;
+  virtual uint32_t step() = 0;
+  virtual void end() = 0;
 
   virtual uint32_t eos_token() const = 0;
 };
 
-std::shared_ptr<TextInferenceEngine> create_engine(rust::Str model_path);
+std::unique_ptr<TextInferenceEngine> create_engine(rust::Str model_path);
 }  // namespace
