3600d9e399ec8a518f948d04b59cc32941b4e68d
Meng Zhang <meng@tabbyml.com>
2023-10-03 20:28:52 -0700

    chore: update sync.sh models

--- experimental/copy-to-modelscope/sync.sh
+++ experimental/copy-to-modelscope/sync.sh
@@ -2,15 +2,14 @@
 
 set -ex
 
-# TabbyML/StarCoder-3B
 
 MODELS=$(cat <<EOF
+TabbyML/StarCoder-1B
+TabbyML/StarCoder-3B
 TabbyML/StarCoder-7B
-TabbyML/WizardCoder-1B
-TabbyML/WizardCoder-3B
 TabbyML/CodeLlama-7B
 TabbyML/CodeLlama-13B
-TabbyML/StarCoder-1B
+TabbyML/WizardCoder-3B
 EOF
 )
 
