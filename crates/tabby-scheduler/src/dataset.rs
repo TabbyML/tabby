9f91fe2ced6156413a7e92a3fdac7483ec2102ec
Meng Zhang <meng@tabbyml.com>
2023-10-04 15:48:53 -0700

    update

--- crates/tabby-scheduler/src/dataset.rs
+++ crates/tabby-scheduler/src/dataset.rs
@@ -41,7 +41,7 @@ impl RepositoryExt for Repository {
                 .to_owned();
             if let Ok(file_content) = read_to_string(entry.path()) {
                 info!("Building {:?}", relative_path);
-                let doc = SourceFile {
+                let source_file = SourceFile {
                     git_url: self.git_url.clone(),
                     filepath: relative_path.display().to_string(),
                     max_line_length: metrics::max_line_length(&file_content),
@@ -51,7 +51,7 @@ impl RepositoryExt for Repository {
                     language,
                     content: file_content,
                 };
-                writer.write_json_lines([doc])?;
+                writer.write_json_lines([source_file])?;
             } else {
                 error!("Cannot read {:?}", relative_path);
             }
