bb8a2b58b34c591779b851922accf5a90f17594f
Meng Zhang <meng@tabbyml.com>
2023-10-06 11:40:12 -0700

    improve debugger

--- experimental/scheduler/search.py
+++ experimental/scheduler/search.py
@@ -2,20 +2,6 @@ import requests
 import streamlit as st
 from typing import NamedTuple
 
-class Doc(NamedTuple):
-    name: str
-    body: str
-    score: float
-
-    @staticmethod
-    def from_json(json: dict):
-        doc = json["doc"]
-        return Doc(
-            name=doc["name"][0],
-            body=doc["body"][0],
-            score=json["score"]
-        )
-
 # force wide mode
 st.set_page_config(layout="wide")
 
@@ -25,6 +11,4 @@ if query:
     r = requests.get("http://localhost:8080/v1beta/search", params=dict(q=query))
     hits = r.json()["hits"]
     for x in hits:
-        doc = Doc.from_json(x)
-        st.write(doc.name + " : " + str(doc.score))
-        st.code(doc.body)
+        st.write(x)
