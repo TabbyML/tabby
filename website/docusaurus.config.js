8390fff42324877b8e8601fdd3cad7cef8e2fc8b
Meng Zhang <meng@tabbyml.com>
2023-10-13 23:05:50 -0700

    docs: add instruction on creating a Tabby instance with repository context (#552)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -141,6 +141,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ['toml', 'rust'],
       },
       colorMode: {
         defaultMode: "light",
