8d49b251c964a437b833def461a9cb39b220d3cf
sensinsane <34210817+sensinsane@users.noreply.github.com>
2023-10-17 13:17:42 +0200

    fix: python.scm
    Tested with:
    
    ```# Import statements
    import os
    from math import sqrt
    
    # Function definition
    def say_hello(name):
        print(f"Hello, {name}!")
    
    # Async function definition
    async def fetch_data():
        print("Fetching data...")
    
    # Class definition
    class MyClass:
        def __init__(self, value):
            self.value = value
    
    # Control structures
    if True:
        print("This is true")
    elif False:
        print("This won't happen")
    else:
        print("This also won't happen")
    
    try:
        x = 1 / 0
    except ZeroDivisionError:
        print("Cannot divide by zero")
    finally:
        print("Finally block")
    
    # For loop
    for i in range(3):
        print(i)
    
    # While loop
    count = 0
    while count < 3:
        print(count)
        count += 1
    
    # With statement
    with open('test.txt', 'w') as f:
        f.write('Hello, world!')
    
    # Yield expression
    def my_gen():
        yield 1
    
    # Function call
    say_hello("John")
    
    # Return and raise statements
    def my_func():
        return None
    
    def raise_error():
        raise ValueError("This is an error")```

--- crates/tabby-scheduler/queries/python.scm
+++ crates/tabby-scheduler/queries/python.scm
@@ -1,32 +1,49 @@
 (function_definition
   name: (identifier) @name) @definition.function
-
 (class_definition
   name: (identifier) @name) @definition.class
 
 (import_statement
-  module: (identifier) @name) @definition.module
+  (dotted_name (identifier) @name)) @definition.module
 
-(import_from_statement
-  module: (identifier) @name) @definition.module
+(assignment
+  left: (_) @assignment.left
+  right: (_) @assignment.right) @assignment
 
-(if_statement) @control.if
-(elif_statement) @control.elif
-(else_statement) @control.else
+(if_statement
+  condition: (_) @if-condition
+  consequence: (_) @if-consequence)
+
+(elif_clause
+  condition: (_) @elif-condition
+  consequence: (_) @elif-consequence)
+
+(else_clause
+  body: (_) @else-body)
+
 (try_statement) @control.try
-(except_clause) @control.except
-(finally_clause) @control.finally
-(for_statement) @control.for
-(while_statement) @control.while
-(return_statement) @control.return
-(raise_statement) @control.raise
 
-(async_function_definition
-  name: (identifier) @name) @definition.function.async
+(except_clause) @control.except
+
+(finally_clause) @control.finally
+
+(for_statement) @control.for
+
+(while_statement) @control.while
+
+(return_statement) @control.return
+
+(raise_statement) @control.raise
 
 (with_statement) @control.with
 
-(yield_expression) @expression.yield
+(yield) @expression.yield
+
+(pass_statement) @control.pass
+
+(break_statement) @control.break
+
+(continue_statement) @control.continue
 
 (call
   function: (identifier) @name) @reference.call
@@ -36,3 +53,5 @@
 
 (#set-adjacent! @comment @definition.function)
 (#set-adjacent! @comment @definition.class)
+
+(await) @expression.await
