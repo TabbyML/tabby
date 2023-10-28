9309e0314f00283ca5f4dd0115b347c6adc8f10a
Meng Zhang <meng@tabbyml.com>
2023-10-27 21:25:45 -0700

    fix: fix docker build

--- crates/llama-cpp-bindings/build.rs
+++ crates/llama-cpp-bindings/build.rs
@@ -3,6 +3,8 @@ use cmake::Config;
 fn main() {
     println!("cargo:rerun-if-changed=cc/*.h");
     println!("cargo:rerun-if-changed=cc/*.cc");
+    println!("cargo:rustc-link-lib=llama");
+    println!("cargo:rustc-link-lib=ggml_static");
 
     let mut config = Config::new("llama.cpp");
     if cfg!(target_os = "macos") {
@@ -15,7 +17,6 @@ fn main() {
     if cfg!(feature = "cuda") {
         config.define("LLAMA_CUBLAS", "ON");
         println!("cargo:rustc-link-search=native=/usr/local/cuda/lib64");
-        println!("cargo:rustc-link-search=native=/opt/cuda/lib64");
         println!("cargo:rustc-link-lib=cudart");
         println!("cargo:rustc-link-lib=culibos");
         println!("cargo:rustc-link-lib=cublas");
@@ -24,8 +25,6 @@ fn main() {
 
     let dst = config.build();
     println!("cargo:rustc-link-search=native={}/build", dst.display());
-    println!("cargo:rustc-link-lib=llama");
-    println!("cargo:rustc-link-lib=ggml_static");
 
     cxx_build::bridge("src/lib.rs")
         .file("src/engine.cc")
