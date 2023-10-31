665c0334883094096941ae7bbc92264bf76f4214
yan91083 <zhyan910@gmail.com>
2023-10-31 10:52:25 -0700

    update analyze next line script

--- experimental/eval/analyze.py
+++ experimental/eval/analyze.py
@@ -1,21 +1,29 @@
 import json
+import sys
 
-count = 0
+def analyze(language, input_file):
+    count = 0
+    output_file = "result_" + input_file[7:]
 
-with open("./result.jsonl", "w") as fout:
-    with open("./output_starcode_1B_rg1_bm25.jsonl") as fin:
-        for line in fin:
-            obj = json.loads(line)
-            groundtruth = obj["label"]
-            prediction = obj["prediction"]
-            first_line_groundtruth = groundtruth.split("\n")[0]
-            first_line_prediction = prediction.split("\n")[0]
-            if first_line_groundtruth == first_line_prediction:
-                match = 1
-                count = count + 1
-            else:
-                match = 0
-            json.dump(dict(groundtruth=groundtruth, prediction=prediction, first_line_groundtruth=first_line_groundtruth, first_line_prediction=first_line_prediction, match=match), fout)
-            fout.write("\n")
+    with open("./"+ language + "/" + output_file, "w") as fout:
+        with open("./" + language + "/" + input_file, "r") as fin:
+            for line in fin:
+                obj = json.loads(line)
+                prompt = obj["prompt"]
+                groundtruth = obj["label"]
+                prediction = obj["prediction"]
+                first_line_groundtruth = groundtruth.split("\n")[0].strip()
+                first_line_prediction = prediction.split("\n")[0].strip()
 
-print(str(count) + "records matched!")
+                if first_line_groundtruth == first_line_prediction:
+                    match = 1
+                    count = count + 1
+                else:
+                    match = 0
+                json.dump(dict(groundtruth=groundtruth, prediction=prediction, first_line_groundtruth=first_line_groundtruth, first_line_prediction=first_line_prediction, match=match), fout)
+                fout.write("\n")
+
+    print(str(count) + "records matched!")
+
+if __name__ == "__main__":
+    analyze(sys.argv[1], sys.argv[2])
