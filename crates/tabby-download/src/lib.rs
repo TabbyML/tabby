b4fe2496368282d50541fa5267386a3acab4bc5f
Meng Zhang <meng@tabbyml.com>
2023-11-04 19:38:06 -0700

    feat: support downloading resume (#700)

--- crates/tabby-download/src/lib.rs
+++ crates/tabby-download/src/lib.rs
@@ -1,8 +1,7 @@
-use std::{cmp, fs, io::Write, path::Path};
+use std::{fs, path::Path};
 
+use aim::bar::WrappedBar;
 use anyhow::{anyhow, Result};
-use futures_util::StreamExt;
-use indicatif::{ProgressBar, ProgressStyle};
 use tabby_common::registry::{parse_model_id, ModelRegistry};
 use tokio_retry::{
     strategy::{jitter, ExponentialBackoff},
@@ -51,40 +50,17 @@ async fn download_model_impl(
 }
 
 async fn download_file(url: &str, path: &Path) -> Result<()> {
-    fs::create_dir_all(path.parent().unwrap())?;
+    let dir = path.parent().unwrap();
+    fs::create_dir_all(dir)?;
 
-    // Reqwest setup
-    let res = reqwest::get(url).await?;
+    let filename = path.to_str().unwrap();
+    let intermediate_filename = filename.to_owned() + ".tmp";
 
-    if !res.status().is_success() {
-        return Err(anyhow!(format!("Invalid url: {}", url)));
-    }
+    let mut bar = WrappedBar::new(0, url, false);
 
-    let total_size = res
-        .content_length()
-        .ok_or(anyhow!("No content length in headers"))?;
+    aim::https::HTTPSHandler::get(url, &intermediate_filename, &mut bar, "").await?;
 
-    // Indicatif setup
-    let pb = ProgressBar::new(total_size);
-    pb.set_style(ProgressStyle::default_bar()
-        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")?
-        .progress_chars("#>-"));
-    pb.set_message(format!("Downloading {}", path.display()));
-
-    // download chunks
-    let mut file = fs::File::create(path)?;
-    let mut downloaded: u64 = 0;
-    let mut stream = res.bytes_stream();
-
-    while let Some(item) = stream.next().await {
-        let chunk = item?;
-        file.write_all(&chunk)?;
-        let new = cmp::min(downloaded + (chunk.len() as u64), total_size);
-        downloaded = new;
-        pb.set_position(new);
-    }
-
-    pb.finish_with_message(format!("Downloaded {}", path.display()));
+    fs::rename(intermediate_filename, filename)?;
     Ok(())
 }
 
