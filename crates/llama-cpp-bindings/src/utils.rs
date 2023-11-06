82a2d9019235eb6fc7ab1093fd61c108e0b7598a
Meng Zhang <meng@tabbyml.com>
2023-11-06 12:29:49 -0800

    fix: when there's an error happens in background inference loop, it should exit the process

---
+++ crates/llama-cpp-bindings/src/utils.rs
@@ -0,0 +1,16 @@
+#[macro_export]
+macro_rules! fatal {
+    ($msg:expr) => {
+        ({
+            tracing::error!($msg);
+            std::process::exit(1);
+        })
+    };
+
+    ($fmt:expr, $($arg:tt)*) => {
+        ({
+            tracing::error!($fmt, $($arg)*);
+            std::process::exit(1);
+        })
+    };
+}
