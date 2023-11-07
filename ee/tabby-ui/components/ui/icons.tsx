e7f874b8da1f155c49a58eea0d4d45615d00f389
aliang <1098486429@qq.com>
2023-11-08 05:29:07 +0800

    feat: adding an auto completion component in chat playground (#702)
    * feat: Adding an auto-completion component
    
    * Update ee/tabby-ui/components/prompt-form.tsx
    
    Co-authored-by: Meng Zhang <meng@tabbyml.com>
    
    * fix: prepare for submitting prompt
    
    * fix: Generate code block templates
    
    * fix: select completion and replace with doc name
    
    ---------
    
    Co-authored-by: Meng Zhang <meng@tabbyml.com>

--- ee/tabby-ui/components/ui/icons.tsx
+++ ee/tabby-ui/components/ui/icons.tsx
@@ -554,6 +554,24 @@ function IconNotice({ className, ...props }: React.ComponentProps<'svg'>) {
   )
 }
 
+function IconSymbolFunction({
+  className,
+  ...props
+}: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M13.51 4l-5-3h-1l-5 3-.49.86v6l.49.85 5 3h1l5-3 .49-.85v-6L13.51 4zm-6 9.56l-4.5-2.7V5.7l4.5 2.45v5.41zM3.27 4.7l4.74-2.84 4.74 2.84-4.74 2.59L3.27 4.7zm9.74 6.16l-4.5 2.7V8.15l4.5-2.45v5.16z" />
+    </svg>
+  )
+}
+
 export {
   IconEdit,
   IconNextChat,
@@ -583,5 +601,6 @@ export {
   IconExternalLink,
   IconChevronUpDown,
   IconSlack,
-  IconNotice
+  IconNotice,
+  IconSymbolFunction
 }
