5454be1ca324a9262d32ca7ca46ab59fbcb4a87b
yan91083 <zhyan910@gmail.com>
2023-11-06 16:06:59 -0800

    add new predict script for incremental running & get model_id from environment variable, new analyze file, all files for WizardCoder-3B model

--- experimental/eval/analyze.py
+++ experimental/eval/analyze.py
@@ -3,15 +3,17 @@ import sys
 
 def analyze(model, language, file):
     count = 0
+    empty = 0
+
     input_file = "./data/" + model + "/" + language + "/" + file
-    output_file = "./data/" + model + "/" + language + "/result_" + file[7:]
+    output_file = "./data/" + model + "/" + language + "/result_" + file
 
     with open(output_file, "w") as fout:
         with open(input_file, "r") as fin:
             for line in fin:
                 obj = json.loads(line)
                 prompt = obj["prompt"]
-                groundtruth = obj["label"]
+                groundtruth = obj["groundtruth"]
                 prediction = obj["prediction"]
                 first_line_groundtruth = groundtruth.split("\n")[0]
                 first_line_prediction = prediction.split("\n")[0]
@@ -21,6 +23,10 @@ def analyze(model, language, file):
                     count = count + 1
                 else:
                     match = 0
+
+                if prediction == "":
+                    empty += 1
+
                 json.dump(dict(groundtruth=groundtruth, prediction=prediction, first_line_groundtruth=first_line_groundtruth, first_line_prediction=first_line_prediction, match=match), fout)
                 fout.write("\n")
 
