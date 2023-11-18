6cf7bae4d9952f818317c317fb4e14b82468e2a7
Zhiming Ma <codes.icy@gmail.com>
2023-11-18 15:36:05 +0800

    feat(intellij): Add dont-show-again for warnings. Add more online help links.

---
+++ clients/intellij/src/main/kotlin/com/tabbyml/intellijtabby/actions/OpenOnlineDocumentation.kt
@@ -0,0 +1,11 @@
+package com.tabbyml.intellijtabby.actions
+
+import com.intellij.ide.BrowserUtil
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class OpenOnlineDocumentation: AnAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+    BrowserUtil.browse("https://tabby.tabbyml.com/")
+  }
+}
