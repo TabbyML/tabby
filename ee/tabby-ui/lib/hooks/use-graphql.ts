298ed2097ad16d3cb1bb6c0f44b6ab6f1e7f67fd
liangfung <1098486429@qq.com>
2023-11-22 01:26:19 +0800

    fix: remove useless parameter

--- ee/tabby-ui/lib/hooks/use-graphql.ts
+++ ee/tabby-ui/lib/hooks/use-graphql.ts
@@ -2,26 +2,16 @@ import useSWR, { SWRResponse, SWRConfiguration } from 'swr'
 import { request } from '@/lib/tabby-gql-client'
 import { Variables } from 'graphql-request'
 import { TypedDocumentNode } from '@graphql-typed-document-node/core'
-import { ASTNode, Kind, OperationDefinitionNode } from 'graphql'
-
-const isOperationDefinition = (def: ASTNode): def is OperationDefinitionNode =>
-  def.kind === Kind.OPERATION_DEFINITION
 
 function useGraphQL<TResult, TVariables extends Variables | undefined>(
   document: TypedDocumentNode<TResult, TVariables>,
   variables?: TVariables,
-  options?: SWRConfiguration<TResult>
+  swrConfiguration?: SWRConfiguration<TResult>
 ): SWRResponse<TResult> {
   return useSWR(
-    [
-      document.definitions.find(isOperationDefinition)?.name?.value,
-      document,
-      variables
-    ],
-    ([_key, document, variables]) => {
-      return request({ document, variables })
-    },
-    options
+    [document, variables],
+    ([document, variables]) => request({ document, variables }),
+    swrConfiguration
   )
 }
 
