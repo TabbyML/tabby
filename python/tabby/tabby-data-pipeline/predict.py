efc886c3a7e7b94fd24dde47af5e94b64e6034f7
yan91083 <zhyan910@gmail.com>
2023-11-24 12:17:27 -0800

    Revert "updating predict.py, adding tabby_python_client package"
    This reverts commit 8faf2365e561ddd815deb8ed27739f791eb1b1e6.

--- python/tabby/tabby-data-pipeline/predict.py
+++ python/tabby/tabby-data-pipeline/predict.py
@@ -6,19 +6,17 @@ import os
 
 
 import asyncio
-from collections import namedtuple
-from datetime import datetime
 
 GPU_CONFIG = gpu.A10G()
 
-MODEL_ID = os.environ.get("MODEL_ID")
+MODEL_ID = os.popen("cat /tmp/tabby_model_id").read().strip()
 LAUNCH_FLAGS = ["serve", "--model", MODEL_ID, "--port", "8000", "--device", "cuda"]
 
 
 def download_model():
     import subprocess
     import os
-    MODEL_ID = os.environ.get("MODEL_ID")
+    MODEL_ID = os.popen("cat /tmp/tabby_model_id").read().strip()
     print(f'MODEL_ID={MODEL_ID}')
     subprocess.run(
         [
@@ -35,13 +33,12 @@ image = (
         "tabbyml/tabby:0.5.5",
         add_python="3.11",
     )
-    .env({"MODEL_ID": os.environ.get("MODEL_ID")})
     .dockerfile_commands("ENTRYPOINT []")
-    .copy_local_dir(local_path='./modal/tabby_python_client/tabby_python_client', remote_path='/root/tabby_python_client')
     .pip_install(
-        "httpx",
+        "git+https://github.com/TabbyML/tabby.git#egg=tabby-python-client&subdirectory=experimental/eval/tabby-python-client",
         "pandas"
     )
+    .copy_local_file(local_path="/tmp/tabby_model_id", remote_path="/tmp/tabby_model_id")
     .run_function(download_model)
 )
 
@@ -65,9 +62,8 @@ class Model:
 
         my_env = os.environ.copy()
         my_env["TABBY_DISABLE_USAGE_COLLECTION"] = "1"
-        MODEL_ID = os.environ.get("MODEL_ID")
+        MODEL_ID = os.popen("cat /tmp/tabby_model_id").read().strip()
         print(f'MODEL_ID={MODEL_ID}')
-       
         LAUNCH_FLAGS = ["serve", "--model", MODEL_ID, "--port", "8000", "--device", "cuda"]
         self.launcher = subprocess.Popen(["/opt/tabby/bin/tabby"] + LAUNCH_FLAGS, env=my_env)
         self.client = Client("http://127.0.0.1:8000", timeout=240)
@@ -75,7 +71,7 @@ class Model:
         # Poll until webserver at 127.0.0.1:8000 accepts connections before running inputs.
         def webserver_ready():
             try:
-                socket.create_connection(("127.0.0.1", 8000), timeout=30).close()
+                socket.create_connection(("127.0.0.1", 8000), timeout=1).close()
                 return True
             except (socket.timeout, ConnectionRefusedError):
                 # Check if launcher webserving process has exited.
@@ -103,7 +99,7 @@ class Model:
         return resp.to_dict()
 
     @method()
-    async def complete(self, language: str, index: int, prompt: str, prediction: bool):
+    async def complete(self, language, crossfile_context, index, row):
         from tabby_python_client.api.v1 import completion
         from tabby_python_client.models import (
             CompletionRequest,
@@ -115,10 +111,15 @@ class Model:
         from tabby_python_client import errors
         import pandas as pd
 
-        # if prediction exists, just skip
-        if prediction:
+        if 'prediction' in row and not pd.isnull(row['prediction']):
             return None, None, None
-        
+
+        if crossfile_context:
+            prompt = row["crossfile_context"]["text"] + row["prompt"]
+        else:
+            prompt = row["prompt"]
+
+        groundtruth = row["groundtruth"]
        
         request = CompletionRequest(
             language=language, debug_options=DebugOptions(raw_prompt=prompt)
@@ -140,14 +141,10 @@ class Model:
         except Exception as e:
             return index, None, f"error type: {type(e)}"
 
-def write_log(log: str):
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open('./modal/log.txt', 'a') as f:
-        f.write(f"{now} : {log}")
-        f.write("\n")
+
 
 @stub.local_entrypoint()
-async def main(language: str):
+async def main(language, file):
     import json
     import pandas as pd
 
@@ -160,60 +157,45 @@ async def main(language: str):
     print(health_resp)
     assert(health_resp['model'] == MODEL_ID)
 
-    
-    for file in ['line_completion.jsonl', 'line_completion_rg1_bm25.jsonl', 'line_completion_oracle_bm25.jsonl']:
-  
-        whole_path_file = "./data/" + MODEL_ID.split("/")[-1] + "/" + language + "/" + file
-        objs = []
-        with open(whole_path_file) as fin:
-            for line in fin:
-                obj = json.loads(line)
-                if file == 'line_completion.jsonl':
-                    obj['raw_prompt'] = obj['prompt']
-                else:
-                    obj['raw_prompt'] = obj['crossfile_context']['text']
-                objs.append(obj)
+    whole_path_file = "./data/" + MODEL_ID.split("/")[-1] + "/" + language + "/" + file
 
-        df = pd.DataFrame(objs)
-        
-        write_log(f"model: {MODEL_ID}; language: {language}; file: {file}: length = {len(df)}")
+    if file == 'line_completion.jsonl':
+        crossfile_context = False
+    else:
+        crossfile_context = True
 
-        def chunker(seq, size):
-            return (seq[pos:pos + size] for pos in range(0, len(seq), size))
-        
-        def get_prediction(row):
-            if 'prediction' in row and not pd.isnull(row['prediction']):
-                return True
-            else:
-                return False
+    objs = []
+    with open(whole_path_file) as fin:
+        for line in fin:
+            obj = json.loads(line)
+            objs.append(obj)
 
-        skipped = 0
-        success = 0
-        error = 0
+    df = pd.DataFrame(objs)
 
-        for group in chunker(df, 30):
-            outputs = await asyncio.gather(*[model.complete.remote.aio(language, index, row['raw_prompt'], get_prediction(row)) for index, row in group.iterrows()])
+    outputs = await asyncio.gather(*[model.complete.remote.aio(language, crossfile_context, index, row) for index, row in df.iterrows()])
 
-            for index, prediction, error_msg in outputs:
-                if index is None:
-                    skipped += 1
-                elif prediction is not None:
-                    df.loc[index, 'prediction'] = prediction
-                    success += 1
-                else:
-                    df.loc[index, 'error'] = error_msg
-                    error += 1
+    skipped = 0
+    success = 0
+    error = 0
 
-        write_log(f"Skipped {skipped} rows, {success} rows with predictions, {error} rows with errors")
+    for index, prediction, error_msg in outputs:
+        if index is None:
+            skipped += 1
+        elif prediction is not None:
+            df.loc[index, 'prediction'] = prediction
+            success += 1
+        else:
+            df.loc[index, 'error'] = error_msg
+            error += 1
+    print(f"Skipped {skipped} rows, {success} rows with predictions, {error} rows with errors")
 
-        whole_path_file = "./data/" + MODEL_ID.split("/")[-1] + "/" + language + "/" + file
+    with open(whole_path_file, 'w') as fout:
+        for index, row in df.iterrows():
+            json.dump(row.to_dict(), fout)
+            fout.write('\n')
+                
 
-        with open(whole_path_file, 'w') as fout:
-            for index, row in df.iterrows():
-                row_dict = row.to_dict()
-                json.dump(row_dict, fout)
-                fout.write('\n')
-                    
-
-
-        write_log(f"model: {MODEL_ID}; language: {language}; file: {file}: end!\n")
+    with open("log.txt", "a") as flog:
+        flog.write(f"model: {MODEL_ID}; language: {language}; file: {file}")
+        flog.write(f"Skipped {skipped} rows, {success} rows with predictions, {error} rows with errors")
+        flog.write("\n\n")
