ed6826408d6b11207ab6ba254ae87bea629a6a43
Lucy Gao <gyxlucy@gmail.com>
2023-11-24 00:33:25 -0800

    docs: add Loom video demo (#884)
    * docs: add loom video demo
    
    * docs: update wording
    
    * update
    
    ---------
    
    Co-authored-by: Meng Zhang <meng@tabbyml.com>

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   corePlugins: {
     preflight: false, // disable Tailwind's reset
   },
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "../docs/**/*.mdx"], // my markdown stuff is in ../docs, not /src
-  darkMode: ['class', '[data-theme="dark"]'], // hooks into docusaurus' dark mode settigns
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./docs/**/*.mdx"],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {},
   },
