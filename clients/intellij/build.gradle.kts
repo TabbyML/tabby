e7741114ca601560dcd6a8e0ec62deb9b59d052d
Zhiming Ma <codes.icy@gmail.com>
2023-11-27 11:46:12 +0800

    fix(intellij): update supported version range. (#903)

--- clients/intellij/build.gradle.kts
+++ clients/intellij/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.tabbyml"
-version = "1.1.1"
+version = "1.2.0-dev"
 
 repositories {
   mavenCentral()
@@ -33,7 +33,7 @@ tasks {
 
   patchPluginXml {
     sinceBuild.set("222")
-    untilBuild.set("232.*")
+    untilBuild.set("233.*")
     changeNotes.set(provider {
       changelog.renderItem(
         changelog.getLatest(),
