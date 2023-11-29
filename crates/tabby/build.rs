af9b102c701531aaa1bbb354790e98399ae7c7fe
Meng Zhang <meng@tabbyml.com>
2023-11-29 11:11:00 +0800

    update

--- crates/tabby/build.rs
+++ crates/tabby/build.rs
@@ -3,10 +3,6 @@ use std::error::Error;
 use vergen::EmitBuilder;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    if cfg!(feature = "rocm") {
-        let rocm_root = "/opt/rocm";
-        println!("cargo:rustc-link-search=native={}/lib", rocm_root);
-    }
     // touch
     EmitBuilder::builder()
         .all_build()
