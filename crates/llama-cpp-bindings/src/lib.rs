d01da92100779b1793360c11e88771048cb0ebb8
darknight <illuminating.me@gmail.com>
2023-12-04 23:16:14 +0800

    feat: support build on windows

--- crates/llama-cpp-bindings/src/lib.rs
+++ crates/llama-cpp-bindings/src/lib.rs
@@ -114,3 +114,15 @@ impl TextGeneration for LlamaTextGeneration {
         Box::pin(s)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_engine() {
+        let engine = create_engine(false, "invalid_path", 1);
+
+        assert!(engine.is_null());
+    }
+}
