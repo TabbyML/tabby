4357d9b5529f1fbc41658d4c8e3576de4e493d21
Meng Zhang <meng@tabbyml.com>
2023-12-05 22:41:34 +0800

    feat: support FormMessage to be used under root Form

--- ee/tabby-ui/components/ui/form.tsx
+++ ee/tabby-ui/components/ui/form.tsx
@@ -43,18 +43,19 @@ const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
   const { getFieldState, formState } = useFormContext()
+  const name = fieldContext.name || "root";
 
-  const fieldState = getFieldState(fieldContext.name, formState)
+  const fieldState = getFieldState(name, formState)
 
-  if (!fieldContext) {
-    throw new Error("useFormField should be used within <FormField>")
+  if (!formState) {
+    throw new Error("useFormField should be used within <Form>")
   }
 
   const { id } = itemContext
 
   return {
     id,
-    name: fieldContext.name,
+    name,
     formItemId: `${id}-form-item`,
     formDescriptionId: `${id}-form-item-description`,
     formMessageId: `${id}-form-item-message`,
