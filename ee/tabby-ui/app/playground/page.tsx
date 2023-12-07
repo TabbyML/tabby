2df3b1c00d5ae9a2b8f1e3f1a6f9a34ef01c8c9b
Meng Zhang <meng@tabbyml.com>
2023-12-07 15:09:23 +0800

    fix: add back header to playground

--- ee/tabby-ui/app/playground/page.tsx
+++ ee/tabby-ui/app/playground/page.tsx
@@ -1,10 +1,18 @@
 import { Metadata } from 'next'
 import Chats from './components/chats'
+import { Header } from '@/components/header'
 
 export const metadata: Metadata = {
   title: 'Playground'
 }
 
 export default function IndexPage() {
-  return <Chats />
+  return (
+    <>
+      <Header />
+      <main className="bg-muted/50 flex flex-1 flex-col">
+        <Chats />
+      </main>
+    </>
+  )
 }
