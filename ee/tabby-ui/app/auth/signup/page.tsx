a2e76119b71c02fac40d46dedbaf867176939b57
Meng Zhang <meng@tabbyml.com>
2023-12-07 16:30:41 +0800

    feat: tweak signin page

--- ee/tabby-ui/app/auth/signup/page.tsx
+++ ee/tabby-ui/app/auth/signup/page.tsx
@@ -12,15 +12,30 @@ export default function Signup() {
     ? 'Create an admin account'
     : 'Create an account';
 
+  const description = isAdmin
+    ? 'The admin account has access to invite collaborators and manage Tabby configuration'
+    : 'Fill form below to create your account'
+
+  if (isAdmin || invitationCode) {
+    return <Content title={title} description={description} show />
+  } else {
+    return <Content title='No invitation code' description='Please contact your Tabby admin for an invitation code to register' />
+  }
+}
+
+function Content({ title, description, show }: { title: string, description: string, show?: boolean }) {
+  const searchParams = useSearchParams()
+  const invitationCode = searchParams.get('invitationCode') || undefined
+
   return (
     <div className="space-y-6 w-[350px]">
       <div className="flex flex-col space-y-2 text-center">
         <h1 className="text-2xl font-semibold tracking-tight">{title}</h1>
         <p className="text-sm text-muted-foreground">
-          Fill form below to create your account
+          {description}
         </p>
       </div>
-      <UserAuthForm invitationCode={invitationCode} />
+      {show && <UserAuthForm invitationCode={invitationCode} />}
     </div>
   )
 }
