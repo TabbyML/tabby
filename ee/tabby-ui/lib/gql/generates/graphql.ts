28cd6063f5d3bc15ddeef095d877fe3ec0872747
aliang <1098486429@qq.com>
2023-12-08 23:51:24 +0800

    fix: use return data from useSWR (#991)

--- ee/tabby-ui/lib/gql/generates/graphql.ts
+++ ee/tabby-ui/lib/gql/generates/graphql.ts
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
-
 export type Maybe<T> = T | null
 export type InputMaybe<T> = Maybe<T>
 export type Exact<T extends { [key: string]: unknown }> = {
