11794157036b031ac9f8f806dfb862a30ff3a82a
Someet Sahoo <32305867+Someet-Git@users.noreply.github.com>
2023-12-10 14:02:40 +0000

    refactor: Added the changes required

--- crates/tabby-scheduler/src/dataset/deps/javascript.rs
+++ crates/tabby-scheduler/src/dataset/deps/javascript.rs
@@ -1,39 +1,23 @@
-use std::path::Path;
-use anyhow::Result;
+use std::{collections::HashMap, path::Path};
 use tabby_common::Package;
-use serde::Deserialize;
+use package_lock_json_parser::PackageLockJson;
 
-#[derive(Debug, Deserialize)]
-struct NpmPackage {
-    name: String,
-    version: String,
-}
+fn extract_deps(lock_file: &PackageLockJson) -> Vec<Package> {
+    let packages = lock_file.packages.as_ref().map_or_else(HashMap::new, |p| p.clone());
 
-fn extract_npm_deps<'a, I>(packages: I) -> Vec<Package>
-where
-    I: IntoIterator<Item = &'a NpmPackage>,
-{
-    let mut res = packages
-        .into_iter()
-        .map(|npm_package| Package {
+    packages
+        .values()
+        .map(|pkg| Package {
+            name: pkg.name.clone().unwrap_or_default(),
+            version: Some(pkg.version.clone()),
             language: String::from("javascript"),
-            name: npm_package.name.clone(),
-            version: Some(npm_package.version.clone())
         })
-        .collect::<std::collections::HashSet<_>>()
-        .into_iter()
-        .collect::<Vec<_>>();
-    res.sort_unstable();
-    res
+        .collect()
 }
 
-pub fn process_package_lock_json(path: &Path) -> Result<Vec<Package>> {
-    let package_lock_file = path.join("package-lock.json");
+pub fn process_package_lock_json(path: &Path) -> Result<Vec<Package>, Box<dyn std::error::Error>> {
+    let package_lock_json_text = std::fs::read_to_string(path.join("package-lock.json"))?;
+    let lock_file: PackageLockJson = package_lock_json_parser::parse(package_lock_json_text)?;
 
-    let file_content = std::fs::read_to_string(&package_lock_file)?;
-    
-    let npm_packages: Vec<NpmPackage> = serde_json::from_str(&file_content)?;
-    
-    let deps = extract_npm_deps(&npm_packages);
-    Ok(deps)
+    Ok(extract_deps(&lock_file))
 }
