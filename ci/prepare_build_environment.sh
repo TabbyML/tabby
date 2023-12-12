5be320ab8e1dac18cd07c8c99aef3bbffd63c7f1
Cromefire_ <cromefire+git@pm.me>
2023-12-12 22:07:28 +0100

    Install minimal rocm on the fly

--- ci/prepare_build_environment.sh
+++ ci/prepare_build_environment.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
   brew install protobuf
 fi
@@ -11,12 +13,24 @@ install_protobuf_centos() {
   rm protoc-3.15.8-linux-x86_64.zip
 }
 
+install_hipblas_5_7_2_centos() {
+  wget -O /tmp/amdgpu-install.rpm https://repo.radeon.com/amdgpu-install/5.7.2/rhel/7.9/amdgpu-install-5.7.50702-1.el7.noarch.rpm
+  yum install /tmp/amdgpu-install.rpm
+  rm /tmp/amdgpu-install.rpm
+
+  yum install hipblas-devel hipblaslt-devel
+}
+
 if [[ "$OSTYPE" == "linux"* ]]; then
   if command -v apt-get ; then
     sudo apt-get -y install protobuf-compiler libopenblas-dev
   else
     # Build from manylinux2014 container
     yum -y install openblas-devel perl-IPC-Cmd unzip curl openssl-devel
+
+    if [[ "$ROCM" == "5.7.2" ]]; then
+      install_hipblas_5_7_2_centos
+    fi
 
     # Disable safe directory in docker
     git config --system --add safe.directory "*"
