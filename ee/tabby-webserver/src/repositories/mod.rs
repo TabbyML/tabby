656a77de83c203623f8aba4a23b81bc649da2823
Meng Zhang <meng@tabbyml.com>
2023-12-14 20:14:55 +0800

    fix: /repositories should requires authentication as well (#1042)

--- ee/tabby-webserver/src/repositories/mod.rs
+++ ee/tabby-webserver/src/repositories/mod.rs
@@ -1,21 +1,56 @@
 mod resolve;
 
+use std::sync::Arc;
+
 use anyhow::Result;
-use axum::{extract::Path, http::StatusCode, response::Response, routing, Json, Router};
+use axum::{
+    extract::{Path, State},
+    http::{Request, StatusCode},
+    middleware::{from_fn_with_state, Next},
+    response::{IntoResponse, Response},
+    routing, Json, Router,
+};
+use hyper::Body;
+use juniper_axum::extract::AuthBearer;
 use tabby_common::path::repositories_dir;
 use tracing::{instrument, warn};
 
 use crate::{
     repositories,
     repositories::resolve::{resolve_dir, resolve_file, resolve_meta, Meta, ResolveParams},
+    schema::{auth::AuthenticationService, ServiceLocator},
 };
 
-pub fn routes() -> Router {
+pub fn routes(locator: Arc<dyn ServiceLocator>) -> Router {
     Router::new()
         .route("/:name/resolve/", routing::get(repositories::resolve))
         .route("/:name/resolve/*path", routing::get(repositories::resolve))
         .route("/:name/meta/", routing::get(repositories::meta))
         .route("/:name/meta/*path", routing::get(repositories::meta))
+        .layer(from_fn_with_state(locator, require_login_middleware))
+}
+
+async fn require_login_middleware(
+    State(locator): State<Arc<dyn ServiceLocator>>,
+    AuthBearer(token): AuthBearer,
+    request: Request<Body>,
+    next: Next<Body>,
+) -> axum::response::Response {
+    let unauthorized = axum::response::Response::builder()
+        .status(StatusCode::UNAUTHORIZED)
+        .body(Body::empty())
+        .unwrap()
+        .into_response();
+
+    let Some(token) = token else {
+        return unauthorized;
+    };
+
+    let Ok(_) = locator.auth().verify_access_token(&token).await else {
+        return unauthorized;
+    };
+
+    next.run(request).await
 }
 
 #[instrument(skip(repo))]
