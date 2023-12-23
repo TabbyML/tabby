70c62323ed62e08c1bf9eaa3966d576343f6a65b
Meng Zhang <meng@tabbyml.com>
2023-12-23 14:27:44 +0800

    feat(ui): consume /v1beta/chat/completions as openai compatible stream

--- ee/tabby-ui/lib/hooks/use-patch-fetch.ts
+++ ee/tabby-ui/lib/hooks/use-patch-fetch.ts
@@ -3,7 +3,8 @@ import {
   createCallbacksTransformer,
   createStreamDataTransformer,
   StreamingTextResponse,
-  type AIStreamCallbacksAndOptions
+  type AIStreamCallbacksAndOptions,
+  OpenAIStream
 } from 'ai'
 
 import { useSession } from '../tabby/auth'
@@ -39,74 +40,8 @@ export function usePatchFetch() {
         headers
       })
 
-      const stream = StreamAdapter(res, undefined)
+      const stream = OpenAIStream(res, undefined)
       return new StreamingTextResponse(stream)
     }
   }, [data?.accessToken])
-}
-
-const utf8Decoder = new TextDecoder('utf-8')
-
-async function processLines(
-  lines: string[],
-  controller: ReadableStreamDefaultController<string>
-) {
-  for (const line of lines) {
-    const { content } = JSON.parse(line)
-    controller.enqueue(content)
-  }
-}
-
-async function readAndProcessLines(
-  reader: ReadableStreamDefaultReader<Uint8Array>,
-  controller: ReadableStreamDefaultController<string>
-) {
-  let segment = ''
-
-  while (true) {
-    const { value: chunk, done } = await reader.read()
-    if (done) {
-      break
-    }
-
-    segment += utf8Decoder.decode(chunk, { stream: true })
-
-    const linesArray = segment.split(/\r\n|\n|\r/g)
-    segment = linesArray.pop() || ''
-
-    await processLines(linesArray, controller)
-  }
-
-  if (segment) {
-    const linesArray = [segment]
-    await processLines(linesArray, controller)
-  }
-
-  controller.close()
-}
-
-function createParser(res: Response) {
-  const reader = res.body?.getReader()
-
-  return new ReadableStream<string>({
-    async start(controller): Promise<void> {
-      if (!reader) {
-        controller.close()
-        return
-      }
-
-      await readAndProcessLines(reader, controller)
-    }
-  })
-}
-
-function StreamAdapter(
-  reader: Response,
-  callbacks?: AIStreamCallbacksAndOptions
-): ReadableStream {
-  return createParser(reader)
-    .pipeThrough(createCallbacksTransformer(callbacks))
-    .pipeThrough(
-      createStreamDataTransformer(callbacks?.experimental_streamData)
-    )
 }
