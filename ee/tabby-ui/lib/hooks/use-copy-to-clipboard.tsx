8ff1bcdedde3fbdb3aa9ad2e6ec5d7bd75045402
liangfung <1098486429@qq.com>
2023-12-28 15:08:57 +0800

    fix: use document.execCommand for non-secure context

--- ee/tabby-ui/lib/hooks/use-copy-to-clipboard.tsx
+++ ee/tabby-ui/lib/hooks/use-copy-to-clipboard.tsx
@@ -1,32 +1,50 @@
 'use client'
 
 import * as React from 'react'
+import copy from 'copy-to-clipboard'
+import { toast } from 'react-hot-toast'
 
 export interface useCopyToClipboardProps {
   timeout?: number
+  onError?: (e?: any) => void
 }
 
 export function useCopyToClipboard({
-  timeout = 2000
+  timeout = 2000,
+  onError
 }: useCopyToClipboardProps) {
   const [isCopied, setIsCopied] = React.useState<Boolean>(false)
 
+  const onCopySuccess = () => {
+    setIsCopied(true)
+
+    setTimeout(() => {
+      setIsCopied(false)
+    }, timeout)
+  }
+
+  const onCopyError = (error?: any) => {
+    onError?.(error)
+    toast.error('Failed to copy.')
+  }
+
   const copyToClipboard = (value: string) => {
-    if (typeof window === 'undefined' || !navigator.clipboard?.writeText) {
-      return
+    if (typeof window === 'undefined') return
+    if (!value) return
+
+    if (!!navigator.clipboard?.writeText) {
+      navigator.clipboard
+        .writeText(value)
+        .then(onCopySuccess)
+        .catch(onCopyError)
+    } else {
+      const copyResult = copy(value)
+      if (copyResult) {
+        onCopySuccess()
+      } else {
+        onCopyError()
+      }
     }
-
-    if (!value) {
-      return
-    }
-
-    navigator.clipboard.writeText(value).then(() => {
-      setIsCopied(true)
-
-      setTimeout(() => {
-        setIsCopied(false)
-      }, timeout)
-    })
   }
 
   return { isCopied, copyToClipboard }
