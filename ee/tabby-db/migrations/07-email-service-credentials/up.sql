69d49b796f38a1ea6b5c55bec1dc1bbb98ba0a50
Redempt <instantiable@gmail.com>
2024-01-08 11:58:19 -0500

    Remove trailing comma in SQL query

--- ee/tabby-db/migrations/07-email-service-credentials/up.sql
+++ ee/tabby-db/migrations/07-email-service-credentials/up.sql
@@ -2,5 +2,5 @@ CREATE TABLE email_service_credential(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     smtp_username VARCHAR(255),
     smtp_password VARCHAR(255),
-    smtp_server VARCHAR(255),
+    smtp_server VARCHAR(255)
 );
