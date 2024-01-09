fd22fb98e5b8257145cfd3f0d1bbc60735d02450
boxbeam <instantiable@gmail.com>
2024-01-09 12:16:48 -0500

    feat(db): Implement DAO for repositories (#1178)
    * feat: Implement DAO for repositories
    
    * Add method to get repository, add test
    
    * Apply suggestions
    
    * Make delete_repository take an ID instead of name
    
    * Return an error on failed update
    
    * Use anyhow error instead of custom error
    
    * Apply suggested changes
    
    * [autofix.ci] apply automated fixes
    
    * [autofix.ci] apply automated fixes (attempt 2/3)
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ ee/tabby-db/migrations/08-repositories-table/up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE repositories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    git_url VARCHAR(255) NOT NULL,
+    CONSTRAINT `idx_name` UNIQUE (`name`)
+    CONSTRAINT `idx_git_url` UNIQUE (`git_url`)
+);
