342d838bf38098321f861ad02b4aec81ca61c2f4
Redempt <instantiable@gmail.com>
2024-01-11 09:40:39 -0500

    Do not include password in email credentials when reading

--- ee/tabby-webserver/src/schema/dao.rs
+++ ee/tabby-webserver/src/schema/dao.rs
@@ -86,7 +86,6 @@ impl From<EmailServiceCredentialDAO> for EmailServiceCredential {
     fn from(value: EmailServiceCredentialDAO) -> Self {
         EmailServiceCredential {
             smtp_username: value.smtp_username,
-            smtp_password: value.smtp_password,
             smtp_server: value.smtp_server,
         }
     }
--- ee/tabby-webserver/src/schema/email_service_credential.rs
+++ ee/tabby-webserver/src/schema/email_service_credential.rs
@@ -1,16 +1,17 @@
 use anyhow::Result;
 use async_trait::async_trait;
 use juniper::GraphQLObject;
+use tabby_db::EmailServiceCredentialDAO;
 
 #[derive(GraphQLObject)]
 pub struct EmailServiceCredential {
     pub smtp_username: String,
-    pub smtp_password: String,
     pub smtp_server: String,
 }
 
 #[async_trait]
 pub trait EmailServiceCredentialService: Send + Sync {
     async fn get_email_service_credential(&self) -> Result<Option<EmailServiceCredential>>;
-    async fn update_email_service_credential(&self, creds: EmailServiceCredential) -> Result<()>;
+    async fn update_email_service_credential(&self, creds: EmailServiceCredentialDAO)
+        -> Result<()>;
 }
