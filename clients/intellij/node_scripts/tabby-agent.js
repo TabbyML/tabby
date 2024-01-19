e2ca847b6963b0a0f75da68d644a76836d8ab041
Nathan Anderson <andnat@gmail.com>
2024-01-19 11:00:46 -0500

    fix(agent): Use /usr/bin/env instead of /bin/env

--- clients/intellij/node_scripts/tabby-agent.js
+++ clients/intellij/node_scripts/tabby-agent.js
@@ -1,4 +1,4 @@
-#!/bin/env node
+#!/usr/bin/env node
 'use strict';
 
 var events = require('events');
--- clients/tabby-agent/src/cli.ts
+++ clients/tabby-agent/src/cli.ts
@@ -1,4 +1,4 @@
-#!/bin/env node
+#!/usr/bin/env node
 
 import { TabbyAgent } from "./TabbyAgent";
 import { JsonLineServer } from "./JsonLineServer";
