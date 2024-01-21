2380d7de1d6893517c5233c6e6d9ae55d51e4a28
Meng Zhang <meng@tabbyml.com>
2024-01-20 21:22:25 -0800

    feat(ui): adjust header layout (#1253)
    * feat(ui): adjust header layout
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- ee/tabby-ui/app/playground/page.tsx
+++ ee/tabby-ui/app/playground/page.tsx
@@ -1,7 +1,5 @@
 import { Metadata } from 'next'
 
-import { Header } from '@/components/header'
-
 import Chats from './components/chats'
 
 export const metadata: Metadata = {
@@ -10,11 +8,8 @@ export const metadata: Metadata = {
 
 export default function IndexPage() {
   return (
-    <>
-      <Header />
-      <main className="flex flex-1 flex-col">
-        <Chats />
-      </main>
-    </>
+    <main className="flex flex-1 flex-col">
+      <Chats />
+    </main>
   )
 }
--- ee/tabby-ui/components/user-panel.tsx
+++ ee/tabby-ui/components/user-panel.tsx
@@ -1,23 +1,85 @@
 import React from 'react'
+import Link from 'next/link'
+import { has } from 'lodash-es'
+import NiceAvatar, { genConfig } from 'react-nice-avatar'
 
+import { WorkerKind } from '@/lib/gql/generates/graphql'
+import { useWorkers } from '@/lib/hooks/use-workers'
 import { useAuthenticatedSession, useSignOut } from '@/lib/tabby/auth'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 
-import { IconLogout } from './ui/icons'
+import { IconBackpack, IconChat, IconCode, IconLogout } from './ui/icons'
 
 export default function UserPanel() {
-  const session = useAuthenticatedSession()
+  const user = useAuthenticatedSession()
   const signOut = useSignOut()
 
+  const workers = useWorkers()
+  const isChatEnabled = has(workers, WorkerKind.Chat)
+
+  if (!user) {
+    return
+  }
+
+  const config = genConfig(user.email)
+
   return (
-    session && (
-      <div className="flex justify-center py-4 text-sm font-medium">
-        <span className="flex items-center gap-2">
-          <span title="Sign out">
-            <IconLogout className="cursor-pointer" onClick={signOut} />
-          </span>
-          {session.email}
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <span className="flex h-10 w-10 rounded-full border">
+          <NiceAvatar className="w-full" {...config} />
         </span>
-      </div>
-    )
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {isChatEnabled && (
+          <DropdownMenuItem>
+            <Link
+              target="_blank"
+              href="/playground"
+              className="flex w-full items-center"
+            >
+              <IconChat />
+              <span className="ml-2">Chat Playground</span>
+            </Link>
+          </DropdownMenuItem>
+        )}
+        <DropdownMenuItem>
+          <Link
+            target="_blank"
+            href="/files"
+            className="flex w-full items-center"
+          >
+            <IconCode />
+            <span className="ml-2">Code Browser</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link
+            target="_blank"
+            href="/api"
+            className="flex w-full items-center"
+          >
+            <IconBackpack />
+            <span className="ml-2">API Docs</span>
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={signOut}>
+          <span className="flex items-center">
+            <IconLogout />
+            <span className="ml-2">Logout</span>
+          </span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
