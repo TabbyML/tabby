f0c2de90cadca3b0d0e9595002905d9e164723df
Meng Zhang <meng@tabbyml.com>
2024-01-22 14:27:41 -0800

    fix(webserver): disable scheduler jobs in current upcoming release (#1274)
    * fix(webserver): disable scheduler jobs in current upcoming release
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- ee/tabby-webserver/src/service/cron/mod.rs
+++ ee/tabby-webserver/src/service/cron/mod.rs
@@ -16,29 +16,38 @@ async fn new_job_scheduler(jobs: Vec<Job>) -> anyhow::Result<JobScheduler> {
     Ok(scheduler)
 }
 
-pub fn run_cron(db_conn: &DbConn) {
+pub fn run_cron(db_conn: &DbConn, turn_on_scheduler_jobs: bool) {
     let db_conn = db_conn.clone();
     tokio::spawn(async move {
+        let mut jobs = vec![];
+
         let Ok(job1) = db::refresh_token_job(db_conn.clone()).await else {
             error!("failed to create db job");
             return;
         };
-        // run every 5 minutes
-        let Ok(job2) =
-            job_utils::run_job(db_conn.clone(), "sync".to_owned(), "0 1/5 * * * * *").await
-        else {
-            error!("failed to create sync job");
-            return;
-        };
-        // run every 5 hours
-        let Ok(job3) =
-            job_utils::run_job(db_conn.clone(), "index".to_owned(), "0 0 1/5 * * * *").await
-        else {
-            error!("failed to create index job");
-            return;
-        };
+        jobs.push(job1);
 
-        let Ok(mut scheduler) = new_job_scheduler(vec![job1, job2, job3]).await else {
+        if turn_on_scheduler_jobs {
+            // run every 5 minutes
+            let Ok(job2) =
+                job_utils::run_job(db_conn.clone(), "sync".to_owned(), "0 1/5 * * * * *").await
+            else {
+                error!("failed to create sync job");
+                return;
+            };
+            jobs.push(job2);
+
+            // run every 5 hours
+            let Ok(job3) =
+                job_utils::run_job(db_conn.clone(), "index".to_owned(), "0 0 1/5 * * * *").await
+            else {
+                error!("failed to create index job");
+                return;
+            };
+            jobs.push(job3);
+        }
+
+        let Ok(mut scheduler) = new_job_scheduler(jobs).await else {
             error!("failed to start job scheduler");
             return;
         };
