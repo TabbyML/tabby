8fd2a81fbad4568d221195a5382d9d382c22ba98
boxbeam <instantiable@gmail.com>
2024-01-29 23:26:45 -0500

    fix(juniper_axum): Fix how has_previous_page and has_next_page are computed (#1322)
    * Determine has_previous_page using first > 0
    
    * Fix computing has_previous_page and has_next_page
    
    * Simplify code
    
    * Add unit test for has_next_page and has_previous_page
    
    * [autofix.ci] apply automated fixes
    
    * [autofix.ci] apply automated fixes (attempt 2/3)
    
    * Change algorithm for next and prev page
    
    * Add more test cases
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- crates/juniper-axum/src/relay/mod.rs
+++ crates/juniper-axum/src/relay/mod.rs
@@ -12,6 +12,32 @@ pub use edge::Edge;
 pub use node_type::NodeType;
 pub use page_info::PageInfo;
 
+fn validate_first_last(
+    first: Option<i32>,
+    last: Option<i32>,
+) -> FieldResult<(Option<usize>, Option<usize>)> {
+    if first.is_some() && last.is_some() {
+        return Err("The \"first\" and \"last\" parameters cannot exist at the same time".into());
+    }
+
+    let first = match first {
+        Some(first) if first < 0 => {
+            return Err("The \"first\" parameter must be a non-negative number".into());
+        }
+        Some(first) => Some(first as usize),
+        None => None,
+    };
+
+    let last = match last {
+        Some(last) if last < 0 => {
+            return Err("The \"last\" parameter must be a non-negative number".into());
+        }
+        Some(last) => Some(last as usize),
+        None => None,
+    };
+    Ok((first, last))
+}
+
 pub fn query<Node, F>(
     after: Option<String>,
     before: Option<String>,
@@ -28,41 +54,20 @@ where
         Option<usize>,
     ) -> FieldResult<Vec<Node>>,
 {
-    if first.is_some() && last.is_some() {
-        return Err("The \"first\" and \"last\" parameters cannot exist at the same time".into());
-    }
+    let (first, last) = validate_first_last(first, last)?;
 
-    let first = match first {
-        Some(first) if first < 0 => {
-            return Err("The \"first\" parameter must be a non-negative number".into());
-        }
-        Some(first) => Some(first as usize),
-        None => None,
-    };
-
-    let last = match last {
-        Some(last) if last < 0 => {
-            return Err("The \"last\" parameter must be a non-negative number".into());
-        }
-        Some(last) => Some(last as usize),
-        None => None,
-    };
-
-    match (first, last) {
-        (None, None) => {
-            let nodes = f(after, before, None, None)?;
-            Ok(Connection::build_connection(nodes, None, None))
-        }
-        (Some(first), None) => {
-            let nodes = f(after, before, Some(first + 1), None)?;
-            Ok(Connection::build_connection(nodes, Some(first), None))
-        }
-        (None, Some(last)) => {
-            let nodes = f(after, before, None, Some(last + 1))?;
-            Ok(Connection::build_connection(nodes, None, Some(last)))
-        }
-        _ => Err("The \"first\" and \"last\" parameters cannot exist at the same time".into()),
-    }
+    let first_plus_one = first.map(|i| i + 1);
+    let last_plus_one = last.map(|i| i + 1);
+    let after_some = after.is_some();
+    let before_some = before.is_some();
+    let nodes = f(after, before, first_plus_one, last_plus_one)?;
+    Ok(Connection::build_connection(
+        nodes,
+        after_some,
+        before_some,
+        first,
+        last,
+    ))
 }
 
 pub async fn query_async<Node, F, R>(
@@ -77,39 +82,18 @@ where
     F: FnOnce(Option<String>, Option<String>, Option<usize>, Option<usize>) -> R,
     R: Future<Output = FieldResult<Vec<Node>>>,
 {
-    if first.is_some() && last.is_some() {
-        return Err("The \"first\" and \"last\" parameters cannot exist at the same time".into());
-    }
+    let (first, last) = validate_first_last(first, last)?;
 
-    let first = match first {
-        Some(first) if first < 0 => {
-            return Err("The \"first\" parameter must be a non-negative number".into());
-        }
-        Some(first) => Some(first as usize),
-        None => None,
-    };
-
-    let last = match last {
-        Some(last) if last < 0 => {
-            return Err("The \"last\" parameter must be a non-negative number".into());
-        }
-        Some(last) => Some(last as usize),
-        None => None,
-    };
-
-    match (first, last) {
-        (None, None) => {
-            let nodes = f(after, before, None, None).await?;
-            Ok(Connection::build_connection(nodes, None, None))
-        }
-        (Some(first), None) => {
-            let nodes = f(after, before, Some(first + 1), None).await?;
-            Ok(Connection::build_connection(nodes, Some(first), None))
-        }
-        (None, Some(last)) => {
-            let nodes = f(after, before, None, Some(last + 1)).await?;
-            Ok(Connection::build_connection(nodes, None, Some(last)))
-        }
-        _ => Err("The \"first\" and \"last\" parameters cannot exist at the same time".into()),
-    }
+    let first_plus_one = first.map(|i| i + 1);
+    let last_plus_one = last.map(|i| i + 1);
+    let after_some = after.is_some();
+    let before_some = before.is_some();
+    let nodes = f(after, before, first_plus_one, last_plus_one).await?;
+    Ok(Connection::build_connection(
+        nodes,
+        after_some,
+        before_some,
+        first,
+        last,
+    ))
 }
