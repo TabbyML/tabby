f9fca157c299938babc99e53c32f2b088b8d3f3d
boxbeam <instantiable@gmail.com>
2024-02-02 15:11:06 -0500

    Rename feature to prod-db, passthrough feature from tabby crate

--- ee/tabby-db/src/path.rs
+++ ee/tabby-db/src/path.rs
@@ -7,11 +7,11 @@ fn tabby_ee_root() -> PathBuf {
 }
 
 pub fn db_file() -> PathBuf {
-    #[cfg(feature = "prod")]
+    #[cfg(feature = "prod-db")]
     {
         tabby_ee_root().join("db.sqlite")
     }
-    #[cfg(not(feature = "prod"))]
+    #[cfg(not(feature = "prod-db"))]
     {
         tabby_ee_root().join("dev-db.sqlite")
     }
