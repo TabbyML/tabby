5870c7947ad888b9e8a47874c602355f16ef4f18
Tyler Burnett <11090043+TylerBurnett@users.noreply.github.com>
2024-02-02 19:29:34 +1000

    feat(scheduler, docs): Add repo context support for C#, added language to supported list in docs

---
+++ crates/tabby-scheduler/queries/csharp.scm
@@ -0,0 +1,19 @@
+(
+  (class_declaration (identifier) @name) @definition.class
+)
+
+(
+  (struct_declaration (identifier) @name) @definition.struct
+)
+
+(
+  (method_declaration (identifier) @name) @definition.method
+)
+
+(
+  (interface_declaration (identifier) @name) @definition.interface
+)
+
+(
+  (local_function_statement (identifier) @name) @definition.function
+)
