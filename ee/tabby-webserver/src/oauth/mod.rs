22670830d924193fecc2f4d1709f8bd1d66486ec
Meng Zhang <meng@tabbyml.com>
2024-02-02 00:49:40 -0800

    fix(ui): support url encoded path in ui handler (#1354)
    * support-urlencode-path
    
    * fix url encoding for error_message
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- ee/tabby-webserver/src/oauth/mod.rs
+++ ee/tabby-webserver/src/oauth/mod.rs
@@ -166,7 +166,7 @@ fn match_auth_result(
 
 fn make_error_redirect(provider: OAuthProvider, message: &str) -> Redirect {
     let query = querystring::stringify(vec![
-        ("error_message", message),
+        ("error_message", urlencoding::encode(message).as_ref()),
         (
             "provider",
             serde_json::to_string(&provider).unwrap().as_str(),
