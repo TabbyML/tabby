693fa2cfd0406322ff01fb1d97aee43350a50bf8
Meng Zhang <meng@tabbyml.com>
2024-02-05 22:24:18 -0800

    update

--- crates/tabby-scheduler/src/lib.rs
+++ crates/tabby-scheduler/src/lib.rs
@@ -5,7 +5,7 @@ mod index;
 mod repository;
 mod utils;
 
-use std::{process::Stdio, sync::Arc, time::Duration};
+use std::{process::Stdio, sync::Arc};
 
 use anyhow::Result;
 use tabby_common::config::{RepositoryAccess, RepositoryConfig};
@@ -29,68 +29,64 @@ pub async fn scheduler<T: RepositoryAccess + 'static>(
 
         // Every 10 minutes
         scheduler
-            //.add(Job::new_async("* 1/10 * * * * *", move |_, _| {
-            .add(Job::new_one_shot_async(
-                Duration::from_secs(1),
-                move |_, _| {
-                    let access = access.clone();
-                    let args = args.clone();
-                    Box::pin(async move {
-                        info!("Running scheduler job...");
-                        let exe = std::env::current_exe().unwrap();
-                        let job_id = access
-                            .create_job_run("scheduler".to_owned())
-                            .await
-                            .unwrap_or_default();
+            .add(Job::new_async("* 1/10 * * * * *", move |_, _| {
+                let access = access.clone();
+                let args = args.clone();
+                Box::pin(async move {
+                    info!("Running scheduler job...");
+                    let exe = std::env::current_exe().unwrap();
+                    let job_id = access
+                        .create_job_run("scheduler".to_owned())
+                        .await
+                        .unwrap_or_default();
 
-                        let mut child = tokio::process::Command::new(exe)
-                            .arg("scheduler")
-                            .arg("--now")
-                            .args(args)
-                            .stdout(Stdio::piped())
-                            .stderr(Stdio::piped())
-                            .spawn()
-                            .unwrap();
+                    let mut child = tokio::process::Command::new(exe)
+                        .arg("scheduler")
+                        .arg("--now")
+                        .args(args)
+                        .stdout(Stdio::piped())
+                        .stderr(Stdio::piped())
+                        .spawn()
+                        .unwrap();
 
-                        {
-                            // Pipe stdout
-                            let access = access.clone();
-                            let stdout = child.stdout.take().unwrap();
-                            tokio::spawn(async move {
-                                let stdout = tokio::io::BufReader::new(stdout);
-                                let mut stdout = stdout.lines();
-                                while let Ok(Some(line)) = stdout.next_line().await {
-                                    println!("{line}");
-                                    let _ = access
-                                        .update_job_output(job_id, line + "\n", "".to_owned())
-                                        .await;
-                                }
-                            });
-                        }
+                    {
+                        // Pipe stdout
+                        let access = access.clone();
+                        let stdout = child.stdout.take().unwrap();
+                        tokio::spawn(async move {
+                            let stdout = tokio::io::BufReader::new(stdout);
+                            let mut stdout = stdout.lines();
+                            while let Ok(Some(line)) = stdout.next_line().await {
+                                println!("{line}");
+                                let _ = access
+                                    .update_job_output(job_id, line + "\n", "".to_owned())
+                                    .await;
+                            }
+                        });
+                    }
 
-                        {
-                            // Pipe stderr
-                            let access = access.clone();
-                            let stderr = child.stderr.take().unwrap();
-                            tokio::spawn(async move {
-                                let stderr = tokio::io::BufReader::new(stderr);
-                                let mut stdout = stderr.lines();
-                                while let Ok(Some(line)) = stdout.next_line().await {
-                                    eprintln!("{line}");
-                                    let _ = access
-                                        .update_job_output(job_id, "".to_owned(), line + "\n")
-                                        .await;
-                                }
-                            });
-                        }
-                        if let Some(exit_code) = child.wait().await.ok().and_then(|s| s.code()) {
-                            let _ = access.complete_job_run(job_id, exit_code).await;
-                        } else {
-                            let _ = access.complete_job_run(job_id, -1).await;
-                        }
-                    })
-                },
-            )?)
+                    {
+                        // Pipe stderr
+                        let access = access.clone();
+                        let stderr = child.stderr.take().unwrap();
+                        tokio::spawn(async move {
+                            let stderr = tokio::io::BufReader::new(stderr);
+                            let mut stdout = stderr.lines();
+                            while let Ok(Some(line)) = stdout.next_line().await {
+                                eprintln!("{line}");
+                                let _ = access
+                                    .update_job_output(job_id, "".to_owned(), line + "\n")
+                                    .await;
+                            }
+                        });
+                    }
+                    if let Some(exit_code) = child.wait().await.ok().and_then(|s| s.code()) {
+                        let _ = access.complete_job_run(job_id, exit_code).await;
+                    } else {
+                        let _ = access.complete_job_run(job_id, -1).await;
+                    }
+                })
+            })?)
             .await?;
 
         info!("Scheduler activated...");
