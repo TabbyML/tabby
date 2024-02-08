12b68a7346bfd42e236786ce991545cc1bf3ddf3
Meng Zhang <meng@tabbyml.com>
2024-02-08 12:10:36 -0800

    feat(http): remove vertex-ai bindings, add chat model supports

--- crates/http-api-bindings/src/lib.rs
+++ crates/http-api-bindings/src/lib.rs
@@ -1,35 +1,26 @@
 mod openai;
-mod vertex_ai;
 
 use std::sync::Arc;
 
 use openai::OpenAIEngine;
 use serde_json::Value;
 use tabby_inference::{make_text_generation, TextGeneration};
-use vertex_ai::VertexAIEngine;
 
-pub fn create(model: &str) -> (Arc<dyn TextGeneration>, String) {
+pub fn create(model: &str) -> (Arc<dyn TextGeneration>, Option<String>, Option<String>) {
     let params = serde_json::from_str(model).expect("Failed to parse model string");
     let kind = get_param(&params, "kind");
-    if kind == "vertex-ai" {
-        let api_endpoint = get_param(&params, "api_endpoint");
-        let authorization = get_param(&params, "authorization");
-        let engine = make_text_generation(VertexAIEngine::create(
-            api_endpoint.as_str(),
-            authorization.as_str(),
-        ));
-        (Arc::new(engine), VertexAIEngine::prompt_template())
-    } else if kind == "openai" {
+    if kind == "openai" {
         let model_name = get_param(&params, "model_name");
         let api_endpoint = get_param(&params, "api_endpoint");
         let authorization = get_optional_param(&params, "authorization");
-        let prompt_template = get_param(&params, "prompt_template");
+        let prompt_template = get_optional_param(&params, "prompt_template");
+        let chat_template = get_optional_param(&params, "chat_template");
         let engine = make_text_generation(OpenAIEngine::create(
             api_endpoint.as_str(),
             model_name.as_str(),
             authorization,
         ));
-        (Arc::new(engine), prompt_template)
+        (Arc::new(engine), prompt_template, chat_template)
     } else {
         panic!("Only vertex_ai and openai are supported for http backend");
     }
