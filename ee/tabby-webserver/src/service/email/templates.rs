a5a37a34495eef36c9624b8be335abb393a77821
boxbeam <instantiable@gmail.com>
2024-02-13 12:15:37 -0500

    feat(webserver): Create template system for emails (#1447)
    * feat(webserver): Create template system for emails
    
    * Put newlines between lines of email body
    
    * Move email template folder

---
+++ ee/tabby-webserver/src/service/email/templates.rs
@@ -0,0 +1,26 @@
+pub struct EmailContents {
+    pub subject: String,
+    pub body: String,
+}
+
+fn format_email(template: &'static str, replacements: &[(&str, &str)]) -> EmailContents {
+    let mut lines = template.lines();
+    let mut subject = lines
+        .next()
+        .expect("Email must have subject line")
+        .to_string();
+    let body: Vec<&str> = lines.collect();
+    let mut body = body.join("\n");
+    for (name, replacement) in replacements {
+        body = body.replace(name, replacement);
+        subject = subject.replace(name, replacement);
+    }
+    EmailContents { subject, body }
+}
+
+pub fn invitation_email(external_url: &str, code: &str) -> EmailContents {
+    format_email(
+        include_str!("../../../email_templates/invitation.html"),
+        &[("{external_url}", external_url), ("{code}", code)],
+    )
+}
