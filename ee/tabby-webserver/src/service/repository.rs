7acd0c857ae183acbb373f85d3946857c1a99188
boxbeam <instantiable@gmail.com>
2024-02-14 11:08:48 -0500

    test(webserver): Add tests to cover repository service

--- ee/tabby-webserver/src/service/repository.rs
+++ ee/tabby-webserver/src/service/repository.rs
@@ -80,4 +80,74 @@ mod tests {
             "A repository with the same name or URL already exists"
         );
     }
+
+    #[tokio::test]
+    pub async fn test_repository_mutations() {
+        let db = DbConn::new_in_memory().await.unwrap();
+        let service: &dyn RepositoryService = &db;
+
+        let id_1 = service
+            .create_repository(
+                "example".into(),
+                "https://github.com/example/example".into(),
+            )
+            .await
+            .unwrap();
+
+        let id_2 = service
+            .create_repository(
+                "example2".into(),
+                "https://github.com/example/example2".into(),
+            )
+            .await
+            .unwrap();
+
+        service
+            .create_repository(
+                "example3".into(),
+                "https://github.com/example/example3".into(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(
+            service
+                .list_repositories(None, None, None, None)
+                .await
+                .unwrap()
+                .len(),
+            3
+        );
+
+        service.delete_repository(&id_1).await.unwrap();
+
+        assert_eq!(
+            service
+                .list_repositories(None, None, None, None)
+                .await
+                .unwrap()
+                .len(),
+            2
+        );
+
+        service
+            .update_repository(
+                &id_2,
+                "Example2".to_string(),
+                "https://github.com/example/Example2".to_string(),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(
+            service
+                .list_repositories(None, None, None, None)
+                .await
+                .unwrap()
+                .first()
+                .unwrap()
+                .name,
+            "Example2"
+        );
+    }
 }
