c876a3d2b4728d782d7aa891f291a0f494852772
boxbeam <instantiable@gmail.com>
2024-02-15 13:35:38 -0500

    Rename parameters

--- ee/tabby-db/src/password_reset.rs
+++ ee/tabby-db/src/password_reset.rs
@@ -13,13 +13,13 @@ pub struct PasswordResetDAO {
 }
 
 impl DbConn {
-    pub async fn create_password_reset(&self, id: i32) -> Result<String> {
+    pub async fn create_password_reset(&self, user_id: i32) -> Result<String> {
         let code = Uuid::new_v4().to_string();
         let time = Utc::now();
         query!(
             "INSERT INTO password_reset (user_id, code, created_at) VALUES ($1, $2, $3)
             ON CONFLICT(user_id) DO UPDATE SET code= $2, created_at = $3;",
-            id,
+            user_id,
             code,
             time
         )
@@ -28,24 +28,24 @@ impl DbConn {
         Ok(code)
     }
 
-    pub async fn delete_password_reset(&self, id: String) -> Result<()> {
-        query!("DELETE FROM password_reset WHERE user_id = ?", id)
+    pub async fn delete_password_reset(&self, user_id: i32) -> Result<()> {
+        query!("DELETE FROM password_reset WHERE user_id = ?", user_id)
             .execute(&self.pool)
             .await?;
         Ok(())
     }
 
-    pub async fn get_password_reset(&self, id: i32) -> Result<Option<PasswordResetDAO>> {
+    pub async fn get_password_reset(&self, user_id: i32) -> Result<Option<PasswordResetDAO>> {
         let password_reset = sqlx::query_as(
             "SELECT user_id, code, created_at FROM password_reset WHERE user_id = ?;",
         )
-        .bind(id)
+        .bind(user_id)
         .fetch_optional(&self.pool)
         .await?;
         Ok(password_reset)
     }
 
-    pub async fn delete_expired_password_reset(&self) -> Result<()> {
+    pub async fn delete_expired_password_resets(&self) -> Result<()> {
         let time = Utc::now() - Duration::hours(1);
         query!("DELETE FROM password_reset WHERE created_at < ?", time)
             .execute(&self.pool)
