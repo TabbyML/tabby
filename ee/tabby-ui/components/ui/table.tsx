201b85015542b9a8e1ff18b67f1a393474671baa
liangfung <1098486429@qq.com>
2024-02-18 11:50:52 +0800

    format

--- ee/tabby-ui/components/ui/table.tsx
+++ ee/tabby-ui/components/ui/table.tsx
@@ -6,11 +6,11 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-    <table
-      ref={ref}
-      className={cn('w-full caption-bottom text-sm', className)}
-      {...props}
-    />
+  <table
+    ref={ref}
+    className={cn('w-full caption-bottom text-sm', className)}
+    {...props}
+  />
 ))
 Table.displayName = 'Table'
 
