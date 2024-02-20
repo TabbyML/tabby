ba50886a3328ea60989d25c64f66e9c09c75bd16
liangfung <1098486429@qq.com>
2024-02-20 18:26:56 +0800

    feat: init

---
+++ ee/tabby-ui/app/(dashboard)/settings/subscription/components/license-table.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { IconCheck } from "@/components/ui/icons"
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table"
+
+export const LicenseTable = () => {
+
+
+  return (
+    <Table className="text-center border">
+      <TableHeader>
+        <TableRow className="hidden">
+          <TableHead className="w-[40%]"></TableHead>
+          <TableHead className="w-[20%]"></TableHead>
+          <TableHead className="w-[20%]"></TableHead>
+          <TableHead className="w-[20%]"></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell colSpan={4} className='bg-accent text-accent-foreground text-left'>Change Management</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="text-left">User count</TableCell>
+          <TableCell>Up to 20</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell>$250.00</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="text-left">State-based change</TableCell>
+          <TableCell>
+            <div className="flex justify-center">
+              {/* todo switch to another icon which can set strokeWidth */}
+              <IconCheck style={{ strokeWidth: 2 }} />
+            </div>
+          </TableCell>
+          <TableCell><div className="flex justify-center">
+            <IconCheck />
+          </div></TableCell>
+          <TableCell><div className="flex justify-center">
+            <IconCheck />
+          </div></TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell colSpan={4} className='bg-accent text-accent-foreground text-left'>Security</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="text-left">User count</TableCell>
+          <TableCell>Up to 20</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell>$250.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  )
+}
--- ee/tabby-ui/app/(dashboard)/settings/subscription/components/subscription.tsx
+++ ee/tabby-ui/app/(dashboard)/settings/subscription/components/subscription.tsx
@@ -1,9 +1,33 @@
 'use client'
 
+import { SubHeader } from "@/components/sub-header"
+import { LicenseTable } from "./license-table"
+
 export default function Subscription() {
 
 
   return (
-    <div>subs</div>
+    <div className="p-4">
+      <SubHeader>
+        You can upload your Bytebase license to unlock pro/enterprise features.
+      </SubHeader>
+      <div className="flex flex-col gap-8">
+        <div className="grid lg:grid-cols-3 font-bold">
+          <div>
+            <div className='text-muted-foreground'>Current plan</div>
+            <div className="text-3xl">Enterprise</div>
+          </div>
+          <div>
+            <div className='text-muted-foreground'>Assigned / Total Instance License</div>
+            <div className="text-3xl">2 / 999</div>
+          </div>
+          <div>
+            <div className='text-muted-foreground'>Expires at</div>
+            <div className="text-3xl">09/20/2222</div>
+          </div>
+        </div>
+        <LicenseTable />
+      </div>
+    </div>
   )
 }
