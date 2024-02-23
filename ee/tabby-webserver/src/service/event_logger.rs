224ade63e027d9676703dced8f2a6606941a6a9d
boxbeam <instantiable@gmail.com>
2024-02-23 13:44:24 -0500

    test(webserver): Create test for user completions (#1523)
    * test(webserver): Create test for user completions
    
    * Utility function to sleep 50ms

--- ee/tabby-webserver/src/service/event_logger.rs
+++ ee/tabby-webserver/src/service/event_logger.rs
@@ -13,6 +13,10 @@ fn log_err<T, E: Display>(res: Result<T, E>) {
     }
 }
 
+pub fn new_event_logger(db: DbConn) -> impl EventLogger {
+    EventLoggerImpl { db }
+}
+
 impl EventLogger for EventLoggerImpl {
     fn log(&self, e: tabby_common::api::event::Event) {
         match e {
@@ -56,5 +60,65 @@ impl EventLogger for EventLoggerImpl {
             }
             tabby_common::api::event::Event::ChatCompletion { .. } => {}
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+
+    use super::*;
+    use tabby_common::api::event::Event;
+    use tabby_db::DbConn;
+
+    async fn sleep_50() {
+        tokio::time::sleep(Duration::from_millis(50)).await;
+    }
+
+    #[tokio::test]
+    async fn test_event_logger() {
+        let db = DbConn::new_in_memory().await.unwrap();
+        let logger = new_event_logger(db.clone());
+        logger.log(Event::Completion {
+            completion_id: "test_id".into(),
+            language: "rust".into(),
+            prompt: "testprompt".into(),
+            segments: None,
+            choices: vec![],
+            user: Some("example".into()),
+        });
+
+        sleep_50().await;
+        assert!(!db.list_user_completions().await.unwrap().is_empty());
+
+        logger.log(Event::View {
+            completion_id: "test_id".into(),
+            choice_index: 0,
+            view_id: None,
+        });
+
+        sleep_50().await;
+        assert_eq!(!db.list_user_completions().await.unwrap()[0].views, 1);
+
+        logger.log(Event::Dismiss {
+            completion_id: "test_id".into(),
+            choice_index: 0,
+            view_id: None,
+            elapsed: None,
+        });
+
+        sleep_50().await;
+        assert_eq!(!db.list_user_completions().await.unwrap()[0].dismisses, 1);
+
+        logger.log(Event::Select {
+            completion_id: "test_id".into(),
+            choice_index: 0,
+            view_id: None,
+            kind: None,
+            elapsed: None,
+        });
+
+        sleep_50().await;
+        assert_eq!(!db.list_user_completions().await.unwrap()[0].selects, 1);
     }
 }
