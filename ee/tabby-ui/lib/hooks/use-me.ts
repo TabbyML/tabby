5980accd6a401a6968615b0296c428b64301693f
aliang <1098486429@qq.com>
2024-02-27 15:53:01 +0800

    fix(ui): get email from me query (#1562)
    * fix(ui): get email from me query
    
    * reexecuteQueryMe

---
+++ ee/tabby-ui/lib/hooks/use-me.ts
@@ -0,0 +1,19 @@
+import { useQuery } from 'urql'
+
+import { graphql } from '@/lib/gql/generates'
+
+const meQuery = graphql(/* GraphQL */ `
+  query MeQuery {
+    me {
+      authToken
+      email
+      isAdmin
+    }
+  }
+`)
+
+const useMe = () => {
+  return useQuery({ query: meQuery })
+}
+
+export { useMe }
