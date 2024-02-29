bb19cf912ff713988cee5cfd729e968caa37d69c
Meng Zhang <meng@tabbyml.com>
2024-02-29 09:08:59 -0800

    Update ee/tabby-webserver/src/service/auth.rs

--- ee/tabby-webserver/src/service/auth.rs
+++ ee/tabby-webserver/src/service/auth.rs
@@ -502,7 +502,7 @@ async fn check_invitation(
     };
 
     if invitation.email != email {
-        return Err(anyhow!("Invitation code is not for this user").into());
+        return Err(anyhow!("Invitation code is not for this email address").into());
     }
 
     Ok(Some(invitation))
