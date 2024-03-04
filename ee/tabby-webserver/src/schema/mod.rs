7dee3b188fda550b743bc2a89edd4f619980a5c1
boxbeam <instantiable@gmail.com>
2024-03-04 13:50:39 -0500

    fix(webserver): Disallow admins from disabling themselves or changing their own roles (#1617)
    * fix(webserver): Disallow admins from disabling themselves or changing their own roles
    
    * Make CoreError for self-targeted actions
    
    * Use unauthorized error
    
    * Use forbidden
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- ee/tabby-webserver/src/schema/mod.rs
+++ ee/tabby-webserver/src/schema/mod.rs
@@ -392,12 +392,20 @@ impl Mutation {
 
     async fn update_user_active(ctx: &Context, id: ID, active: bool) -> Result<bool> {
         check_admin(ctx)?;
+        if ctx.claims.as_ref().is_some_and(|c| c.sub.0 == id) {
+            return Err(CoreError::Forbidden(
+                "You cannot change your own active status",
+            ));
+        }
         ctx.locator.auth().update_user_active(&id, active).await?;
         Ok(true)
     }
 
     async fn update_user_role(ctx: &Context, id: ID, is_admin: bool) -> Result<bool> {
         check_admin(ctx)?;
+        if ctx.claims.as_ref().is_some_and(|c| c.sub.0 == id) {
+            return Err(CoreError::Forbidden("You cannot update your own role"));
+        }
         ctx.locator.auth().update_user_role(&id, is_admin).await?;
         Ok(true)
     }
