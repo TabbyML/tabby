fa77be1e504ebb08d3a70070c8edf966ea894b98
boxbeam <instantiable@gmail.com>
2024-03-06 13:41:46 -0500

    Fixes for conflicts

--- ee/tabby-webserver/src/handler.rs
+++ ee/tabby-webserver/src/handler.rs
@@ -51,44 +51,44 @@ impl WebserverHandle {
         local_port: u16,
     ) -> (Router, Router) {
         let ctx = create_service_locator(logger, code, self.db.clone(), is_chat_enabled).await;
-        cron::run_cron(ctx.auth(), ctx.job(), ctx.worker(), local_port).await;
+        let events = cron::run_cron(ctx.auth(), ctx.job(), ctx.worker(), local_port).await;
 
-        let repository_cache = Arc::new(RepositoryCache::new_initialized(ctx.repository()).await);
-        repository_cache.start_reload_job().await;
+        let repository_cache = RepositoryCache::new_initialized(ctx.repository(), &events).await;
 
         let schema = Arc::new(create_schema());
         let rs = Arc::new(repository_cache);
 
-    let api = api
-        .route(
-            "/v1beta/server_setting",
-            routing::get(server_setting).with_state(ctx.clone()),
-        )
-        // Routes before `distributed_tabby_layer` are protected by authentication middleware for following routes:
-        // 1. /v1/*
-        // 2. /v1beta/*
-        .layer(from_fn_with_state(ctx.clone(), distributed_tabby_layer))
-        .route(
-            "/graphql",
-            routing::post(graphql::<Arc<Schema>, Arc<dyn ServiceLocator>>).with_state(ctx.clone()),
-        )
-        .route("/graphql", routing::get(playground("/graphql", None)))
-        .layer(Extension(schema))
-        .route(
-            "/hub",
-            routing::get(hub::ws_handler).with_state(ctx.clone()),
-        )
-        .nest(
-            "/repositories",
-            repositories::routes(rs.clone(), ctx.auth()),
-        )
-        .nest("/oauth", oauth::routes(ctx.auth()));
+        let api = api
+            .route(
+                "/v1beta/server_setting",
+                routing::get(server_setting).with_state(ctx.clone()),
+            )
+            // Routes before `distributed_tabby_layer` are protected by authentication middleware for following routes:
+            // 1. /v1/*
+            // 2. /v1beta/*
+            .layer(from_fn_with_state(ctx.clone(), distributed_tabby_layer))
+            .route(
+                "/graphql",
+                routing::post(graphql::<Arc<Schema>, Arc<dyn ServiceLocator>>)
+                    .with_state(ctx.clone()),
+            )
+            .route("/graphql", routing::get(playground("/graphql", None)))
+            .layer(Extension(schema))
+            .route(
+                "/hub",
+                routing::get(hub::ws_handler).with_state(ctx.clone()),
+            )
+            .nest(
+                "/repositories",
+                repositories::routes(rs.clone(), ctx.auth()),
+            )
+            .nest("/oauth", oauth::routes(ctx.auth()));
 
-    let ui = ui.route("/graphiql", routing::get(graphiql("/graphql", None)));
+        let ui = ui.route("/graphiql", routing::get(graphiql("/graphql", None)));
 
-    let ui = ui.fallback(ui::handler);
+        let ui = ui.fallback(ui::handler);
 
-    (api, ui)
+        (api, ui)
     }
 }
 
