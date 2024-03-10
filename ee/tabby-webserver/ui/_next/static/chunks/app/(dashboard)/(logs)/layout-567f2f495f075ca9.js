60310d416177de629853d06e746d1a058ffd9a7b
Meng Zhang <meng@tabbyml.com>
2024-03-09 22:59:44 -0800

    chore: update ui (#1653)

---
+++ ee/tabby-webserver/ui/_next/static/chunks/app/(dashboard)/(logs)/layout-567f2f495f075ca9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6845,7110,4738],{63521:function(){}},function(n){n.O(0,[2971,7864,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
--- ee/tabby-webserver/ui/_next/static/chunks/app/api/page-9cb0e38abc3ffc91.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7110,4738,6845,3875],{63521:function(){}},function(n){n.O(0,[2971,7864,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
