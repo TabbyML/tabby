119bcf79a880ec36ae8031fe0a18e33306866227
liangfung <1098486429@qq.com>
2024-03-19 20:25:57 +0800

    rehype-sanitize

--- ee/tabby-ui/app/files/components/markdown-view.tsx
+++ ee/tabby-ui/app/files/components/markdown-view.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import rehypeRaw from 'rehype-raw'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
+import rehypeSanitize from 'rehype-sanitize'
 
 import { MemoizedReactMarkdown } from '@/components/markdown'
 
@@ -15,7 +16,7 @@ const MarkdownView: React.FC<MarkdownViewProps> = ({ value }) => {
       <MemoizedReactMarkdown
         className="prose-full-width prose break-words dark:prose-invert prose-h1:border-b prose-h1:pb-2 prose-h2:border-b prose-h2:pb-2 prose-p:leading-relaxed prose-a:text-primary prose-img:inline prose-img:w-auto prose-img:max-w-full"
         remarkPlugins={[remarkGfm, remarkMath]}
-        rehypePlugins={[rehypeRaw]}
+        rehypePlugins={[rehypeRaw, rehypeSanitize]}
       >
         {value}
       </MemoizedReactMarkdown>
