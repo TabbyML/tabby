5372bdfb2628a86c39994ac09558af8a60c2de88
wwayne <wayne.wang0821@gmail.com>
2024-03-25 22:45:59 +0800

    fix warning;

--- ee/tabby-ui/components/ui/icons.tsx
+++ ee/tabby-ui/components/ui/icons.tsx
@@ -1195,9 +1195,9 @@ function IconSparkles({ className, ...props }: React.ComponentProps<'svg'>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className={cn('h-4 w-4', className)}
       {...props}
     >
