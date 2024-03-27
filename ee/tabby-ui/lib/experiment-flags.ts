8ecb729cb2c683ac605c24cbf20cbb785a0c0dcf
Wang Zixiao <wayne.wang0821@gmail.com>
2024-03-27 13:49:32 +0800

    feat(ui): add experiment flag store for experiment ai page (#1723)
    * feat(ui): store for experimental flag
    
    * add hydrated to the store
    
    * [autofix.ci] apply automated fixes
    
    * using generatic type
    
    * [autofix.ci] apply automated fixes
    
    * add enum EXPERIMENTAL_FALG
    
    * [autofix.ci] apply automated fixes
    
    * refactory
    
    * [autofix.ci] apply automated fixes
    
    * add factory class
    
    * [autofix.ci] apply automated fixes
    
    * update
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ ee/tabby-ui/lib/experiment-flags.ts
@@ -0,0 +1,70 @@
+import useLocalStorage from 'use-local-storage'
+
+class ExperimentFlag {
+  constructor(
+    private storageKey: string,
+    readonly description: string,
+    readonly defaultValue: boolean
+  ) {}
+
+  get value() {
+    if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+      const storageValue = localStorage.getItem(this.storageKey)
+      if (storageValue) {
+        return storageValue === 'true'
+      }
+    }
+
+    return this.defaultValue
+  }
+}
+
+class ExperimentFlagFactory {
+  private storageKey: string
+  private description: string
+  private defaultValue: boolean
+
+  constructor(storageKey: string, description: string, defaultValue?: boolean) {
+    this.storageKey = `EXP_${storageKey}`
+    this.description = description
+    this.defaultValue = defaultValue ?? false
+  }
+
+  defineGlobalVar() {
+    return new ExperimentFlag(
+      this.storageKey,
+      this.description,
+      this.defaultValue
+    )
+  }
+
+  defineHook() {
+    return (): [{ value: boolean; description: string }, () => void] => {
+      const [storageValue, setStorageValue] = useLocalStorage(
+        this.storageKey,
+        this.defaultValue
+      )
+      const toggleFlag = () => {
+        setStorageValue(!storageValue)
+      }
+      return [
+        {
+          value: storageValue,
+          description: this.description
+        },
+        toggleFlag
+      ]
+    }
+  }
+}
+
+const enableCodeBrowserQuickActionBarFactory = new ExperimentFlagFactory(
+  'enable_code_browser_quick_action_bar',
+  'Show a quick action popup upon selecting code snippets',
+  false
+)
+
+export const EXP_enable_code_browser_quick_action_bar =
+  enableCodeBrowserQuickActionBarFactory.defineGlobalVar()
+export const useEnableCodeBrowserQuickActionBar =
+  enableCodeBrowserQuickActionBarFactory.defineHook()
