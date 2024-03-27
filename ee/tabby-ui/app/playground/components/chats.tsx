bf9068fb614144dad4713f82de17226c91c48b36
liangfung <1098486429@qq.com>
2024-03-27 16:46:01 +0800

    feat: default prompt

--- ee/tabby-ui/app/playground/components/chats.tsx
+++ ee/tabby-ui/app/playground/components/chats.tsx
@@ -1,34 +1,69 @@
 'use client'
 
 import React from 'react'
+import { useSearchParams } from 'next/navigation'
 import type { Message } from 'ai'
 
 import { useStore } from '@/lib/hooks/use-store'
+import { addChat } from '@/lib/stores/chat-actions'
 import { useChatStore } from '@/lib/stores/chat-store'
 import { getChatById } from '@/lib/stores/utils'
-import { Chat } from '@/components/chat'
+import { truncateText } from '@/lib/utils'
+import { Chat, ChatRef } from '@/components/chat'
+import LoadingWrapper from '@/components/loading-wrapper'
+import { ListSkeleton } from '@/components/skeleton'
 
 import { ChatSessions } from './chat-sessions'
 
 const emptyMessages: Message[] = []
 
 export default function Chats() {
+  const searchParams = useSearchParams()
+  const defaultPrompt = searchParams.get('prompt')?.toString()
+  const shouldConsumeDefaultPrompt = React.useRef(!!defaultPrompt)
+  const chatRef = React.useRef<ChatRef>(null)
+
   const _hasHydrated = useStore(useChatStore, state => state._hasHydrated)
   const chats = useStore(useChatStore, state => state.chats)
   const activeChatId = useStore(useChatStore, state => state.activeChatId)
+  const chat = getChatById(chats, activeChatId)
 
-  const chatId = activeChatId
-  const chat = getChatById(chats, chatId)
+  React.useEffect(() => {
+    if (!shouldConsumeDefaultPrompt.current) return
+    if (!chatRef.current?.append) return
+
+    if (activeChatId && defaultPrompt) {
+      // append default prompt
+      chatRef.current.append({
+        role: 'user',
+        content: defaultPrompt
+      })
+      // store as a new chat
+      addChat(activeChatId, truncateText(defaultPrompt))
+
+      shouldConsumeDefaultPrompt.current = false
+    }
+  }, [chatRef.current?.append])
 
   return (
     <div className="grid flex-1 overflow-hidden lg:grid-cols-[280px_1fr]">
       <ChatSessions className="hidden w-[280px] border-r lg:block" />
-      <Chat
-        loading={!_hasHydrated}
-        id={chatId}
-        key={chatId}
-        initialMessages={chat?.messages ?? emptyMessages}
-      />
+      <LoadingWrapper
+        delay={0}
+        loading={!_hasHydrated || !activeChatId}
+        fallback={
+          <div className="mx-auto w-full max-w-2xl pt-4 md:pt-10">
+            <ListSkeleton />
+          </div>
+        }
+      >
+        <Chat
+          id={activeChatId}
+          key={activeChatId}
+          initialMessages={chat?.messages ?? emptyMessages}
+          ref={chatRef}
+        />
+      </LoadingWrapper>
     </div>
   )
 }
--- ee/tabby-ui/components/chat.tsx
+++ ee/tabby-ui/components/chat.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { useChat } from 'ai/react'
-import type { Message } from 'ai/react'
+import type { Message, UseChatHelpers } from 'ai/react'
 import { find, findIndex } from 'lodash-es'
 import { toast } from 'sonner'
 
@@ -16,17 +16,36 @@ import { ChatList } from '@/components/chat-list'
 import { ChatPanel } from '@/components/chat-panel'
 import { ChatScrollAnchor } from '@/components/chat-scroll-anchor'
 import { EmptyScreen } from '@/components/empty-screen'
-import { ListSkeleton } from '@/components/skeleton'
 
 export interface ChatProps extends React.ComponentProps<'div'> {
   initialMessages?: Message[]
   id?: string
-  loading?: boolean
+  defaultPrompt?: string | string
 }
 
-export function Chat({ id, initialMessages, loading, className }: ChatProps) {
+export interface ChatRef extends UseChatHelpers {}
+
+function ChatRenderer(
+  { id, initialMessages, className }: ChatProps,
+  ref: React.ForwardedRef<ChatRef>
+) {
   usePatchFetch()
   const chats = useStore(useChatStore, state => state.chats)
+
+  const useChatHelpers = useChat({
+    initialMessages,
+    id,
+    body: {
+      id
+    },
+    onResponse(response) {
+      console.log('=====call onresponse', response)
+      if (response.status === 401) {
+        toast.error(response.statusText)
+      }
+    }
+  })
+
   const {
     messages,
     append,
@@ -36,18 +55,7 @@ export function Chat({ id, initialMessages, loading, className }: ChatProps) {
     input,
     setInput,
     setMessages
-  } = useChat({
-    initialMessages,
-    id,
-    body: {
-      id
-    },
-    onResponse(response) {
-      if (response.status === 401) {
-        toast.error(response.statusText)
-      }
-    }
-  })
+  } = useChatHelpers
 
   const [selectedMessageId, setSelectedMessageId] = React.useState<string>()
 
@@ -122,20 +130,19 @@ export function Chat({ id, initialMessages, loading, className }: ChatProps) {
     return () => stop()
   }, [])
 
+  React.useImperativeHandle(
+    ref,
+    () => {
+      return useChatHelpers
+    },
+    [useChatHelpers]
+  )
+
   return (
     <div className="flex justify-center overflow-x-hidden">
       <div className="w-full max-w-2xl px-4">
         <div className={cn('pb-[200px] pt-4 md:pt-10', className)}>
-          {loading ? (
-            <div className="group relative mb-4 flex animate-pulse items-start md:-ml-12">
-              <div className="shrink-0">
-                <span className="block h-8 w-8 rounded-md bg-gray-200 dark:bg-gray-700"></span>
-              </div>
-              <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
-                <ListSkeleton />
-              </div>
-            </div>
-          ) : messages.length ? (
+          {messages.length ? (
             <>
               <ChatList
                 messages={messages}
@@ -163,3 +170,5 @@ export function Chat({ id, initialMessages, loading, className }: ChatProps) {
     </div>
   )
 }
+
+export const Chat = React.forwardRef<ChatRef, ChatProps>(ChatRenderer)
