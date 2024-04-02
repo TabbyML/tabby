a1f6e74bf8cf5ef304e53baacd4017b02541007d
liangfung <1098486429@qq.com>
2024-04-02 13:57:48 +0800

    cleanup

--- ee/tabby-ui/app/files/components/chat-side-bar.tsx
+++ ee/tabby-ui/app/files/components/chat-side-bar.tsx
@@ -1,23 +1,10 @@
 import React from 'react'
 
 import { useStore } from '@/lib/hooks/use-store'
-import {
-  clearChats,
-  deleteChat,
-  setActiveChatId
-} from '@/lib/stores/chat-actions'
 import { useChatStore } from '@/lib/stores/chat-store'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
-import { IconClose, IconTrash } from '@/components/ui/icons'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger
-} from '@/components/ui/tooltip'
-import { ClearChatsButton } from '@/components/clear-chats-button'
-import { EditChatTitleDialog } from '@/components/edit-chat-title-dialog'
-import { ListSkeleton } from '@/components/skeleton'
+import { IconClose } from '@/components/ui/icons'
 
 import { QuickActionEventPayload } from '../lib/event-emitter'
 import { SourceCodeBrowserContext } from './source-code-browser'
@@ -73,87 +60,6 @@ export const ChatSideBar: React.FC<ChatSideBarProps> = ({
         className="w-full flex-1 border-0"
         key={activeChatId}
         ref={iframeRef}
-      />
-    </div>
-  )
-}
-
-function ChatSessions({ className }: { className?: string }) {
-  const _hasHydrated = useStore(useChatStore, state => state._hasHydrated)
-  const chats = useStore(useChatStore, state => state.chats)
-  const activeChatId = useStore(useChatStore, state => state.activeChatId)
-
-  const onDeleteClick = (
-    e: React.MouseEvent<HTMLButtonElement>,
-    chatId: string
-  ) => {
-    deleteChat(chatId)
-  }
-
-  const handleClearChats = () => {
-    clearChats()
-  }
-
-  const onSelectChat = (id: string) => {
-    setActiveChatId(id)
-  }
-
-  return (
-    <div className={cn('px-4', className)}>
-      <div>
-        {!_hasHydrated ? (
-          <ListSkeleton />
-        ) : (
-          <>
-            {chats?.map(chat => {
-              const isActive = activeChatId === chat.id
-              return (
-                <div
-                  key={chat.id}
-                  onClick={e => onSelectChat(chat.id)}
-                  className={cn(
-                    'group my-2 flex cursor-pointer items-center justify-between gap-3 rounded-lg px-3 py-1 text-sm transition-all hover:bg-primary/10',
-                    isActive && 'bg-primary/10'
-                  )}
-                >
-                  <span className="truncate leading-8">
-                    {chat.title || '(Untitled)'}
-                  </span>
-                  <div
-                    className={cn(
-                      'hidden items-center group-hover:flex',
-                      isActive && 'flex'
-                    )}
-                    onClick={e => e.stopPropagation()}
-                  >
-                    <EditChatTitleDialog
-                      initialValue={chat.title}
-                      chatId={chat.id}
-                    />
-                    <Tooltip>
-                      <TooltipTrigger asChild>
-                        <Button
-                          variant="ghost"
-                          size="icon"
-                          onClick={e => onDeleteClick(e, chat.id)}
-                        >
-                          <IconTrash />
-                          <span className="sr-only">Delete</span>
-                        </Button>
-                      </TooltipTrigger>
-                      <TooltipContent side="bottom">Delete</TooltipContent>
-                    </Tooltip>
-                  </div>
-                </div>
-              )
-            })}
-          </>
-        )}
-      </div>
-      <ClearChatsButton
-        disabled={chats?.length === 0}
-        onClear={handleClearChats}
-        className="justify-center"
       />
     </div>
   )
