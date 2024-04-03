2aa6ddcaa1f6fc680e77cf19ea86a7b341af378c
boxbeam <instantiable@gmail.com>
2024-04-03 14:02:50 -0400

    Apply suggestions, add DAO for github repositories

--- ee/tabby-db/src/github_repository_provider.rs
+++ ee/tabby-db/src/github_repository_provider.rs
@@ -3,30 +3,40 @@ use sqlx::{query, query_as};
 
 use crate::{DbConn, SQLXResultExt};
 
-pub struct GithubProviderDAO {
+pub struct GithubRepositoryProviderDAO {
     pub display_name: String,
     pub application_id: String,
     pub secret: String,
 }
 
+pub struct GithubProvidedRepositoryDAO {
+    pub provider_id: i64,
+    pub candidate_id: String,
+    pub name: String,
+    pub git_url: String,
+}
+
 impl DbConn {
-    pub async fn update_github_provider(
+    pub async fn create_github_provider(
         &self,
         name: String,
         application_id: String,
         secret: String,
-    ) -> Result<()> {
-        query!("INSERT INTO github_repository_provider (display_name, application_id, secret) VALUES ($1, $2, $3);",
+    ) -> Result<i64> {
+        let res = query!("INSERT INTO github_repository_provider (display_name, application_id, secret) VALUES ($1, $2, $3);",
             name,
             application_id,
             secret
-        ).execute(&self.pool).await.unique_error("Provider already exists")?;
-        Ok(())
+        ).execute(&self.pool).await.unique_error("GitHub Application ID already exists")?;
+        Ok(res.last_insert_rowid())
     }
 
-    pub async fn get_github_provider(&self, display_name: String) -> Result<GithubProviderDAO> {
+    pub async fn get_github_provider(
+        &self,
+        display_name: String,
+    ) -> Result<GithubRepositoryProviderDAO> {
         let provider = query_as!(
-            GithubProviderDAO,
+            GithubRepositoryProviderDAO,
             "SELECT display_name, application_id, secret FROM github_repository_provider WHERE display_name = ?;",
             display_name
         )
@@ -44,6 +54,38 @@ impl DbConn {
         .await?;
         if res.rows_affected() != 1 {
             return Err(anyhow!("No github provider details to delete"));
+        }
+        Ok(())
+    }
+
+    pub async fn create_github_provided_repository(
+        &self,
+        provider_id: i64,
+        candidate_id: String,
+        name: String,
+        git_url: String,
+    ) -> Result<i64> {
+        let res = query!(
+            "INSERT INTO github_provided_repositories(provider_id, candidate_id, name, git_url) VALUES (?, ?, ?, ?)",
+            provider_id, candidate_id, name, git_url
+        ).execute(&self.pool).await?;
+        Ok(res.last_insert_rowid())
+    }
+
+    pub async fn delete_github_provided_repository(
+        &self,
+        provider_id: i64,
+        candidate_id: String,
+    ) -> Result<()> {
+        let res = query!(
+            "DELETE FROM github_provided_repositories WHERE provider_id = ? AND candidate_id = ?",
+            provider_id,
+            candidate_id
+        )
+        .execute(&self.pool)
+        .await?;
+        if res.rows_affected() == 0 {
+            return Err(anyhow!("Specified repository does not exist"));
         }
         Ok(())
     }
