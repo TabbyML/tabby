74c3e3e7af79ad82510b7e508969df6b2b4f9ad0
boxbeam <instantiable@gmail.com>
2024-04-04 13:35:47 -0400

    Apply suggestion

--- ee/tabby-db/migrations/0022_github-provider.up.sql
+++ ee/tabby-db/migrations/0022_github-provider.up.sql
@@ -7,9 +7,10 @@ CREATE TABLE github_repository_provider(
 );
 CREATE TABLE github_provided_repositories(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    provider_id INTEGER NOT NULL,
+    github_repository_provider_id INTEGER NOT NULL,
     candidate_id TEXT NOT NULL,
     name TEXT NOT NULL,
     git_url TEXT NOT NULL,
-    CONSTRAINT `idx_provider_id_candidate_id` UNIQUE (`candidate_id`, `provider_id`)
+    CONSTRAINT `idx_provider_id_candidate_id` UNIQUE (`candidate_id`, `github_repository_provider_id`),
+    FOREIGN KEY (github_repository_provider_id) REFERENCES github_repository_provider(id) ON DELETE CASCADE
 );
