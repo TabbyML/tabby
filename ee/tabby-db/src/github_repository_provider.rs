468db099e15d59f5b16a7bde0e1ad247a74fe926
boxbeam <instantiable@gmail.com>
2024-04-05 17:26:14 -0400

    Apply suggestions

--- ee/tabby-db/src/github_repository_provider.rs
+++ ee/tabby-db/src/github_repository_provider.rs
@@ -12,8 +12,8 @@ pub struct GithubRepositoryProviderDAO {
 
 #[derive(FromRow)]
 pub struct GithubProvidedRepositoryDAO {
-    pub provider_id: i64,
-    pub candidate_id: String,
+    pub github_repository_provider_id: i64,
+    pub vendor_id: String,
     pub name: String,
     pub git_url: String,
 }
@@ -33,27 +33,21 @@ impl DbConn {
         Ok(res.last_insert_rowid())
     }
 
-    pub async fn get_github_provider(
-        &self,
-        display_name: String,
-    ) -> Result<GithubRepositoryProviderDAO> {
+    pub async fn get_github_provider(&self, id: i64) -> Result<GithubRepositoryProviderDAO> {
         let provider = query_as!(
             GithubRepositoryProviderDAO,
-            "SELECT display_name, application_id, secret FROM github_repository_provider WHERE display_name = ?;",
-            display_name
+            "SELECT display_name, application_id, secret FROM github_repository_provider WHERE id = ?;",
+            id
         )
         .fetch_one(&self.pool)
         .await?;
         Ok(provider)
     }
 
-    pub async fn delete_github_provider(&self, display_name: String) -> Result<()> {
-        let res = query!(
-            "DELETE FROM github_repository_provider WHERE display_name = ?;",
-            display_name
-        )
-        .execute(&self.pool)
-        .await?;
+    pub async fn delete_github_provider(&self, id: i64) -> Result<()> {
+        let res = query!("DELETE FROM github_repository_provider WHERE id = ?;", id)
+            .execute(&self.pool)
+            .await?;
         if res.rows_affected() != 1 {
             return Err(anyhow!("No github provider details to delete"));
         }
@@ -68,24 +62,16 @@ impl DbConn {
         git_url: String,
     ) -> Result<i64> {
         let res = query!(
-            "INSERT INTO github_provided_repositories(provider_id, candidate_id, name, git_url) VALUES (?, ?, ?, ?)",
+            "INSERT INTO github_provided_repositories(github_repository_provider_id, vendor_id, name, git_url) VALUES (?, ?, ?, ?)",
             provider_id, candidate_id, name, git_url
         ).execute(&self.pool).await?;
         Ok(res.last_insert_rowid())
     }
 
-    pub async fn delete_github_provided_repository(
-        &self,
-        provider_id: i64,
-        candidate_id: String,
-    ) -> Result<()> {
-        let res = query!(
-            "DELETE FROM github_provided_repositories WHERE provider_id = ? AND candidate_id = ?",
-            provider_id,
-            candidate_id
-        )
-        .execute(&self.pool)
-        .await?;
+    pub async fn delete_github_provided_repository(&self, id: i64) -> Result<()> {
+        let res = query!("DELETE FROM github_provided_repositories WHERE id = ?", id)
+            .execute(&self.pool)
+            .await?;
         if res.rows_affected() == 0 {
             return Err(anyhow!("Specified repository does not exist"));
         }
@@ -94,19 +80,29 @@ impl DbConn {
 
     pub async fn list_github_provided_repositories(
         &self,
-        provider_id: i64,
+        github_repository_provider_ids: Vec<i64>,
         limit: Option<usize>,
         skip_id: Option<i32>,
         backwards: bool,
     ) -> Result<Vec<GithubProvidedRepositoryDAO>> {
+        let ids: Vec<_> = github_repository_provider_ids
+            .into_iter()
+            .map(|id| id.to_string())
+            .collect();
+        let ids = format!("({})", ids.join(", "));
         let repos = query_paged_as!(
             GithubProvidedRepositoryDAO,
             "github_provided_repositories",
-            ["provider_id", "candidate_id", "git_url", "name"],
+            [
+                "github_repository_provider_id",
+                "vendor_id",
+                "git_url",
+                "name"
+            ],
             limit,
             skip_id,
             backwards,
-            Some(format!("provider_id = {provider_id}"))
+            Some(format!("github_repository_provider_id IN {ids}"))
         )
         .fetch_all(&self.pool)
         .await?;
