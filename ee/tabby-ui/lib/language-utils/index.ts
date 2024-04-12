143ba21e9a610570e84894215034e607fc69feab
wwayne <wayne.wang0821@gmail.com>
2024-04-12 11:20:49 +0800

    update

--- ee/tabby-ui/lib/language-utils/index.ts
+++ ee/tabby-ui/lib/language-utils/index.ts
@@ -5,30 +5,30 @@ import { Language as ProgrammingLanguage } from '@/lib/gql/generates/graphql'
 
 import languages from './languages'
 
-const filenames: Record<string, string[]> = {}
-const extnames: Record<string, string[]> = {}
-
-for (const [alias, associations] of Object.entries(languages)) {
-  for (const filename of associations.filenames) {
-    if (!has(filenames, filename)) {
-      filenames[filename] = []
-    }
-
-    filenames[filename].push(alias)
-  }
-
-  for (const extname of associations.extnames) {
-    if (!has(extnames, extname)) {
-      extnames[extname] = []
-    }
-
-    extnames[extname].push(alias)
-  }
-}
-
 // Fork from
 // https://github.com/TomerAberbach/filename2prism/
 export const filename2prism: (filename: string) => Array<string> = filename => {
+  const filenames: Record<string, string[]> = {}
+  const extnames: Record<string, string[]> = {}
+
+  for (const [alias, associations] of Object.entries(languages)) {
+    for (const filename of associations.filenames) {
+      if (!has(filenames, filename)) {
+        filenames[filename] = []
+      }
+
+      filenames[filename].push(alias)
+    }
+
+    for (const extname of associations.extnames) {
+      if (!has(extnames, extname)) {
+        extnames[extname] = []
+      }
+
+      extnames[extname].push(alias)
+    }
+  }
+
   const result: string[] = []
   return result
     .concat(
