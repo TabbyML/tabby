d8c3e1351c3236a7a0aedecb1c012cd87d856e2c
boxbeam <instantiable@gmail.com>
2024-04-17 14:17:51 -0400

    Apply suggestion

--- ee/tabby-webserver/src/env.rs
+++ ee/tabby-webserver/src/env.rs
@@ -1,6 +1,3 @@
 pub fn demo_mode() -> bool {
-    matches!(
-        &*std::env::var("TABBY_WEBSERVER_DEMO_MODE").unwrap_or_default(),
-        "true" | "1"
-    )
+    std::env::var("TABBY_WEBSERVER_DEMO_MODE").is_ok()
 }
