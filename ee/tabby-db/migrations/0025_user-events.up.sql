0e194c1756f77bb4c12e095bdbc986cdc7220ddc
boxbeam <instantiable@gmail.com>
2024-04-23 11:35:23 -0400

    Add foreign key on user_id

--- ee/tabby-db/migrations/0025_user-events.up.sql
+++ ee/tabby-db/migrations/0025_user-events.up.sql
@@ -4,7 +4,8 @@ CREATE TABLE user_events (
     completion_id TEXT NOT NULL,
     type TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
-    payload TEXT NOT NULL
+    payload TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
 CREATE INDEX idx_user_events_created_at ON user_events(created_at);
 CREATE INDEX idx_user_events_completion_id ON user_events(completion_id);
