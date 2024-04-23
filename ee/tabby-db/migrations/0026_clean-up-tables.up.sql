b8156f6e4ee2c89b6beb2735e5e6cd5eb92c755e
Meng Zhang <meng@tabbyml.com>
2024-04-23 15:14:17 -0700

    refactor(db): cleanup db tables

---
+++ ee/tabby-db/migrations/0026_clean-up-tables.up.sql
@@ -0,0 +1,16 @@
+DROP TABLE github_oauth_credential;
+DROP TABLE google_oauth_credential;
+
+-- Drop the entire refresh_token table to resolve the foreign key issue.
+-- This is acceptable as it is equivalent to asking all users to sign in again for the new release.
+
+CREATE TABLE refresh_tokens(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  token VARCHAR(255) NOT NULL COLLATE NOCASE,
+  expires_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP DEFAULT(DATETIME('now')),
+  CONSTRAINT `idx_token` UNIQUE(`token`)
+
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
