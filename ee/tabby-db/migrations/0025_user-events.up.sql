c97c8f4e8ec643c277c70af26997c23c68fa49e4
boxbeam <instantiable@gmail.com>
2024-04-23 11:45:59 -0400

    Revert removal of completion_id

--- ee/tabby-db/migrations/0025_user-events.up.sql
+++ ee/tabby-db/migrations/0025_user-events.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE user_events (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
     completion_id TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
     type TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
     payload TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 CREATE INDEX idx_user_events_created_at ON user_events(created_at);
 CREATE INDEX idx_user_events_completion_id ON user_events(completion_id);
