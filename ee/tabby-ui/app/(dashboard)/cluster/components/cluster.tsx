39b2c622ef1c10a08978165ef48f4491bd6f51de
wwayne <wayne.wang0821@gmail.com>
2024-04-23 11:21:50 +0800

    feat(ui): display storage usage

--- ee/tabby-ui/app/(dashboard)/cluster/components/cluster.tsx
+++ ee/tabby-ui/app/(dashboard)/cluster/components/cluster.tsx
@@ -2,6 +2,8 @@
 
 import { noop } from 'lodash-es'
 import { useQuery } from 'urql'
+import bytes from 'bytes';
+import { useTheme } from 'next-themes';
 
 import { graphql } from '@/lib/gql/generates'
 import { WorkerKind } from '@/lib/gql/generates/graphql'
@@ -13,6 +15,19 @@ import { IconRotate } from '@/components/ui/icons'
 import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
 import { CopyButton } from '@/components/copy-button'
+import {
+  ResponsiveContainer,
+  PieChart,
+  Pie,
+  Cell,
+  Label,
+} from 'recharts'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { IconFolderOpen } from '@/components/ui/icons';
 
 import WorkerCard from './worker-card'
 
@@ -46,6 +61,15 @@ export default function Workers() {
   })
 
   if (!healthInfo) return
+
+  // TODO: mock data
+  const usageData = [
+    { name: "Models", usage: 1024 * 1024 * 1024 * 1.1, color: "#0088FE", folders: ["~/.tabby/models"] },
+    { name: "Repository data", usage: 1024 * 1024 * 2.1, color: "#00C49F", folders: ["~/.tabby/dataset", "~/.tabby/index"] },
+    { name: "Database", usage: 1024 * 85, color: "#FFBB28", folders: ["~/.tabby/ee"] },
+    { name: "Events", usage: 1024 * 1200 * 11, color: "#FF8042", folders: ["~/.tabby/events"] }
+  ];
+  const totalUsage = 1024 * 1300 * 1200
 
   return (
     <div className="flex w-full flex-col gap-3">
@@ -112,6 +136,105 @@ export default function Workers() {
           cpuInfo={healthInfo.cpu_info}
         />
       </div>
+
+      <Separator className="mt-5" />
+
+      <div>
+        <p className="font-bold">
+          Storage Usage
+        </p>
+        <p className="text-sm text-muted-foreground">Disk space used by Tabby</p>
+      </div>
+      
+      
+      <div className="flex flex-col items-center gap-x-3 md:flex-row">
+        <ResponsiveContainer width={230} height={220}>
+          <PieChart>
+            <Pie 
+              data={usageData} 
+              dataKey="usage"
+              cx={110}
+              cy={100}
+              innerRadius={70}
+              outerRadius={90}
+              stroke='none'
+            >
+              {usageData.map((entry) => (
+                <Cell key={entry.name} fill={entry.color} />
+              ))}
+              <Label  
+                content={<CustomLabel totalUsage={totalUsage} />}
+                position="center"
+              />
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+
+        <div className="flex w-full flex-col gap-y-2 md:ml-10 md:w-auto">
+          {usageData.map(item => (
+            <Tooltip key={item.name}>
+              <TooltipTrigger>
+                <div className="flex cursor-default items-center justify-between text-xs" >
+                  <div className="flex w-40 items-center">
+                    <div className="mr-1.5 h-3 w-3 rounded" style={{ backgroundColor: item.color }} />
+                    <p className="font-semibold">{item.name}</p>
+                  </div>
+                  <p>{bytes(item.usage)}</p>
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <div className="flex items-center text-muted-foreground">
+                  <IconFolderOpen className="mr-0.5" />
+                  <p>Folder</p>
+                </div>
+                
+                {item.folders.map(folder => (<p>{folder}</p>))}
+              </TooltipContent>
+            </Tooltip>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
+
+function CustomLabel ({
+  viewBox,
+  totalUsage
+}: {
+  viewBox?: {
+    cx: number;
+    cy: number;
+  };
+  totalUsage: number;
+}) {
+  const { theme } = useTheme()
+  if (!viewBox) return
+  const { cx, cy } = viewBox
+  return (
+    <g>
+      <text
+        x={cx}
+        y={cy - 10}
+        textAnchor="middle"
+        dominantBaseline="central"
+        alignmentBaseline="middle"
+        fill={theme === 'dark' ? '#FDFDFD' : '#030302'}
+        className="text-sm"
+      >
+        Total Usuage
+      </text>
+      <text
+        x={cx}
+        y={cy + 13}
+        textAnchor="middle"
+        dominantBaseline="central"
+        alignmentBaseline="middle"
+        fill={theme === 'dark' ? '#FDFDFD' : '#030302'}
+        className="text-lg font-semibold"
+      >
+        {bytes(totalUsage)}
+      </text>
+    </g>
+  );
+};
--- ee/tabby-ui/components/ui/icons.tsx
+++ ee/tabby-ui/components/ui/icons.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react'
 // FIXME(wwayne): Review each icons and consider re-export from `lucide-react`.
-import { BookOpenText, ChevronsDownUp, Mail } from 'lucide-react'
+import { BookOpenText, ChevronsDownUp, Mail, FolderOpen } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 
@@ -1374,6 +1374,10 @@ function IconChevronsDownUp({
   )
 }
 
+function IconFolderOpen({ className, ...props }: React.ComponentProps<typeof FolderOpen>) {
+  return <FolderOpen className={cn('h4 w-4', className)} {...props} />
+}
+
 export {
   IconEdit,
   IconNextChat,
@@ -1444,5 +1448,6 @@ export {
   IconActivity,
   IconBookOpenText,
   IconMail,
-  IconChevronsDownUp
+  IconChevronsDownUp,
+  IconFolderOpen
 }
