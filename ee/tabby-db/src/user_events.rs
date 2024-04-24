02e23795669cfd218c6d72a8c097c25ba0f90efb
wwayne <wayne.wang0821@gmail.com>
2024-04-24 12:16:06 +0800

    fix(db): list_user_events condition not working

--- ee/tabby-db/src/user_events.rs
+++ ee/tabby-db/src/user_events.rs
@@ -48,7 +48,7 @@ impl DbConn {
         start: DateTime<Utc>,
         end: DateTime<Utc>,
     ) -> Result<Vec<UserEventDAO>> {
-        let condition = Some(format!("created_at >= {start} AND created_at < {end}"));
+        let condition = Some(format!("created_at >= '{}' AND created_at < '{}'", start.to_rfc3339(), end.to_rfc3339()));
         let events = query_paged_as!(
             UserEventDAO,
             "user_events",
