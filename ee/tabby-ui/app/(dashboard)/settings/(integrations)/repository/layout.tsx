0c6b508924afdc07e0593f4749aa29438b96f1dc
Meng Zhang <meng@tabbyml.com>
2024-04-25 14:09:49 -0700

    consolidate layouts

--- ee/tabby-ui/app/(dashboard)/settings/(integrations)/repository/layout.tsx
+++ ee/tabby-ui/app/(dashboard)/settings/(integrations)/repository/layout.tsx
@@ -1,9 +1,23 @@
-import GitTabsHeader from './components/tabs-header'
+import { SubHeader } from '@/components/sub-header'
+import RepositoryTabsHeader from './components/tabs-header'
+
+const Header = ({ className }: { className?: string }) => {
+  return (
+    <SubHeader
+      className={className}
+      externalLink="https://tabby.tabbyml.com/blog/2023/10/16/repository-context-for-code-completion"
+    >
+      Connect to Git repositories and uses these repositories as a context to
+      enhance performance of large language model.
+    </SubHeader>
+  )
+}
 
 export default function GitLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <GitTabsHeader />
+      <Header />
+      <RepositoryTabsHeader />
       {children}
     </>
   )
