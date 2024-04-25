3c8db4cb6c0b341364964626c42ea728df984776
boxbeam <instantiable@gmail.com>
2024-04-25 19:51:10 -0400

    Fix ui

--- ee/tabby-ui/app/(dashboard)/settings/(integrations)/repository/github/detail/components/provider-detail-form.tsx
+++ ee/tabby-ui/app/(dashboard)/settings/(integrations)/repository/github/detail/components/provider-detail-form.tsx
@@ -36,7 +36,7 @@ const deleteGithubRepositoryProviderMutation = graphql(/* GraphQL */ `
 `)
 
 const updateGithubRepositoryProviderMutation = graphql(/* GraphQL */ `
-  mutation UpdateRepositoryProvider($input: UpdateRepositoryProviderInput!) {
+  mutation UpdateGithubRepositoryProvider($input: UpdateRepositoryProviderInput!) {
     updateGithubRepositoryProvider(input: $input)
   }
 `)
