9b058129ecc592c08027d9375f299e96e07911b1
boxbeam <instantiable@gmail.com>
2024-04-26 15:12:03 -0400

    Fix migrations

--- ee/tabby-db/migrations/0027_remove-github-repository-columns-on-oauth.up.sql
+++ ee/tabby-db/migrations/0027_remove-github-repository-columns-on-oauth.up.sql
@@ -1,8 +1,8 @@
+DROP TABLE github_repository_provider;
+
 CREATE TABLE github_repository_provider(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     display_name TEXT NOT NULL,
-    application_id TEXT NOT NULL,
-    secret TEXT NOT NULL,
     access_token TEXT,
-    CONSTRAINT `idx_application_id` UNIQUE (`application_id`)
+    synced_at TIMESTAMP
 );
