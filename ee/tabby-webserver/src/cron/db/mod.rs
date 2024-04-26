607efd36707cd48981f0a3eb2e7aa0d193b4702b
Meng Zhang <meng@tabbyml.com>
2024-04-26 13:51:54 -0700

    chore(webserver): improve error log for each cron job to make it easi… (#1982)
    * chore(webserver): improve error log for each cron job to make it easier to identify the stack trace
    
    * cleanup dir structure
    
    * update

--- ee/tabby-webserver/src/cron/db/mod.rs
+++ ee/tabby-webserver/src/cron/db/mod.rs
@@ -1,5 +1,8 @@
 //! db maintenance jobs
 
+mod github;
+mod gitlab;
+
 use std::{sync::Arc, time::Duration};
 
 use anyhow::Result;
@@ -7,7 +10,6 @@ use futures::Future;
 use tokio_cron_scheduler::Job;
 use tracing::{debug, error};
 
-use super::{github, gitlab};
 use crate::schema::{
     auth::AuthenticationService, github_repository_provider::GithubRepositoryProviderService,
     gitlab_repository_provider::GitlabRepositoryProviderService, job::JobService,
@@ -17,6 +19,7 @@ const EVERY_TWO_HOURS: &str = "0 0 1/2 * * * *";
 const EVERY_TEN_MINUTES: &str = "0 1/10 * * * *";
 
 async fn service_job<F, S>(
+    name: &str,
     frequency: &'static str,
     service: Arc<S>,
     job: fn(Arc<S>) -> F,
@@ -25,12 +28,14 @@ where
     F: Future<Output = Result<()>> + 'static + Send,
     S: Send + Sync + 'static + ?Sized,
 {
+    let name = name.to_owned();
     let job = Job::new_async(frequency, move |_, _| {
+        let name = name.clone();
         let auth = service.clone();
         Box::pin(async move {
             let res = job(auth.clone()).await;
             if let Err(e) = res {
-                error!("failed to run cleanup job: {}", e);
+                error!("Failed to run `{name}` job: {}", e);
             }
         })
     })?;
@@ -39,16 +44,22 @@ where
 }
 
 pub async fn refresh_token_job(auth: Arc<dyn AuthenticationService>) -> Result<Job> {
-    service_job(EVERY_TWO_HOURS, auth, |auth| async move {
-        Ok(auth.delete_expired_token().await?)
-    })
+    service_job(
+        "cleanup staled refresh token",
+        EVERY_TWO_HOURS,
+        auth,
+        |auth| async move { Ok(auth.delete_expired_token().await?) },
+    )
     .await
 }
 
 pub async fn password_reset_job(auth: Arc<dyn AuthenticationService>) -> Result<Job> {
-    service_job(EVERY_TWO_HOURS, auth, |auth| async move {
-        Ok(auth.delete_expired_password_resets().await?)
-    })
+    service_job(
+        "cleanup staled password reset",
+        EVERY_TWO_HOURS,
+        auth,
+        |auth| async move { Ok(auth.delete_expired_password_resets().await?) },
+    )
     .await
 }
 
@@ -56,6 +67,7 @@ pub async fn update_integrated_github_repositories_job(
     github_repository_provider: Arc<dyn GithubRepositoryProviderService>,
 ) -> Result<Job> {
     service_job(
+        "sync github repositories",
         EVERY_TEN_MINUTES,
         github_repository_provider,
         |github_repository_provider| async move {
@@ -70,6 +82,7 @@ pub async fn update_integrated_gitlab_repositories_job(
     gitlab_repository_provider: Arc<dyn GitlabRepositoryProviderService>,
 ) -> Result<Job> {
     service_job(
+        "sync gitlab repositories",
         EVERY_TEN_MINUTES,
         gitlab_repository_provider,
         |gitlab_repository_provider| async move {
--- ee/tabby-webserver/src/cron/mod.rs
+++ ee/tabby-webserver/src/cron/mod.rs
@@ -1,6 +1,4 @@
 mod db;
-mod github;
-mod gitlab;
 mod scheduler;
 
 use std::sync::Arc;
