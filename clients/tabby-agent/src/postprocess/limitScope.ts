edb23ff9955e8e6bffa579e73b2ce672208405e9
Zhiming Ma <codes.icy@gmail.com>
2024-04-26 22:44:20 +0800

    chore(agent): clean up experimental post-processing features. (#1973)
    * chore(agent): clean up experimental syntax features.
    
    * fix: lint

--- clients/tabby-agent/src/postprocess/limitScope.ts
+++ clients/tabby-agent/src/postprocess/limitScope.ts
@@ -1,23 +1,10 @@
 import { CompletionContext } from "../CompletionContext";
 import { AgentConfig } from "../AgentConfig";
-import { isBrowser } from "../env";
-import { PostprocessFilter, logger } from "./base";
+import { PostprocessFilter } from "./base";
 import { limitScopeByIndentation } from "./limitScopeByIndentation";
-import { limitScopeBySyntax } from "./limitScopeBySyntax";
 
 export function limitScope(config: AgentConfig["postprocess"]["limitScope"]): PostprocessFilter {
   return async (input: string, context: CompletionContext) => {
-    const preferSyntaxParser =
-      !isBrowser && // syntax parser is not supported in browser yet
-      config.experimentalSyntax;
-
-    if (preferSyntaxParser) {
-      try {
-        return await limitScopeBySyntax()(input, context);
-      } catch (error) {
-        logger.debug({ error }, "Failed to limit scope by syntax parser");
-      }
-    }
-    return limitScopeByIndentation(config["indentation"])(input, context);
+    return limitScopeByIndentation(config)(input, context);
   };
 }
