d99eb9047b1aa62607e9d8f07319dd74cba681b0
liangfung <1098486429@qq.com>
2024-04-27 14:42:07 +0800

    update

--- ee/tabby-ui/components/ui/form.tsx
+++ ee/tabby-ui/components/ui/form.tsx
@@ -137,7 +137,7 @@ const FormDescription = React.forwardRef<
   const { formDescriptionId } = useFormField()
 
   return (
-    <p
+    <div
       ref={ref}
       id={formDescriptionId}
       className={cn('text-sm text-muted-foreground', className)}
