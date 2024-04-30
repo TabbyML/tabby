2cf0082c25716ad8eb967205ec96d88ac18dd906
Meng Zhang <meng@tabbyml.com>
2024-04-29 21:47:30 -0700

    feat(webserver): add is_demo_mode to ServerInfo (#2011)
    * feat(webserver): add is_demo_mode to ServerInfo
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- ee/tabby-webserver/src/schema/mod.rs
+++ ee/tabby-webserver/src/schema/mod.rs
@@ -44,6 +44,7 @@ use self::{
 };
 use crate::{
     axum::FromAuth,
+    env,
     juniper::relay::{self, Connection},
 };
 
@@ -418,6 +419,7 @@ impl Query {
             is_chat_enabled: ctx.locator.worker().is_chat_enabled().await?,
             is_email_configured: ctx.locator.email().read_setting().await?.is_some(),
             allow_self_signup: ctx.locator.auth().allow_self_signup().await?,
+            is_demo_mode: env::demo_mode(),
         })
     }
 
@@ -514,6 +516,7 @@ pub struct ServerInfo {
     is_chat_enabled: bool,
     is_email_configured: bool,
     allow_self_signup: bool,
+    is_demo_mode: bool,
 }
 
 #[derive(Default)]
