de515f235bcddd9faf1e345b004735d89467dd2e
boxbeam <instantiable@gmail.com>
2024-05-08 21:49:28 -0400

    Apply suggestions

--- ee/tabby-db/migrations/0029_merged-provider-tables.up.sql
+++ ee/tabby-db/migrations/0029_merged-provider-tables.up.sql
@@ -9,19 +9,13 @@ CREATE TABLE integration_access_tokens(
 
 CREATE TABLE provided_repositories(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    access_token_provider_id INTEGER NOT NULL,
+    integration_access_token_id INTEGER NOT NULL,
     vendor_id TEXT NOT NULL,
     name TEXT NOT NULL,
     git_url TEXT NOT NULL,
     active BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
     updated_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
-    FOREIGN KEY (access_token_provider_id) REFERENCES integration_access_tokens(id),
-    CONSTRAINT idx_unique_provider_id_vendor_id UNIQUE (access_token_provider_id, vendor_id)
+    FOREIGN KEY (integration_access_token_id) REFERENCES integration_access_tokens(id) ON DELETE CASCADE,
+    CONSTRAINT idx_unique_provider_id_vendor_id UNIQUE (integration_access_token_id, vendor_id)
 );
-
-INSERT INTO integration_access_tokens (kind, display_name, access_token)
-    SELECT 'github', display_name, access_token FROM github_repository_provider;
-
-INSERT INTO integration_access_tokens (kind, display_name, access_token)
-    SELECT 'gitlab', display_name, access_token FROM gitlab_repository_provider;
