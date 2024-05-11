9c2a08d4b2f5dcd23e8a60e81f610cc0004908d6
Meng Zhang <meng@tabbyml.com>
2024-05-10 17:18:40 -0700

    feat(scheduler): add crawler to scheduler (#2066)
    * feat(crawl): add crawler to scheduler
    
    * fix
    
    * fix
    
    * update
    
    * update

--- crates/tabby-scheduler/src/lib.rs
+++ crates/tabby-scheduler/src/lib.rs
@@ -80,3 +80,5 @@ fn job_sync(cache: &mut CacheStore, repositories: &[RepositoryConfig]) {
     println!("Creating dataset...");
     dataset::create_dataset(cache, repositories);
 }
+
+pub mod crawl;
