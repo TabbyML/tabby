e54a101ca22bef97dc10082bfe68983e6c961cec
Meng Zhang <meng@tabbyml.com>
2024-05-13 15:54:57 -0700

    update

---
+++ crates/llama-cpp-server/build.rs
@@ -0,0 +1,63 @@
+use std::env;
+
+use cmake::Config;
+use omnicopy_to_output::copy_to_output_by_path;
+
+fn main() {
+    let mut config = Config::new("../llama-cpp-bindings/llama.cpp");
+    config.profile("Release");
+    config.define("LLAMA_NATIVE", "OFF");
+    config.define("INS_ENB", "ON");
+
+    if cfg!(target_os = "macos") {
+        config.define("LLAMA_METAL", "ON");
+        config.define("LLAMA_METAL_EMBED_LIBRARY", "ON");
+        println!("cargo:rustc-link-lib=framework=Foundation");
+        println!("cargo:rustc-link-lib=framework=Accelerate");
+        println!("cargo:rustc-link-lib=framework=Metal");
+        println!("cargo:rustc-link-lib=framework=MetalKit");
+    }
+    if cfg!(feature = "cuda") {
+        config.define("LLAMA_CUBLAS", "ON");
+        config.define("CMAKE_POSITION_INDEPENDENT_CODE", "ON");
+    }
+    if cfg!(feature = "rocm") {
+        let amd_gpu_targets: Vec<&str> = vec![
+            "gfx803",
+            "gfx900",
+            "gfx906:xnack-",
+            "gfx908:xnack-",
+            "gfx90a:xnack+",
+            "gfx90a:xnack-",
+            "gfx940",
+            "gfx941",
+            "gfx942",
+            "gfx1010",
+            "gfx1012",
+            "gfx1030",
+            "gfx1031",
+            "gfx1100",
+            "gfx1101",
+            "gfx1102",
+            "gfx1103",
+        ];
+
+        let rocm_root = env::var("ROCM_ROOT").unwrap_or("/opt/rocm".to_string());
+        config.define("LLAMA_HIPBLAS", "ON");
+        config.define("CMAKE_C_COMPILER", format!("{}/llvm/bin/clang", rocm_root));
+        config.define(
+            "CMAKE_CXX_COMPILER",
+            format!("{}/llvm/bin/clang++", rocm_root),
+        );
+        config.define("AMDGPU_TARGETS", amd_gpu_targets.join(";"));
+    }
+    if cfg!(feature = "vulkan") {
+        config.define("LLAMA_VULKAN", "ON");
+    }
+
+    let out = config.build();
+    let server_binary = out.join("bin").join("server");
+
+    copy_to_output_by_path(&server_binary)
+        .expect("Failed to copy server binary to output directory");
+}
