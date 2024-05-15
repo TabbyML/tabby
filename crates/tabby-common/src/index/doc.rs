3fd7fa9b91f70d5439f66b4b96e2ec1f3701e692
Meng Zhang <meng@tabbyml.com>
2024-05-14 19:01:55 -0700

    feat: implement chunk based nested indexing with tantivy (#2124)
    * add feature
    
    * lift binarize embedding implementation to DocSearchSchema
    
    * implement chunk based indexing

--- crates/tabby-common/src/index/doc.rs
+++ crates/tabby-common/src/index/doc.rs
@@ -2,16 +2,19 @@ use tantivy::schema::{Field, Schema, STORED, STRING};
 
 pub struct DocSearchSchema {
     pub schema: Schema,
+
+    // === Fields for document ===
     pub field_id: Field,
-
-    /// Binarized embedding tokens with the following mapping:
-    /// * [-1, 0] -> 0
-    /// * (0, 1] -> 1
-    pub field_embedding_token: Field,
-
     pub field_title: Field,
     pub field_link: Field,
     pub field_body: Field,
+
+    // === Fields for chunk ===
+    pub field_chunk_id: Field,
+    // Binarized embedding tokens with the following mapping:
+    // * [-1, 0] -> 0
+    // * (0, 1] -> 1
+    pub field_chunk_embedding_token: Field,
 }
 
 impl DocSearchSchema {
@@ -19,21 +22,37 @@ impl DocSearchSchema {
         let mut builder = Schema::builder();
 
         let field_id = builder.add_text_field("id", STRING | STORED);
-        let field_embedding_token = builder.add_text_field("embedding_token", STRING);
         let field_title = builder.add_text_field("title", STORED);
         let field_link = builder.add_text_field("link", STORED);
         let field_body = builder.add_text_field("body", STORED);
+
+        let field_chunk_id = builder.add_text_field("chunk_id", STRING | STORED);
+        let field_chunk_embedding_token = builder.add_text_field("chunk_embedding_token", STRING);
 
         let schema = builder.build();
 
         Self {
             schema,
             field_id,
-            field_embedding_token,
             field_title,
             field_link,
             field_body,
+
+            field_chunk_id,
+            field_chunk_embedding_token,
         }
+    }
+
+    pub fn binarize_embedding<'a>(
+        embedding: impl Iterator<Item = &'a f32> + 'a,
+    ) -> impl Iterator<Item = String> + 'a {
+        embedding.enumerate().map(|(i, value)| {
+            if *value <= 0.0 {
+                format!("embedding_zero_{}", i)
+            } else {
+                format!("embedding_one_{}", i)
+            }
+        })
     }
 }
 
--- crates/tabby-scheduler/src/doc/mod.rs
+++ crates/tabby-scheduler/src/doc/mod.rs
@@ -1,9 +1,11 @@
-use std::{collections::HashSet, sync::Arc};
+use std::sync::Arc;
 
+use async_stream::stream;
+use futures::{Stream, StreamExt};
 use tabby_common::{index::DocSearchSchema, path};
 use tabby_inference::Embedding;
-use tantivy::{doc, IndexWriter, Term};
-use text_splitter::{Characters, TextSplitter};
+use tantivy::{doc, IndexWriter, TantivyDocument, Term};
+use text_splitter::TextSplitter;
 use tracing::warn;
 
 use crate::tantivy_utils::open_or_create_index;
@@ -19,18 +21,9 @@ pub struct DocIndex {
     embedding: Arc<dyn Embedding>,
     doc: DocSearchSchema,
     writer: IndexWriter,
-    splitter: TextSplitter<Characters>,
 }
 
 const CHUNK_SIZE: usize = 2048;
-
-fn make_embedding_token(i: usize, is_one: bool) -> String {
-    if is_one {
-        format!("embedding_one_{i}")
-    } else {
-        format!("embedding_zero_{i}")
-    }
-}
 
 impl DocIndex {
     pub fn new(embedding: Arc<dyn Embedding>) -> Self {
@@ -44,7 +37,6 @@ impl DocIndex {
             embedding,
             doc,
             writer,
-            splitter: TextSplitter::default().with_trim_chunks(true),
         }
     }
 
@@ -53,52 +45,62 @@ impl DocIndex {
         self.writer
             .delete_term(Term::from_field_text(self.doc.field_id, &document.id));
 
-        let Some(embedding_tokens) = self.compute_embedding_tokens(&document.body).await else {
-            warn!(
-                "Failed to compute embedding tokens for document '{}'",
-                document.id
-            );
-            return;
-        };
+        self.iter_docs(document)
+            .await
+            .for_each(|doc| async {
+                self.writer
+                    .add_document(doc)
+                    .expect("Failed to add document");
+            })
+            .await;
+    }
 
-        let mut doc = doc! {
+    async fn iter_docs(&self, document: SourceDocument) -> impl Stream<Item = TantivyDocument> {
+        let id = document.id.clone();
+        let content = document.body.clone();
+
+        let doc = doc! {
             self.doc.field_id => document.id,
             self.doc.field_title => document.title,
             self.doc.field_link => document.link,
             self.doc.field_body => document.body,
         };
 
-        for token in embedding_tokens {
-            doc.add_text(self.doc.field_embedding_token, token);
-        }
-
-        // Add the document
-        self.writer
-            .add_document(doc)
-            .expect("Failed to add document");
+        futures::stream::once(async { doc }).chain(self.iter_chunks(id, content).await)
     }
 
     /// This function splits the document into chunks and computes the embedding for each chunk. It then converts the embeddings
     /// into binarized tokens by thresholding on zero.
-    ///
-    /// The current implementation deduplicates tokens at the document level, but this may require further consideration in the future.
-    async fn compute_embedding_tokens(&self, content: &str) -> Option<Vec<String>> {
-        let mut tokens = HashSet::new();
-        for chunk in self.splitter.chunks(content, CHUNK_SIZE) {
-            let embedding = match self.embedding.embed(chunk).await {
-                Ok(embedding) => embedding,
-                Err(e) => {
-                    warn!("Failed to embed document: {}", e);
-                    return None;
-                }
-            };
+    async fn iter_chunks(
+        &self,
+        id: String,
+        content: String,
+    ) -> impl Stream<Item = TantivyDocument> {
+        let splitter = TextSplitter::default().with_trim_chunks(true);
+        let embedding = self.embedding.clone();
 
-            for (i, value) in embedding.iter().enumerate() {
-                tokens.insert(make_embedding_token(i, *value > 0.0));
+        let field_id = self.doc.field_id;
+        let field_chunk_id = self.doc.field_chunk_id;
+        let field_chunk_embedding_token = self.doc.field_chunk_embedding_token;
+        stream! {
+            for (chunk_id, chunk) in splitter.chunks(&content, CHUNK_SIZE).enumerate() {
+                let mut doc = doc! {
+                    field_id => id.clone(),
+                    field_chunk_id => chunk_id.to_string()
+                };
+
+                let Ok(embedding) = embedding.embed(chunk).await else {
+                    warn!("Failed to embed chunk {} of document '{}'", chunk_id, id);
+                    continue;
+                };
+
+                for token in DocSearchSchema::binarize_embedding(embedding.iter()) {
+                    doc.add_text(field_chunk_embedding_token, token);
+                }
+
+                yield doc;
             }
         }
-
-        Some(tokens.into_iter().collect())
     }
 
     pub fn delete(&mut self, id: &str) {
@@ -111,5 +113,64 @@ impl DocIndex {
         self.writer
             .wait_merging_threads()
             .expect("Failed to wait for merging threads");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use async_trait::async_trait;
+    use tantivy::schema::Value;
+
+    use super::*;
+
+    struct DummyEmbedding;
+
+    #[async_trait]
+    impl Embedding for DummyEmbedding {
+        async fn embed(&self, prompt: &str) -> anyhow::Result<Vec<f32>> {
+            Ok(vec![0.0; prompt.len()])
+        }
+    }
+
+    fn new_test_index() -> DocIndex {
+        DocIndex::new(Arc::new(DummyEmbedding))
+    }
+
+    fn is_empty(doc: &TantivyDocument, field: tantivy::schema::Field) -> bool {
+        doc.get_first(field).is_none()
+    }
+
+    fn get_text(doc: &TantivyDocument, field: tantivy::schema::Field) -> String {
+        doc.get_first(field).unwrap().as_str().unwrap().to_string()
+    }
+
+    #[tokio::test]
+    async fn test_iter_docs() {
+        let index = new_test_index();
+        let document = SourceDocument {
+            id: "test".to_string(),
+            title: "Test".to_string(),
+            link: "https://example.com".to_string(),
+            body: "Hello, world!".to_string(),
+        };
+        let docs = index.iter_docs(document).await.collect::<Vec<_>>().await;
+        assert_eq!(2, docs.len());
+
+        // Check document
+        assert_eq!("test", get_text(&docs[0], index.doc.field_id));
+        assert!(is_empty(&docs[0], index.doc.field_chunk_id));
+        assert!(is_empty(&docs[0], index.doc.field_chunk_embedding_token));
+
+        // Check chunks.
+        assert_eq!("test", get_text(&docs[1], index.doc.field_id));
+        assert!(is_empty(&docs[1], index.doc.field_title));
+        assert!(is_empty(&docs[1], index.doc.field_link));
+        assert!(is_empty(&docs[1], index.doc.field_body));
+
+        assert_eq!("0", get_text(&docs[1], index.doc.field_chunk_id));
+        assert_eq!(
+            "embedding_zero_0",
+            get_text(&docs[1], index.doc.field_chunk_embedding_token)
+        );
     }
 }
