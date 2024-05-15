2cc133983c884e7ca016e148c949a1b386bfbbfc
Meng Zhang <meng@tabbyml.com>
2024-05-15 09:53:59 -0700

    test(scheduler): add unit test for CodeIntelligence (#2133)

---
+++ crates/tabby-scheduler/testdata/repositories/https_github.com_TabbyML_tabby/rust.rs
@@ -0,0 +1,34 @@
+mod metrics {
+    use std::cmp::max;
+
+    pub fn max_line_length(content: &str) -> usize {
+        content.lines().map(|x| x.len()).reduce(max).unwrap_or(0)
+    }
+
+    pub fn avg_line_length(content: &str) -> f32 {
+        let mut total = 0;
+        let mut len = 0;
+        for x in content.lines() {
+            len += 1;
+            total += x.len();
+        }
+
+        if len > 0 {
+            total as f32 / len as f32
+        } else {
+            0.0
+        }
+    }
+
+    pub fn alphanum_fraction(content: &str) -> f32 {
+        let num_alphanumn: f32 = content
+            .chars()
+            .map(|x| f32::from(u8::from(x.is_alphanumeric())))
+            .sum();
+        if !content.is_empty() {
+            num_alphanumn / content.len() as f32
+        } else {
+            0.0
+        }
+    }
+}
