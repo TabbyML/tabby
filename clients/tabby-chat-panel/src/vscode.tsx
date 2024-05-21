3e8b0f44808ffa9761c4ed4297cab6508ad1e6ee
wwayne <wayne.wang0821@gmail.com>
2024-05-21 15:38:54 +0800

    feat(chat-panel): add vscode bundle script

---
+++ clients/tabby-chat-panel/src/vscode.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import type { Context } from './index'
+import { useClient } from './react';
+
+declare global {
+  interface Window {
+    token?: string;
+    endpoint?: string;
+  }
+}
+
+function ChatPanel () {
+  const [endpoint, setEndpoint] = React.useState("")
+  const [token, setToken] = React.useState("")
+  const iframeRef = React.useRef<HTMLIFrameElement>(null)
+
+  const client = useClient(iframeRef, {
+    navigate: (context: Context) => {
+      // FIXME(wwayne): Send message to VSCode webview to invoke VSCode API
+    }
+  })
+
+  React.useEffect(() => {
+    setEndpoint(window.endpoint || "")
+    setToken(window.token || "")
+  }, [])
+
+  React.useEffect(() => {
+    if (iframeRef?.current && token) {
+      client?.init({
+        fetcherOptions: {
+          authorization: token
+        }
+      })
+    }
+  }, [iframeRef?.current, client, token])
+
+  return (
+    <iframe
+      src={`${endpoint}/chat`}
+      style={{
+        width: '100vw',
+        height: '100vh'
+      }}
+      ref={iframeRef} />
+  )
+}
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <ChatPanel />
+);
