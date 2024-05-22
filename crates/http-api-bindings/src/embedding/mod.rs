4a5c71a3a8f055a27baae56c1726b96142246cbb
Meng Zhang <meng@tabbyml.com>
2024-05-22 14:02:17 -0700

    feat(core): Add OpenAI embedding engine to http-api-bindings

--- crates/http-api-bindings/src/embedding/mod.rs
+++ crates/http-api-bindings/src/embedding/mod.rs
@@ -1,4 +1,5 @@
 mod llama;
+mod openai;
 
 use std::sync::Arc;
 
@@ -6,9 +7,18 @@ use llama::LlamaCppEngine;
 use tabby_common::config::HttpModelConfig;
 use tabby_inference::Embedding;
 
+use self::openai::OpenAIEmbeddingEngine;
+
 pub fn create(config: &HttpModelConfig) -> Arc<dyn Embedding> {
     if config.kind == "llama.cpp/embedding" {
         let engine = LlamaCppEngine::create(&config.api_endpoint, config.api_key.clone());
+        Arc::new(engine)
+    } else if config.kind == "openai-embedding" {
+        let engine = OpenAIEmbeddingEngine::create(
+            &config.api_endpoint,
+            config.model_name.as_deref().unwrap_or_default(),
+            config.api_key.clone(),
+        );
         Arc::new(engine)
     } else {
         panic!("Only llama are supported for http embedding");
