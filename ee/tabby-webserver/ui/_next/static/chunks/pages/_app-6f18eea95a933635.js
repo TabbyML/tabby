9b5bc4f3800594a94d360bdccd1d5c901831886b
Meng Zhang <meng@tabbyml.com>
2024-05-27 16:11:15 +0800

    chore(webserver): update ui

---
+++ ee/tabby-webserver/ui/_next/static/chunks/pages/_app-6f18eea95a933635.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2888],{10238:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(47223)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[9774,179],function(){return _(10238),_(1067)}),_N_E=n.O()}]);
---
+++ ee/tabby-webserver/ui/_next/static/chunks/pages/_error-e9871f0a501df54a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4820],{20079:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(87805)}])}},function(n){n.O(0,[2888,9774,179],function(){return n(n.s=20079)}),_N_E=n.O()}]);
