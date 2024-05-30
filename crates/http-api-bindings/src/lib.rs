9230bb400c18f3d8a54f3e6de61665566f6a61eb
Meng Zhang <meng@tabbyml.com>
2024-05-30 08:11:45 +0800

    feat(core): Add mistral/completion for fim completion

--- crates/http-api-bindings/src/lib.rs
+++ crates/http-api-bindings/src/lib.rs
@@ -3,5 +3,5 @@ mod completion;
 mod embedding;
 
 pub use chat::create as create_chat;
-pub use completion::create;
+pub use completion::{build_completion_prompt, create};
 pub use embedding::create as create_embedding;
