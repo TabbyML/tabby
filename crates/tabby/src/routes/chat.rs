6a898f1e5be9d71c68ea8fca2e81dfa7821d2d5e
Meng Zhang <meng@tabbyml.com>
2024-05-31 12:59:13 +0800

    Revert "feat(tabby): populate user field for chat completion logging (#2302)"
    This reverts commit 88cbeaa95fe7c0504e376620a40fb86c1158015a.

--- crates/tabby/src/routes/chat.rs
+++ crates/tabby/src/routes/chat.rs
@@ -5,11 +5,9 @@ use axum::{
     response::sse::{Event, KeepAlive, Sse},
     Json,
 };
-use axum_extra::TypedHeader;
 use futures::{Stream, StreamExt};
 use tracing::instrument;
 
-use super::MaybeUser;
 use crate::services::chat::{ChatCompletionRequest, ChatService};
 
 #[utoipa::path(
@@ -30,13 +28,8 @@ use crate::services::chat::{ChatCompletionRequest, ChatService};
 #[instrument(skip(state, request))]
 pub async fn chat_completions(
     State(state): State<Arc<ChatService>>,
-    TypedHeader(MaybeUser(user)): TypedHeader<MaybeUser>,
-    Json(mut request): Json<ChatCompletionRequest>,
+    Json(request): Json<ChatCompletionRequest>,
 ) -> Sse<impl Stream<Item = Result<Event, serde_json::Error>>> {
-    if let Some(user) = user {
-        request.user.replace(user);
-    }
-
     let stream = state.generate(request).await;
     Sse::new(stream.map(|chunk| match serde_json::to_string(&chunk) {
         Ok(s) => Ok(Event::default().data(s)),
