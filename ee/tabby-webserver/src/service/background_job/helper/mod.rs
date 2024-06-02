4ddf1d3a9fd0adf2fe40cdfec2e29f7a7ea5569b
Meng Zhang <meng@tabbyml.com>
2024-06-02 10:05:05 +0800

    update

--- ee/tabby-webserver/src/service/background_job/helper/mod.rs
+++ ee/tabby-webserver/src/service/background_job/helper/mod.rs
@@ -3,9 +3,6 @@ mod logger;
 
 pub use cron::CronStream;
 pub use logger::JobLogger;
-use tokio::sync::mpsc::error::SendError;
-
-use super::BackgroundJobEvent;
 
 pub trait Job {
     const NAME: &'static str;
