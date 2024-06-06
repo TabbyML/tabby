31fae30dbe3c63d5b4bd23b64c08f3bd509271e4
boxbeam <instantiable@gmail.com>
2024-06-05 23:09:05 -0400

    fix(webserver): report all errors in integration status (#2352)

--- ee/tabby-webserver/src/service/repository/third_party/fetch.rs
+++ ee/tabby-webserver/src/service/repository/third_party/fetch.rs
@@ -1,4 +1,3 @@
-use octocrab::GitHubError;
 use tabby_schema::integration::IntegrationKind;
 
 use self::{github::fetch_all_github_repos, gitlab::fetch_all_gitlab_repos};
@@ -16,26 +15,13 @@ pub async fn fetch_all_repos(
     kind: IntegrationKind,
     access_token: &str,
     api_base: &str,
-) -> Result<Vec<RepositoryInfo>, (anyhow::Error, bool)> {
+) -> Result<Vec<RepositoryInfo>, anyhow::Error> {
     match kind {
         IntegrationKind::Github | IntegrationKind::GithubSelfHosted => {
-            match fetch_all_github_repos(access_token, api_base).await {
-                Ok(repos) => Ok(repos),
-                Err(octocrab::Error::GitHub {
-                    source: source @ GitHubError { .. },
-                    ..
-                }) if source.status_code.is_client_error() => Err((source.into(), true)),
-                Err(e) => Err((e.into(), false)),
-            }
+            Ok(fetch_all_github_repos(access_token, api_base).await?)
         }
         IntegrationKind::Gitlab | IntegrationKind::GitlabSelfHosted => {
-            match fetch_all_gitlab_repos(access_token, api_base).await {
-                Ok(repos) => Ok(repos),
-                Err(e) => {
-                    let client_error = e.is_client_error();
-                    Err((e.into(), client_error))
-                }
-            }
+            Ok(fetch_all_gitlab_repos(access_token, api_base).await?)
         }
     }
 }
