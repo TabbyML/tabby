18a875f03408dc565bccf39f826d1b210bd66ddc
Meng Zhang <meng@tabbyml.com>
2024-06-11 23:11:45 +0800

    feat(webserver): add jobInfo / triggerJob graphql api (#2388)
    * feat(webserver): add jobInfo / triggerJob graphql api
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- ee/tabby-schema/src/schema/mod.rs
+++ ee/tabby-schema/src/schema/mod.rs
@@ -48,7 +48,7 @@ use self::{
     user_event::{UserEvent, UserEventService},
 };
 use crate::{
-    env,
+    bail, env,
     juniper::relay::{self, query_async, Connection},
 };
 
@@ -799,6 +799,12 @@ impl Mutation {
             .update_repository_active(id, active)
             .await?;
         Ok(true)
+    }
+
+    /// Trigger a job run given its command string.
+    async fn trigger_job_run(ctx: &Context, _command: String) -> Result<ID> {
+        check_admin(ctx).await?;
+        bail!("Not implemented")
     }
 }
 
