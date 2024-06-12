6f493e29f9913c371f3e947372918ab63df263d2
liangfung <1098486429@qq.com>
2024-06-12 14:35:26 +0800

    revert

--- ee/tabby-ui/components/ui/icons.tsx
+++ ee/tabby-ui/components/ui/icons.tsx
@@ -6,7 +6,6 @@ import {
   Blocks,
   BookOpenText,
   ChevronsDownUp,
-  CircleStop,
   GitFork,
   Layers2,
   Mail,
@@ -358,12 +357,19 @@ function IconRefresh({ className, ...props }: React.ComponentProps<'svg'>) {
   )
 }
 
-const IconStop = ({
-  className,
-  ...props
-}: React.ComponentProps<typeof CircleStop>) => (
-  <CircleStop className={cn('h4 w-4', className)} {...props} />
-)
+function IconStop({ className, ...props }: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={cn('h-4 w-4', className)}
+      {...props}
+    >
+      <path d="M128 24a104 104 0 1 0 104 104A104.11 104.11 0 0 0 128 24Zm0 192a88 88 0 1 1 88-88 88.1 88.1 0 0 1-88 88Zm24-120h-48a8 8 0 0 0-8 8v48a8 8 0 0 0 8 8h48a8 8 0 0 0 8-8v-48a8 8 0 0 0-8-8Zm-8 48h-32v-32h32Z" />
+    </svg>
+  )
+}
 
 function IconSidebar({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
