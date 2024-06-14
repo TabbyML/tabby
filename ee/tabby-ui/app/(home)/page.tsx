b74b6aa15f2cd6f3c88120da931389b097d7523c
wwayne <wayne.wang0821@gmail.com>
2024-06-14 17:03:47 +0800

    search header

--- ee/tabby-ui/app/(home)/page.tsx
+++ ee/tabby-ui/app/(home)/page.tsx
@@ -20,9 +20,10 @@ import { useMutation } from '@/lib/tabby/gql'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import { CardContent, CardFooter } from '@/components/ui/card'
-import { IconJetBrains, IconRotate, IconVSCode } from '@/components/ui/icons'
+import { IconJetBrains, IconRotate, IconVSCode, IconPlus, IconChevronLeft } from '@/components/ui/icons'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 import {
   Tooltip,
   TooltipContent,
@@ -49,6 +50,167 @@ const resetUserAuthTokenDocument = graphql(/* GraphQL */ `
     resetUserAuthToken
   }
 `)
+
+function MainPanel() {
+  const [searchFlag] = useEnableSearch()
+  const { data: healthInfo } = useHealth()
+  const [{ data }] = useMe()
+  const isChatEnabled = useIsChatEnabled()
+  const { theme } = useTheme()
+  const [isShowDemoBanner] = useShowDemoBanner()
+  const [isSearch, setIsSearch] = useState(false)
+  const [initialMsg, setInitialMsg] = useState('')
+  const elementRef = useRef<HTMLDivElement | null>(null)
+  const searchRef = useRef<SearchRef>(null)
+  const [isSearchDialogOpen, setIsSearchDialogOpen] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (elementRef.current) {
+        const disable =
+          elementRef.current.scrollHeight > elementRef.current.clientHeight
+        AOS.init({
+          once: true,
+          duration: 250,
+          disable
+        })
+      }
+    }, 100)
+  }, [elementRef.current])
+
+  useEffect(() => {
+    if (isSearch && searchRef.current) {
+      searchRef.current.onSubmitSearch(initialMsg)
+    }
+    if (!isSearch) {
+      document.title = 'Tabby'
+    }
+  }, [isSearch])
+
+  if (!healthInfo || !data?.me) return <></>
+
+  const onSearch = (question: string) => {
+    setInitialMsg(question)
+    setIsSearch(true)
+    setIsSearchDialogOpen(false)
+  }
+
+  const hideSearch = () => {
+    if (searchRef.current) {
+      searchRef.current.stop()
+    }
+    setIsSearch(false)
+  }
+
+  const style = isShowDemoBanner
+    ? { height: `calc(100vh - ${BANNER_HEIGHT})` }
+    : { height: '100vh' }
+  return (
+    <div className="transition-all" style={style}>
+      <header className="flex h-16 items-center justify-between px-4">
+        <div>
+          {isSearch && (
+            <div className="flex items-center gap-x-6">
+              <Button variant='ghost' className="text-sm text-muted-foreground">
+                <IconChevronLeft className="w-5 h-5 mr-1" />
+                Home
+              </Button>
+            </div>
+          )}
+        </div>
+        <div className="flex items-center gap-x-6">
+          <Dialog open={isSearchDialogOpen} onOpenChange={setIsSearchDialogOpen}>
+            <DialogTrigger asChild>
+              <Button variant='outline' className="text-sm shadow-none text-muted-foreground">
+                <IconPlus className="w-3 h-3 mr-1" />
+                New thread
+              </Button>
+            </DialogTrigger>
+            <DialogContent className="dialog-without-close-btn -mt-48 border-none bg-transparent shadow-none sm:max-w-4xl">
+              <div className="flex flex-col items-center">
+                <Image
+                  src={tabbyUrl}
+                  alt="logo"
+                  width={192}
+                  className="my-4 invert-0"
+                  data-aos="fade-down"
+                  data-aos-delay="150"
+                />
+                <p
+                  className="mb-6 flex scroll-m-20 items-center gap-2 text-sm tracking-tight text-white"
+                  data-aos="fade-down"
+                  data-aos-delay="100"
+                >
+                  <span>research</span>
+                  <Separator orientation="vertical" className="h-[80%]" />
+                  <span>develop</span>
+                  <Separator orientation="vertical" className="h-[80%]" />
+                  <span>debug</span>
+                </p>
+                <TextAreaSearch onSearch={onSearch} />
+              </div>
+            </DialogContent>
+          </Dialog>
+          
+          <ClientOnly>
+            <ThemeToggle />
+          </ClientOnly>
+          <UserPanel showHome={false} showSetting>
+            <UserAvatar className="h-10 w-10 border" />
+          </UserPanel>
+        </div>
+      </header>
+
+      <main
+        className={cn('h-[calc(100%-4rem)] overflow-auto py-10 lg:py-0', {
+          'lg:flex flex-col items-center justify-center': !isSearch
+        })}
+        ref={elementRef}
+      >
+        {!isSearch && (
+          <div className="mx-auto flex w-full flex-col items-center px-10 lg:-mt-[2vh] lg:max-w-4xl lg:px-0">
+            <Image
+              src={tabbyUrl}
+              alt="logo"
+              width={192}
+              className="my-4 invert dark:invert-0"
+              data-aos="fade-down"
+              data-aos-delay="150"
+            />
+            <p
+              className="mb-6 flex scroll-m-20 items-center gap-2 text-sm tracking-tight text-secondary-foreground"
+              data-aos="fade-down"
+              data-aos-delay="100"
+            >
+              <span>research</span>
+              <Separator orientation="vertical" className="h-[80%]" />
+              <span>develop</span>
+              <Separator orientation="vertical" className="h-[80%]" />
+              <span>debug</span>
+            </p>
+            {isChatEnabled && searchFlag.value && (
+              <div className="w-full" data-aos="fade-down">
+                <TextAreaSearch onSearch={onSearch} />
+              </div>
+            )}
+            <div className="mt-10 flex w-full flex-col gap-x-5 lg:flex-row">
+              <div
+                className="mb-10 w-full rounded-lg p-4 lg:mb-0 lg:w-[21rem]"
+                style={{ background: theme === 'dark' ? '#333' : '#e8e1d3' }}
+                data-aos="fade-up"
+                data-aos-delay="100"
+              >
+                <Configuration />
+              </div>
+              <Stats />
+            </div>
+          </div>
+        )}
+        {isSearch && <Search ref={searchRef} />}
+      </main>
+    </div>
+  )
+}
 
 function Configuration({ className }: { className?: string }) {
   const [{ data }, reexecuteQuery] = useMe()
@@ -153,133 +315,6 @@ function IDELink({
         <p>{name}</p>
       </TooltipContent>
     </Tooltip>
-  )
-}
-
-function MainPanel() {
-  const [searchFlag] = useEnableSearch()
-  const { data: healthInfo } = useHealth()
-  const [{ data }] = useMe()
-  const isChatEnabled = useIsChatEnabled()
-  const { theme } = useTheme()
-  const [isShowDemoBanner] = useShowDemoBanner()
-  const [isSearch, setIsSearch] = useState(false)
-  const [initialMsg, setInitialMsg] = useState('')
-  const elementRef = useRef<HTMLDivElement | null>(null)
-  const searchRef = useRef<SearchRef>(null)
-
-  useEffect(() => {
-    setTimeout(() => {
-      if (elementRef.current) {
-        const disable =
-          elementRef.current.scrollHeight > elementRef.current.clientHeight
-        AOS.init({
-          once: true,
-          duration: 250,
-          disable
-        })
-      }
-    }, 100)
-  }, [elementRef.current])
-
-  useEffect(() => {
-    if (isSearch && searchRef.current) {
-      searchRef.current.onSubmitSearch(initialMsg)
-    }
-    if (!isSearch) {
-      document.title = 'Tabby'
-    }
-  }, [isSearch])
-
-  if (!healthInfo || !data?.me) return <></>
-
-  const onSearch = (question: string) => {
-    setInitialMsg(question)
-    setIsSearch(true)
-  }
-
-  const hideSearch = () => {
-    if (searchRef.current) {
-      searchRef.current.stop()
-    }
-    setIsSearch(false)
-  }
-
-  const style = isShowDemoBanner
-    ? { height: `calc(100vh - ${BANNER_HEIGHT})` }
-    : { height: '100vh' }
-  return (
-    <div className="transition-all" style={style}>
-      <header className="flex h-16 items-center justify-between px-4">
-        <div>
-          {isSearch && (
-            <Image
-              src={theme === 'dark' ? logoDarkUrl : logoUrl}
-              className="cursor-pointer"
-              alt="logo"
-              width={theme === 'dark' ? 80 : 100}
-              onClick={hideSearch}
-            />
-          )}
-        </div>
-        <div className="flex items-center gap-x-6">
-          <ClientOnly>
-            <ThemeToggle />
-          </ClientOnly>
-          <UserPanel showHome={false} showSetting>
-            <UserAvatar className="h-10 w-10 border" />
-          </UserPanel>
-        </div>
-      </header>
-
-      <main
-        className={cn('h-[calc(100%-4rem)] overflow-auto py-10 lg:py-0', {
-          'lg:flex flex-col items-center justify-center': !isSearch
-        })}
-        ref={elementRef}
-      >
-        {!isSearch && (
-          <div className="mx-auto flex w-full flex-col items-center px-10 lg:-mt-[2vh] lg:max-w-4xl lg:px-0">
-            <Image
-              src={tabbyUrl}
-              alt="logo"
-              width={192}
-              className="my-4 invert dark:invert-0"
-              data-aos="fade-down"
-              data-aos-delay="150"
-            />
-            <p
-              className="mb-6 flex scroll-m-20 items-center gap-2 text-sm tracking-tight text-secondary-foreground"
-              data-aos="fade-down"
-              data-aos-delay="100"
-            >
-              <span>research</span>
-              <Separator orientation="vertical" className="h-[80%]" />
-              <span>develop</span>
-              <Separator orientation="vertical" className="h-[80%]" />
-              <span>debug</span>
-            </p>
-            {isChatEnabled && searchFlag.value && (
-              <div className="w-full" data-aos="fade-down">
-                <TextAreaSearch onSearch={onSearch} />
-              </div>
-            )}
-            <div className="mt-10 flex w-full flex-col gap-x-5 lg:flex-row">
-              <div
-                className="mb-10 w-full rounded-lg p-4 lg:mb-0 lg:w-[21rem]"
-                style={{ background: theme === 'dark' ? '#333' : '#e8e1d3' }}
-                data-aos="fade-up"
-                data-aos-delay="100"
-              >
-                <Configuration />
-              </div>
-              <Stats />
-            </div>
-          </div>
-        )}
-        {isSearch && <Search ref={searchRef} />}
-      </main>
-    </div>
   )
 }
 
