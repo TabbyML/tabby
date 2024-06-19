6ee418525296a874894509a6b5027cb3fd22765b
Wang Zixiao <wayne.wang0821@gmail.com>
2024-06-19 17:36:44 +0800

    chore(ui): render toml code syntax correctly (#2441)

--- ee/tabby-ui/components/ui/codeblock.tsx
+++ ee/tabby-ui/components/ui/codeblock.tsx
@@ -68,6 +68,7 @@ const CodeBlock: FC<Props> = memo(({ language, value, onCopyContent }) => {
     copyToClipboard(value)
   }
 
+  const languageForSyntax = language === 'toml' ? 'bash' : language
   return (
     <div className="codeblock relative w-full bg-zinc-950 font-sans">
       <div className="flex w-full items-center justify-between bg-zinc-800 px-6 py-2 pr-4 text-zinc-100">
@@ -85,7 +86,7 @@ const CodeBlock: FC<Props> = memo(({ language, value, onCopyContent }) => {
         </div>
       </div>
       <SyntaxHighlighter
-        language={language}
+        language={languageForSyntax}
         style={coldarkDark}
         PreTag="div"
         showLineNumbers
