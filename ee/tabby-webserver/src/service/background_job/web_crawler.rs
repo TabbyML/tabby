2eb5c2abe382df8ba5245bd3f72e198cb70c4e7e
boxbeam <instantiable@gmail.com>
2024-06-20 21:42:37 -0400

    fix(webserver): remove web crawler cron job, making it manual only (#2463)

--- ee/tabby-webserver/src/service/background_job/web_crawler.rs
+++ ee/tabby-webserver/src/service/background_job/web_crawler.rs
@@ -1,11 +1,8 @@
 use std::sync::Arc;
 
-use chrono::{DateTime, Utc};
 use tabby_inference::Embedding;
-use tabby_schema::{web_crawler::WebCrawlerService, Result};
-use tokio::sync::mpsc::UnboundedSender;
 
-use super::{helper::Job, BackgroundJobEvent};
+use super::helper::Job;
 
 pub struct WebCrawlerJob {
     url: String,
@@ -22,21 +19,5 @@ impl WebCrawlerJob {
 
     pub async fn run(self, embedding: Arc<dyn Embedding>) {
         tabby_scheduler::crawl_index_docs(&[self.url], embedding).await;
-    }
-
-    pub async fn cron(
-        _now: DateTime<Utc>,
-        sender: UnboundedSender<BackgroundJobEvent>,
-        web_crawler_service: Arc<dyn WebCrawlerService>,
-    ) -> Result<()> {
-        for url in web_crawler_service
-            .list_web_crawler_urls(None, None, None, None)
-            .await?
-        {
-            sender
-                .send(BackgroundJobEvent::WebCrawler(url.url))
-                .expect("Failed to enqueue web crawler job");
-        }
-        Ok(())
     }
 }
