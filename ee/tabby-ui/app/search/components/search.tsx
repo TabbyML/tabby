2a38cede3d02b448c758fd4e29d43b8f2e5894e7
Wang Zixiao <wayne.wang0821@gmail.com>
2024-06-21 16:52:04 +0800

    fix(ui): separate search path with home (#2469)
    * auto focus
    
    * fix(ui): seperate search path with home
    
    * clean
    
    * code comment

--- ee/tabby-ui/app/search/components/search.tsx
+++ ee/tabby-ui/app/search/components/search.tsx
@@ -2,15 +2,13 @@
 
 import {
   createContext,
-  ForwardedRef,
-  forwardRef,
   useContext,
   useEffect,
-  useImperativeHandle,
   useMemo,
   useRef,
   useState
 } from 'react'
+import { useRouter } from 'next/navigation'
 import { Message } from 'ai'
 import DOMPurify from 'dompurify'
 import { marked } from 'marked'
@@ -18,6 +16,7 @@ import { nanoid } from 'nanoid'
 import remarkGfm from 'remark-gfm'
 import remarkMath from 'remark-math'
 
+import { SESSION_STORAGE_KEY } from '@/lib/constants'
 import { useEnableSearch } from '@/lib/experiment-flags'
 import { useLatest } from '@/lib/hooks/use-latest'
 import { useIsChatEnabled } from '@/lib/hooks/use-server-info'
@@ -34,6 +33,7 @@ import {
 } from '@/components/ui/hover-card'
 import {
   IconBlocks,
+  IconChevronLeft,
   IconChevronRight,
   IconLayers,
   IconPlus,
@@ -46,9 +46,14 @@ import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
 import { Skeleton } from '@/components/ui/skeleton'
 import { ButtonScrollToBottom } from '@/components/button-scroll-to-bottom'
+import { ClientOnly } from '@/components/client-only'
 import { CopyButton } from '@/components/copy-button'
+import { BANNER_HEIGHT, useShowDemoBanner } from '@/components/demo-banner'
 import { MemoizedReactMarkdown } from '@/components/markdown'
 import TextAreaSearch from '@/components/textarea-search'
+import { ThemeToggle } from '@/components/theme-toggle'
+import { UserAvatar } from '@/components/user-avatar'
+import UserPanel from '@/components/user-panel'
 
 import './search.css'
 
@@ -75,11 +80,6 @@ type SearchContextValue = {
   onSubmitSearch: (question: string) => void
 }
 
-export interface SearchRef {
-  onSubmitSearch: (question: string) => void
-  stop: () => void
-}
-
 export const SearchContext = createContext<SearchContextValue>(
   {} as SearchContextValue
 )
@@ -101,16 +101,20 @@ const SOURCE_CARD_STYLE = {
   expand: 6.3
 }
 
-export function SearchRenderer({}, ref: ForwardedRef<SearchRef>) {
+export function Search() {
   const isChatEnabled = useIsChatEnabled()
   const [searchFlag] = useEnableSearch()
   const [conversation, setConversation] = useState<ConversationMessage[]>([])
   const [showStop, setShowStop] = useState(true)
   const [container, setContainer] = useState<HTMLDivElement | null>(null)
   const [title, setTitle] = useState('')
+  const [isReady, setIsReady] = useState(false)
   const [currentLoadindId, setCurrentLoadingId] = useState<string>('')
   const contentContainerRef = useRef<HTMLDivElement>(null)
   const [showSearchInput, setShowSearchInput] = useState(false)
+  const [isShowDemoBanner] = useShowDemoBanner()
+  const router = useRouter()
+  const initCheckRef = useRef(false)
 
   const { triggerRequest, isLoading, error, answer, stop } = useTabbyAnswer({
     fetcher: tabbyFetcher
@@ -118,27 +122,63 @@ export function SearchRenderer({}, ref: ForwardedRef<SearchRef>) {
 
   const isLoadingRef = useLatest(isLoading)
 
-  useImperativeHandle(
-    ref,
-    () => {
-      return {
-        onSubmitSearch,
-        stop
-      }
-    },
-    []
-  )
+  // Check sessionStorage for initial message or most recent conversation
+  useEffect(() => {
+    if (initCheckRef.current) return
 
+    initCheckRef.current = true
+
+    const initialMessage = sessionStorage.getItem(
+      SESSION_STORAGE_KEY.SEARCH_INITIAL_MSG
+    )
+    if (initialMessage) {
+      sessionStorage.removeItem(SESSION_STORAGE_KEY.SEARCH_INITIAL_MSG)
+      setIsReady(true)
+      onSubmitSearch(initialMessage)
+      return
+    }
+
+    const latesConversation = sessionStorage.getItem(
+      SESSION_STORAGE_KEY.SEARCH_LATEST_MSG
+    )
+    if (latesConversation) {
+      const conversation = JSON.parse(
+        latesConversation
+      ) as ConversationMessage[]
+      setConversation(conversation)
+
+      // Set title
+      if (conversation[0]) setTitle(conversation[0].content)
+
+      // Check if any answer is loading
+      const loadingIndex = conversation.findIndex(item => item.isLoading)
+      if (loadingIndex !== -1) {
+        const loadingAnswer = conversation[loadingIndex]
+        if (loadingAnswer) onRegenerateResponse(loadingAnswer.id, conversation)
+      }
+
+      setIsReady(true)
+      return
+    }
+
+    router.replace('/')
+  }, [])
+
+  // Set page title to the value of the first quesiton
   useEffect(() => {
     if (title) document.title = title
   }, [title])
 
+  // Display the input field with a delayed animatio
   useEffect(() => {
-    setTimeout(() => {
-      setShowSearchInput(true)
-    }, 500)
-  }, [])
+    if (isReady) {
+      setTimeout(() => {
+        setShowSearchInput(true)
+      }, 300)
+    }
+  }, [isReady])
 
+  // Initialize the reference to the ScrollArea used for scrolling to the bottom
   useEffect(() => {
     setContainer(
       contentContainerRef?.current?.children[1] as HTMLDivElement | null
@@ -206,6 +246,21 @@ export function SearchRenderer({}, ref: ForwardedRef<SearchRef>) {
     }
   }, [isLoading])
 
+  // Stop stream before closing the page
+  useEffect(() => {
+    return () => {
+      if (isLoadingRef.current) stop()
+    }
+  }, [])
+
+  // Save conversation into sessionStorage
+  useEffect(() => {
+    sessionStorage.setItem(
+      SESSION_STORAGE_KEY.SEARCH_LATEST_MSG,
+      JSON.stringify(conversation)
+    )
+  }, [conversation])
+
   const onSubmitSearch = (question: string) => {
     // FIXME: code query? extra from user's input?
     const previousMessages = conversation.map(message => ({
@@ -243,19 +298,21 @@ export function SearchRenderer({}, ref: ForwardedRef<SearchRef>) {
     if (!title) setTitle(question)
   }
 
-  const onRegenerateResponse = (id: string) => {
-    const targetAnswerIdx = conversation.findIndex(item => item.id === id)
+  const onRegenerateResponse = (
+    id: string,
+    conversationData?: ConversationMessage[]
+  ) => {
+    const data = conversationData || conversation
+    const targetAnswerIdx = data.findIndex(item => item.id === id)
     if (targetAnswerIdx < 1) return
     const targetQuestionIdx = targetAnswerIdx - 1
-    const targetQuestion = conversation[targetQuestionIdx]
+    const targetQuestion = data[targetQuestionIdx]
 
-    const previousMessages = conversation
-      .slice(0, targetQuestionIdx)
-      .map(message => ({
-        role: message.role,
-        id: message.id,
-        content: message.content
-      }))
+    const previousMessages = data.slice(0, targetQuestionIdx).map(message => ({
+      role: message.role,
+      id: message.id,
+      content: message.content
+    }))
     const newUserMessage = {
       role: 'user',
       id: targetQuestion.id,
@@ -267,7 +324,7 @@ export function SearchRenderer({}, ref: ForwardedRef<SearchRef>) {
       generate_relevant_questions: true
     }
 
-    const newConversation = [...conversation]
+    const newConversation = [...data]
     let newTargetAnswer = newConversation[targetAnswerIdx]
     newTargetAnswer.content = ''
     newTargetAnswer.relevant_documents = undefined
@@ -279,10 +336,13 @@ export function SearchRenderer({}, ref: ForwardedRef<SearchRef>) {
     triggerRequest(answerRequest)
   }
 
-  if (!searchFlag.value || !isChatEnabled) {
+  if (!searchFlag.value || !isChatEnabled || !isReady) {
     return <></>
   }
 
+  const style = isShowDemoBanner
+    ? { height: `calc(100vh - ${BANNER_HEIGHT})` }
+    : { height: '100vh' }
   return (
     <SearchContext.Provider
       value={{
@@ -291,84 +351,105 @@ export function SearchRenderer({}, ref: ForwardedRef<SearchRef>) {
         onSubmitSearch: onSubmitSearch
       }}
     >
-      <>
-        <ScrollArea className="h-full" ref={contentContainerRef}>
-          <div className="mx-auto px-4 pb-24 lg:max-w-4xl lg:px-0">
-            <div className="flex flex-col">
-              {conversation.map((item, idx) => {
-                if (item.role === 'user') {
-                  return (
-                    <div key={item.id + idx}>
-                      {idx !== 0 && <Separator />}
-                      <div className="pb-2 pt-8">
-                        <MessageMarkdown message={item.content} headline />
+      <div className="transition-all" style={style}>
+        <header className="flex h-16 items-center justify-between px-4">
+          <div className="flex items-center gap-x-6">
+            <Button
+              variant="ghost"
+              className="-ml-1 pl-0 text-sm text-muted-foreground"
+              onClick={() => router.back()}
+            >
+              <IconChevronLeft className="mr-1 h-5 w-5" />
+              Home
+            </Button>
+          </div>
+          <div className="flex items-center gap-x-6">
+            <ClientOnly>
+              <ThemeToggle />
+            </ClientOnly>
+            <UserPanel showHome={false} showSetting>
+              <UserAvatar className="h-10 w-10 border" />
+            </UserPanel>
+          </div>
+        </header>
+
+        <main className="h-[calc(100%-4rem)] overflow-auto pb-8 lg:pb-0">
+          <ScrollArea className="h-full" ref={contentContainerRef}>
+            <div className="mx-auto px-4 pb-24 lg:max-w-4xl lg:px-0">
+              <div className="flex flex-col">
+                {conversation.map((item, idx) => {
+                  if (item.role === 'user') {
+                    return (
+                      <div key={item.id + idx}>
+                        {idx !== 0 && <Separator />}
+                        <div className="pb-2 pt-8">
+                          <MessageMarkdown message={item.content} headline />
+                        </div>
                       </div>
-                    </div>
-                  )
-                }
-                if (item.role === 'assistant') {
-                  return (
-                    <div key={item.id + idx} className="pb-8 pt-2">
-                      <AnswerBlock
-                        answer={item}
-                        showRelatedQuestion={idx === conversation.length - 1}
-                      />
-                    </div>
-                  )
-                }
-                return <></>
+                    )
+                  }
+                  if (item.role === 'assistant') {
+                    return (
+                      <div key={item.id + idx} className="pb-8 pt-2">
+                        <AnswerBlock
+                          answer={item}
+                          showRelatedQuestion={idx === conversation.length - 1}
+                        />
+                      </div>
+                    )
+                  }
+                  return <></>
+                })}
+              </div>
+            </div>
+          </ScrollArea>
+
+          {container && (
+            <ButtonScrollToBottom
+              className="!fixed !bottom-[5.4rem] !right-4 !top-auto lg:!bottom-[3.8rem]"
+              container={container}
+              offset={100}
+            />
+          )}
+
+          <div
+            className={cn(
+              'fixed bottom-5 left-0 flex min-h-[5rem] w-full flex-col items-center gap-y-2',
+              {
+                'opacity-100 translate-y-0': showSearchInput,
+                'opacity-0 translate-y-10': !showSearchInput
+              }
+            )}
+            style={{ transition: 'all 0.35s ease-out' }}
+          >
+            <Button
+              className={cn('bg-background', {
+                'opacity-0 pointer-events-none': !showStop,
+                'opacity-100': showStop
               })}
+              style={{
+                transition: 'opacity 0.55s ease-out'
+              }}
+              variant="outline"
+              onClick={stop}
+            >
+              <IconStop className="mr-2" />
+              Stop generating
+            </Button>
+            <div className="relative z-20 flex justify-center self-stretch px-4">
+              <TextAreaSearch
+                onSearch={onSubmitSearch}
+                className="lg:max-w-4xl"
+                placeholder="Ask a follow up question"
+                isLoading={isLoading}
+              />
             </div>
           </div>
-        </ScrollArea>
-
-        {container && (
-          <ButtonScrollToBottom
-            className="!fixed !bottom-[5.4rem] !right-4 !top-auto lg:!bottom-[3.8rem]"
-            container={container}
-            offset={100}
-          />
-        )}
-
-        <div
-          className={cn(
-            'fixed bottom-5 left-0 flex min-h-[5rem] w-full flex-col items-center gap-y-2',
-            {
-              'opacity-100 translate-y-0': showSearchInput,
-              'opacity-0 translate-y-10': !showSearchInput
-            }
-          )}
-          style={{ transition: 'all 0.35s ease-out' }}
-        >
-          <Button
-            className={cn('bg-background', {
-              'opacity-0 pointer-events-none': !showStop,
-              'opacity-100': showStop
-            })}
-            style={{
-              transition: 'opacity 0.55s ease-out'
-            }}
-            variant="outline"
-            onClick={stop}
-          >
-            <IconStop className="mr-2" />
-            Stop generating
-          </Button>
-          <div className="relative z-20 flex justify-center self-stretch px-4">
-            <TextAreaSearch
-              onSearch={onSubmitSearch}
-              className="lg:max-w-4xl"
-              placeholder="Ask a follow up question"
-              isLoading={isLoading}
-            />
-          </div>
-        </div>
-      </>
+        </main>
+      </div>
     </SearchContext.Provider>
   )
 }
-
-export const Search = forwardRef<SearchRef>(SearchRenderer)
 
 function AnswerBlock({
   answer,
@@ -478,7 +559,7 @@ function AnswerBlock({
               <Button
                 className="flex items-center gap-x-1 px-1 font-normal text-muted-foreground"
                 variant="ghost"
-                onClick={onRegenerateResponse.bind(null, answer.id)}
+                onClick={() => onRegenerateResponse(answer.id)}
               >
                 <IconRefresh />
                 <p>Regenerate</p>
