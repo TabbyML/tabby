7c2d99c4514ff5d20d973e2752e986b5d9d71f92
wwayne <wayne.wang0821@gmail.com>
2024-06-21 17:24:58 +0800

    fix(ui): not found style

---
+++ ee/tabby-ui/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+  return (
+    <main className="flex h-screen w-screen items-center justify-center">
+      <div className="flex items-center">
+        <h1 className="mr-4 border-r border-foreground py-2 pr-4 text-xl">
+          404
+        </h1>
+        <p className="text-sm">This page could not be found.</p>
+      </div>
+    </main>
+  )
+}
