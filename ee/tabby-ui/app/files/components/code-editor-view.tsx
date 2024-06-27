ae5c58d57f5f9d2e1144001a8076356ee98a6f00
Jeff Daley <jeffdaley@users.noreply.github.com>
2024-06-27 11:24:34 -0400

    Disable gutterFolding in search results

--- ee/tabby-ui/app/files/components/code-editor-view.tsx
+++ ee/tabby-ui/app/files/components/code-editor-view.tsx
@@ -98,7 +98,6 @@ const CodeEditorView: React.FC<CodeEditorViewProps> = ({
         this.matches = matchDecorator.createDeco(view)
       }
       update(update: ViewUpdate) {
-        // Do something?
         this.matches = matchDecorator.updateDeco(update, this.matches)
       }
     },
@@ -138,29 +137,32 @@ const CodeEditorView: React.FC<CodeEditorViewProps> = ({
         }
       }),
 
-      !interactionsAreDisabled
-        ? selectLinesGutter({
+      interactionsAreDisabled
+        ? emptyExtension
+        : selectLinesGutter({
             onSelectLine: v => {
               if (v === -1 || isNaN(v)) return
               // todo support multi lines
               updateHash(formatLineHashForCodeBrowser({ start: v }))
             }
-          })
-        : emptyExtension,
-      foldGutter({
-        markerDOM(open) {
-          const dom = document.createElement('div')
-          dom.style.cursor = 'pointer'
-          if (open) {
-            dom.innerHTML =
-              '<svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style="display: inline-block; user-select: none; vertical-align: text-bottom; overflow: visible;"><path d="M12.78 5.22a.749.749 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.06 0L3.22 6.28a.749.749 0 1 1 1.06-1.06L8 8.939l3.72-3.719a.749.749 0 0 1 1.06 0Z"></path></svg>'
-          } else {
-            dom.innerHTML =
-              '<svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style="display: inline-block; user-select: none; vertical-align: text-bottom; overflow: visible;"><path d="M6.22 3.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L9.94 8 6.22 4.28a.75.75 0 0 1 0-1.06Z"></path></svg>'
-          }
-          return dom
-        }
-      }),
+          }),
+
+      interactionsAreDisabled
+        ? emptyExtension
+        : foldGutter({
+            markerDOM(open) {
+              const dom = document.createElement('div')
+              dom.style.cursor = 'pointer'
+              if (open) {
+                dom.innerHTML =
+                  '<svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style="display: inline-block; user-select: none; vertical-align: text-bottom; overflow: visible;"><path d="M12.78 5.22a.749.749 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.06 0L3.22 6.28a.749.749 0 1 1 1.06-1.06L8 8.939l3.72-3.719a.749.749 0 0 1 1.06 0Z"></path></svg>'
+              } else {
+                dom.innerHTML =
+                  '<svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style="display: inline-block; user-select: none; vertical-align: text-bottom; overflow: visible;"><path d="M6.22 3.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042L9.94 8 6.22 4.28a.75.75 0 0 1 0-1.06Z"></path></svg>'
+              }
+              return dom
+            }
+          }),
       drawSelection()
     ]
     if (
