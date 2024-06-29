c61a4b2d3e9b911257c9e56a1f1c2fcce7044a04
Meng Zhang <meng@tabbyml.com>
2024-06-30 07:48:19 +0800

    fix: when device is cpu - num_gpu_layers should default to 0

--- crates/tabby/src/main.rs
+++ crates/tabby/src/main.rs
@@ -129,7 +129,7 @@ fn to_local_config(model: &str, parallelism: u8, device: &Device) -> ModelConfig
             .flatten()
             .unwrap_or(9999)
     } else {
-        9999
+        0
     };
     ModelConfig::Local(LocalModelConfig {
         model_id: model.to_owned(),
