11524e2843f88e5bdaf9ba1f38d7092f8ea47691
Meng Zhang <meng@tabbyml.com>
2024-07-22 14:10:06 +0800

    refactor(webserver): move answer routes to webserver

--- crates/tabby-common/src/lib.rs
+++ crates/tabby-common/src/lib.rs
@@ -1,6 +1,7 @@
 //! Common tabby types and utilities.
 //! Defines common types and utilities used across multiple tabby subprojects, especially serialization and deserialization targets.
 pub mod api;
+pub mod axum;
 pub mod config;
 pub mod constants;
 pub mod index;
