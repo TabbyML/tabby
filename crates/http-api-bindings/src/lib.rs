21b4ded487b553feed7243add744a9b04c3a2d1a
Meng Zhang <meng@tabbyml.com>
2024-07-27 09:37:01 -0700

    fix: when connecting to localhost endpoint, do not use proxy settings (#2736)
    * fix: when connecting to localhost endpoint, do not use proxy settings
    
    * update
    
    * update

--- crates/http-api-bindings/src/lib.rs
+++ crates/http-api-bindings/src/lib.rs
@@ -5,3 +5,17 @@ mod embedding;
 pub use chat::create as create_chat;
 pub use completion::{build_completion_prompt, create};
 pub use embedding::create as create_embedding;
+
+fn create_reqwest_client(api_endpoint: &str) -> reqwest::Client {
+    let builder = reqwest::Client::builder();
+
+    let is_localhost = api_endpoint.starts_with("http://localhost")
+        || api_endpoint.starts_with("http://127.0.0.1");
+    let builder = if is_localhost {
+        builder.no_proxy()
+    } else {
+        builder
+    };
+
+    builder.build().unwrap()
+}
