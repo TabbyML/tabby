ec83dfd53e38dbb23f9cee68ece4d3ae5e4dc91f
Zhiming Ma <codes.icy@gmail.com>
2024-08-01 11:36:43 +0800

    feat(intellij): add chat window entrance in statusbar item menu.

--- clients/intellij/src/main/kotlin/com/tabbyml/intellijtabby/widgets/ChatToolWindowFactory.kt
+++ clients/intellij/src/main/kotlin/com/tabbyml/intellijtabby/widgets/ChatToolWindowFactory.kt
@@ -1,22 +1,18 @@
-package com.tabbyml.intellijtabby.chat
+package com.tabbyml.intellijtabby.widgets
 
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
+import com.tabbyml.intellijtabby.chat.ChatBrowser
 
-class TabbyToolWindowFactory : ToolWindowFactory, DumbAware {
+class ChatToolWindowFactory : ToolWindowFactory, DumbAware {
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-    val tabbyBrowser = TabbyBrowser(project)
-    val browserComponent = tabbyBrowser.getBrowserComponent()
+    val browserComponent = ChatBrowser(project).getBrowserComponent()
     val content = ContentFactory.getInstance().createContent(browserComponent, "", false)
     toolWindow.contentManager.addContent(content)
   }
 
   override fun shouldBeAvailable(project: Project) = true
-
-  init {
-    System.setProperty("ide.browser.jcef.contextMenu.devTools.enabled", "true")
-  }
 }
