d17b0b15f3aebaa4bd7b42080696f2f3a85fdb87
Wei Zhang <kweizh@gmail.com>
2024-08-09 02:17:15 +0800

    :sparkles: support reuse the same server if model id is equaled
    Signed-off-by: Wei Zhang <kweizh@gmail.com>

--- crates/tabby/src/services/model/mod.rs
+++ crates/tabby/src/services/model/mod.rs
@@ -6,37 +6,81 @@ use tabby_download::download_model;
 use tabby_inference::{ChatCompletionStream, CodeGeneration, CompletionStream, Embedding};
 use tracing::info;
 
-pub async fn load_chat_completion(chat: &ModelConfig) -> Arc<dyn ChatCompletionStream> {
-    match chat {
-        ModelConfig::Http(http) => http_api_bindings::create_chat(http).await,
-        ModelConfig::Local(llama) => llama_cpp_server::create_chat_completion(llama).await,
-    }
-}
-
 pub async fn load_embedding(config: &ModelConfig) -> Arc<dyn Embedding> {
     llama_cpp_server::create_embedding(config).await
 }
 
-pub async fn load_code_generation(model: &ModelConfig) -> (Arc<CodeGeneration>, PromptInfo) {
-    let (engine, prompt_info) = load_completion(model).await;
-    (Arc::new(CodeGeneration::new(engine)), prompt_info)
+pub async fn load_code_generation_and_chat(
+    completion_model: Option<ModelConfig>,
+    chat_model: Option<ModelConfig>,
+) -> (
+    Option<Arc<CodeGeneration>>,
+    Option<Arc<dyn ChatCompletionStream>>,
+    Option<PromptInfo>,
+) {
+    let (engine, chat, prompt_info) = load_completion_and_chat(completion_model, chat_model).await;
+    let code = if let Some(engine) = engine {
+        Some(Arc::new(CodeGeneration::new(engine)))
+    } else {
+        None
+    };
+    (code, chat, prompt_info)
 }
 
-async fn load_completion(model: &ModelConfig) -> (Arc<dyn CompletionStream>, PromptInfo) {
-    match model {
-        ModelConfig::Http(http) => {
-            let engine = http_api_bindings::create(http).await;
-            let (prompt_template, chat_template) = http_api_bindings::build_completion_prompt(http);
-            (
-                engine,
-                PromptInfo {
-                    prompt_template,
-                    chat_template,
-                },
-            )
+async fn load_completion_and_chat(
+    completion_model: Option<ModelConfig>,
+    chat_model: Option<ModelConfig>,
+) -> (
+    Option<Arc<dyn CompletionStream>>,
+    Option<Arc<dyn ChatCompletionStream>>,
+    Option<PromptInfo>,
+) {
+    if let (Some(completion), Some(chat)) = (&completion_model, &chat_model) {
+        match (completion, chat) {
+            (ModelConfig::Local(completion), ModelConfig::Local(chat)) => {
+                let (completion, chat, prompt) =
+                    llama_cpp_server::create_completion_and_chat(&completion, &chat).await;
+                return (Some(completion), Some(chat), Some(prompt));
+            }
+            _ => {}
         }
-        ModelConfig::Local(llama) => llama_cpp_server::create_completion(llama).await,
     }
+
+    let (completion, prompt) = if let Some(completion) = completion_model {
+        match completion {
+            ModelConfig::Http(http) => {
+                let engine = http_api_bindings::create(&http).await;
+                let (prompt_template, chat_template) =
+                    http_api_bindings::build_completion_prompt(&http);
+                (
+                    Some(engine),
+                    Some(PromptInfo {
+                        prompt_template,
+                        chat_template,
+                    }),
+                )
+            }
+            ModelConfig::Local(llama) => {
+                let (stream, prompt) = llama_cpp_server::create_completion(&llama).await;
+                (Some(stream), Some(prompt))
+            }
+        }
+    } else {
+        (None, None)
+    };
+
+    let chat = if let Some(chat) = chat_model {
+        match chat {
+            ModelConfig::Http(http) => Some(http_api_bindings::create_chat(&http).await),
+            ModelConfig::Local(llama) => {
+                Some(llama_cpp_server::create_chat_completion(&llama).await)
+            }
+        }
+    } else {
+        None
+    };
+
+    (completion, chat, prompt)
 }
 
 pub async fn download_model_if_needed(model: &str) {
