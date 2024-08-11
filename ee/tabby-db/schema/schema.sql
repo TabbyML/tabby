c0128703c062e5f1a367115702104e869a21a555
Meng Zhang <meng@tabbyml.com>
2024-08-10 17:48:40 -0700

    update

--- ee/tabby-db/schema/schema.sql
+++ ee/tabby-db/schema/schema.sql
@@ -163,15 +163,26 @@ CREATE TABLE web_crawler_urls(
   CONSTRAINT `unique_url` UNIQUE(url)
 );
 CREATE INDEX `idx_job_runs_command` ON job_runs(command);
-CREATE TABLE threads(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT);
+CREATE TABLE threads(
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  -- The user who created the thread
+  user_id INTEGER NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+  updated_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+  -- Array of relevant questions, in format of `String`
+  relevant_questions BLOB,
+  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
 CREATE TABLE thread_messages(
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   thread_id INTEGER NOT NULL,
   role TEXT NOT NULL,
   content TEXT NOT NULL,
-  -- Array of code attachments, in format of `MesssageAttachmentCode`
-  code_attachments JSONB,
-  -- Array of doc attachments, in format of `MesssageAttachmentDoc`
-  doc_attachments JSONB,
+  -- Array of code attachments, in format of `ThreadMessageAttachmentCode`
+  code_attachments BLOB,
+  -- Array of doc attachments, in format of `ThreadMessageAttachmentDoc`
+  doc_attachments BLOB,
+  created_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+  updated_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
   FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE
 );
