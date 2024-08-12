c2a4c382ad1a2a7a43beb9a81bc42b97e3a22b82
Meng Zhang <meng@tabbyml.com>
2024-08-12 11:53:32 -0700

    Update AgentConfig.ts

--- clients/tabby-agent/src/AgentConfig.ts
+++ clients/tabby-agent/src/AgentConfig.ts
@@ -194,6 +194,7 @@ You help fix spelling, improve grammar and reply the fixed text enclosed in <GEN
 You should not use other XML tags in response unless they are parts of the user document.
 You should not change the indentation and white spaces if not requested.
 The text content to be processed will be enclosed in <DOCUMENT></DOCUMENT> XML tags.
+
 <DOCUMENT>{{document}}</DOCUMENT>`,
         },
       },
