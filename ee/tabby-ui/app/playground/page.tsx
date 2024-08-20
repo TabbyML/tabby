b1ca5658cf040d34b756b4207f2f4aa05117dcff
liangfung <1098486429@qq.com>
2024-08-20 23:54:15 +0800

    update: remove playground

--- ee/tabby-ui/app/playground/page.tsx
+++ ee/tabby-ui/app/playground/page.tsx
@@ -1,11 +1,12 @@
 import { Metadata } from 'next'
 
-import Chats from './components/chats'
+// import Chats from './components/chats'
 
 export const metadata: Metadata = {
   title: 'Playground'
 }
 
 export default function IndexPage() {
-  return <Chats />
+  return null
+  // return <Chats />
 }
