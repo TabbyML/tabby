735080aad8b27791323eb01ad471d928506d41ff
Meng Zhang <meng@tabbyml.com>
2024-08-20 23:55:05 -0700

    ci: reuse llama.cpp distributions for windows cuda packaging (#2927)
    * ci: reuse llama.cpp distributions for windows cuda packaging
    
    * fix
    
    * fix
    
    * update
    
    * update
    
    * update
    
    * update

---
+++ ci/package-win.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Input variables
+LLAMA_CPP_VERSION=${LLAMA_CPP_VERSION:-b3571}
+LLAMA_CPP_PLATFORM=${LLAMA_CPP_PLATFORM:-cuda-cu11.7.1-x64}
+OUTPUT_NAME=${OUTPUT_NAME:-tabby_x86_64-windows-msvc-cuda117}
+
+NAME=llama-${LLAMA_CPP_VERSION}-bin-win-${LLAMA_CPP_PLATFORM}
+ZIP_FILE=${NAME}.zip
+
+curl https://github.com/ggerganov/llama.cpp/releases/download/${LLAMA_CPP_VERSION}/${ZIP_FILE} -L -o ${ZIP_FILE}
+unzip ${ZIP_FILE} -d ${OUTPUT_NAME}
+
+pushd ${OUTPUT_NAME}
+rm $(ls *.exe | grep -v "llama-server")
+cp ../tabby_x86_64-windows-msvc.exe tabby.exe
+popd
+
+zip -r ${OUTPUT_NAME}.zip ${OUTPUT_NAME}
+rm -rf ${OUTPUT_NAME}
+
+mkdir -p dist
+mv ${OUTPUT_NAME}.zip dist/
