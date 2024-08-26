a41a8c90df6c89154d21d50129d304173503b379
liangfung <1098486429@qq.com>
2024-08-27 00:24:23 +0800

    fix(ui): correct the navigation of Answer Engine

--- ee/tabby-ui/app/search/components/search.tsx
+++ ee/tabby-ui/app/search/components/search.tsx
@@ -293,7 +293,8 @@ export function Search() {
       pathname: `/search/${slugWithThreadId}`,
       searchParams: {
         del: ['q']
-      }
+      },
+      replace: true
     })
   }
 
@@ -1186,7 +1187,7 @@ function Header({ threadId }: { threadId?: string }) {
   }
 
   return (
-    <header className="flex h-16 items-center justify-between px-4">
+    <header className="flex h-16 items-center justify-between px-4 backdrop-blur-xl lg:px-10">
       <div className="flex items-center gap-x-6">
         <Button
           variant="ghost"
