97023deb2a6e64f9d3e2b8ad0541f191f199ebc4
Meng Zhang <meng@tabbyml.com>
2024-08-28 12:26:10 -0700

    update

--- ee/tabby-ui/app/(dashboard)/settings/(integrations)/providers/doc/new/page.tsx
+++ ee/tabby-ui/app/(dashboard)/settings/(integrations)/providers/doc/new/page.tsx
@@ -2,10 +2,6 @@ import { Metadata } from 'next'
 
 import CreateCustomDocument from '../components/create-custom-doc'
 
-export const metadata: Metadata = {
-  title: 'Create Custom Document'
-}
-
 export default function CreateCustomDocumentPage() {
   return <CreateCustomDocument />
 }
