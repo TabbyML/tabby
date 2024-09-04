0ab72527989995245565c78bdcbc4221b80bbf55
Meng Zhang <meng@tabbyml.com>
2024-09-03 23:00:07 -0700

    fix(ui): fix MARKDOWN_SOURCE_REGEX (#3064)

--- ee/tabby-ui/lib/constants/regex.ts
+++ ee/tabby-ui/lib/constants/regex.ts
@@ -1,4 +1,4 @@
-export const MARKDOWN_SOURCE_REGEX = /\[\[source:(.+)\]\]/g
+export const MARKDOWN_SOURCE_REGEX = /\[\[source:([^\]]+)\]\]/g
 
 export const MARKDOWN_CITATION_FUZZY_REGEX = /\[\[?citation:\s*\d+\]?\]/g
 export const MARKDOWN_CITATION_REGEX = /\[\[citation:(\d+)\]\]/g
