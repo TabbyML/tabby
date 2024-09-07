15f150e0486c4e63f1052c228f01aaa6d9bd0f8d
Meng Zhang <meng@tabbyml.com>
2024-09-06 17:31:59 -0700

    docs(website): update title composition

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -5,7 +5,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 export default {
-  title: 'Tabby - Opensource, self-hosted AI coding assitant',
+  title: 'Tabby',
   tagline: 'Opensource, self-hosted AI coding assistant',
   favicon: 'img/favicon.ico',
   trailingSlash: true,
