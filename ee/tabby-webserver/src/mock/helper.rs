050eab469ef043f7baddf8d822b5c9f9bf2a7a4d
Jackson Chen <541898146chen@gmail.com>
2024-09-20 01:14:37 -0500

    to: adding more test to answer.rs

---
+++ ee/tabby-webserver/src/mock/helper.rs
@@ -0,0 +1,74 @@
+pub mod helpers {
+    use std::path::PathBuf;
+    use tabby_schema::{context::ContextInfo, AsID};
+
+    use juniper::ID;
+    use tabby_common::{api::code::CodeSearchParams, config::AnswerConfig};
+    use tabby_schema::{
+        context::{ContextInfoHelper, ContextSourceValue},
+        repository::{Repository, RepositoryKind},
+        thread::CodeQueryInput,
+    };
+
+    const TEST_SOURCE_ID: &str = "source-1";
+    const TEST_GIT_URL: &str = "TabbyML/tabby";
+    const TEST_FILEPATH: &str = "test.rs";
+    const TEST_LANGUAGE: &str = "rust";
+    const TEST_CONTENT: &str = "fn main() {}";
+
+    pub fn make_answer_config() -> AnswerConfig {
+        AnswerConfig {
+            code_search_params: make_code_search_params(),
+        }
+    }
+
+    pub fn make_code_search_params() -> CodeSearchParams {
+        CodeSearchParams {
+            min_bm25_score: 0.5,
+            min_embedding_score: 0.7,
+            min_rrf_score: 0.3,
+            num_to_return: 5,
+            num_to_score: 10,
+        }
+    }
+    pub fn make_code_query_input() -> CodeQueryInput {
+        CodeQueryInput {
+            filepath: Some(TEST_FILEPATH.to_string()),
+            content: TEST_CONTENT.to_string(),
+            git_url: Some(TEST_GIT_URL.to_string()),
+            source_id: Some(TEST_SOURCE_ID.to_string()),
+            language: Some(TEST_LANGUAGE.to_string()),
+        }
+    }
+
+    pub fn make_context_info_helper() -> ContextInfoHelper {
+        ContextInfoHelper::new(&ContextInfo {
+            sources: vec![ContextSourceValue::Repository(Repository {
+                id: ID::from(TEST_SOURCE_ID.to_owned()),
+                source_id: TEST_SOURCE_ID.to_owned(),
+                name: "tabby".to_owned(),
+                kind: RepositoryKind::Github,
+                dir: PathBuf::from("tabby"),
+                git_url: TEST_GIT_URL.to_owned(),
+                refs: vec![],
+            })],
+        })
+    }
+
+    pub fn make_message(
+        id: i32,
+        content: &str,
+        role: tabby_schema::thread::Role,
+        attachment: Option<tabby_schema::thread::MessageAttachment>,
+    ) -> tabby_schema::thread::Message {
+        tabby_schema::thread::Message {
+            id: id.as_id(),
+            thread_id: ID::new("0"),
+            content: content.to_owned(),
+            role,
+            attachment: attachment.unwrap_or_default(),
+            created_at: chrono::Utc::now(),
+            updated_at: chrono::Utc::now(),
+        }
+    }
+}
