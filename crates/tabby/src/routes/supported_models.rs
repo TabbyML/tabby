6d2a84ff430f9de785806fc14047c7e27a434074
zzgu <guzhizhou28@gmail.com>
2024-09-23 10:38:07 -0700

    update model api

--- crates/tabby/src/routes/supported_models.rs
+++ crates/tabby/src/routes/supported_models.rs
@@ -1,7 +1,7 @@
 use axum::Json;
 use tabby_common::api::server_setting::ServerSetting;
 use tabby_common::config::Config;
-// use tabby_common::config::HttpModelConfig;
+use tabby_common::config::HttpModelConfig;
 
 #[utoipa::path(
     get,
@@ -15,9 +15,33 @@ use tabby_common::config::Config;
         ("token" = [])
     )
 )]
-pub async fn models() -> Json<ServerSetting> {
-    let config = ServerSetting {
-        disable_client_side_telemetry: false,
-    };
-    Json(config)
+pub async fn models() -> Json<Vec<String>> {
+    let models: tabby_common::config::ModelConfigGroup = Config::load().expect("Config file should be exist").model;
+    let mut http_model_configs: Vec<String> = Vec::new();
+
+    println!("{:?}", models.embedding);
+    if let Some(tabby_common::config::ModelConfig::Http(embedding_http_config)) = models.embedding {
+        println!("{:?}", embedding_http_config.supported_models);
+        // http_model_configs.push(embedding_http_config);
+    }
+
+    if let Some(tabby_common::config::ModelConfig::Http(completion_http_config)) = models.completion {
+        println!("{:?}", completion_http_config.supported_models);
+        // http_model_configs.extend(completion_http_config.supported_models);
+
+        if let Some(not_none_supported_models) = completion_http_config.supported_models {
+            http_model_configs.extend(not_none_supported_models);
+        }
+    }
+
+    if let Some(tabby_common::config::ModelConfig::Http(http_config)) = models.chat {
+        // http_model_configs.extend(http_config.supported_models);
+        println!("{:?}", http_config);
+
+        if let Some(not_none_supported_models) = http_config.supported_models {
+            http_model_configs.extend(not_none_supported_models);
+        }
+    }
+
+    Json(http_model_configs)
 }
