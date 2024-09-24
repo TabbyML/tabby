ac4eb1536646b3705a4ff685d2ce8683d94b846d
zzgu <guzhizhou28@gmail.com>
2024-09-24 10:54:34 -0700

    fix comments

--- crates/tabby/src/routes/supported_models.rs
+++ crates/tabby/src/routes/supported_models.rs
@@ -1,5 +1,13 @@
 use axum::Json;
 use tabby_common::config::Config;
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+#[derive(Serialize, Deserialize, ToSchema, Clone, Debug)]
+pub struct MyStruct {
+    completion: Vec<String>,
+    chat: Vec<String>,
+}
 
 #[utoipa::path(
     get,
@@ -13,28 +21,23 @@ use tabby_common::config::Config;
         ("token" = [])
     )
 )]
-pub async fn models() -> Json<Vec<String>> {
+pub async fn models() -> Json<MyStruct> {
     let models: tabby_common::config::ModelConfigGroup = Config::load().expect("Config file should be exist").model;
-    let mut http_model_configs: Vec<String> = Vec::new();
+    let mut http_model_configs: MyStruct = MyStruct {
+        completion: Vec::new(),
+        chat: Vec::new(),
+    };
 
-    match models.embedding {
-        tabby_common::config::ModelConfig::Http(http_config) => {
-            http_model_configs.extend(http_config.supported_models.unwrap());
-        }
-        tabby_common::config::ModelConfig::Local(_) => {
-            return Json(http_model_configs)
-        }
-    }
 
     if let Some(tabby_common::config::ModelConfig::Http(completion_http_config)) = models.completion {
         if let Some(not_none_supported_models) = completion_http_config.supported_models {
-            http_model_configs.extend(not_none_supported_models);
+            http_model_configs.completion.extend(not_none_supported_models);
         }
     }
 
     if let Some(tabby_common::config::ModelConfig::Http(http_config)) = models.chat {
         if let Some(not_none_supported_models) = http_config.supported_models {
-            http_model_configs.extend(not_none_supported_models);
+            http_model_configs.chat.extend(not_none_supported_models);
         }
     }
 
