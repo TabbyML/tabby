b03705b28585c7687d566a5768ec001299f5463d
zzgu <guzhizhou28@gmail.com>
2024-09-26 16:39:00 -0700

    draft: to be fix shared reference

--- crates/tabby/src/routes/models.rs
+++ crates/tabby/src/routes/models.rs
@@ -1,13 +1,38 @@
 use std::sync::Arc;
 
 use axum::{extract::State, Json};
-use serde::{Deserialize, Serialize};
+use serde::{de::value, Deserialize, Serialize};
 use utoipa::ToSchema;
 
 #[derive(Serialize, Deserialize, ToSchema, Clone, Debug)]
 pub struct ModelInfo {
     completion: Option<Vec<String>>,
     chat: Option<Vec<String>>,
+}
+
+impl From<tabby_common::config::Config> for ModelInfo {
+    fn from(value: tabby_common::config::Config) -> Self {
+        let models = value.model;
+        let mut http_model_configs: ModelInfo = ModelInfo {
+            completion: None,
+            chat: None,
+        };
+    
+        if let Some(tabby_common::config::ModelConfig::Http(completion_http_config)) = models.completion
+        {
+            if let Some(models) = completion_http_config.supported_models {
+                http_model_configs.completion = Some(models.clone());
+            }
+        }
+    
+        if let Some(tabby_common::config::ModelConfig::Http(chat_http_config)) = models.chat {
+            if let Some(models) = chat_http_config.supported_models {
+                http_model_configs.chat = Some(models.clone());
+            }
+        }
+
+        return http_model_configs;
+    }
 }
 
 #[utoipa::path(
@@ -22,25 +47,6 @@ pub struct ModelInfo {
         ("token" = [])
     )
 )]
-pub async fn models(State(state): State<Arc<tabby_common::config::Config>>) -> Json<ModelInfo> {
-    let models = state.as_ref().clone().model;
-    let mut http_model_configs: ModelInfo = ModelInfo {
-        completion: None,
-        chat: None,
-    };
-
-    if let Some(tabby_common::config::ModelConfig::Http(completion_http_config)) = models.completion
-    {
-        if let Some(models) = completion_http_config.supported_models {
-            http_model_configs.completion = Some(models.clone());
-        }
-    }
-
-    if let Some(tabby_common::config::ModelConfig::Http(chat_http_config)) = models.chat {
-        if let Some(models) = chat_http_config.supported_models {
-            http_model_configs.chat = Some(models.clone());
-        }
-    }
-
-    Json(http_model_configs)
+pub async fn models(State(state): State<Arc<ModelInfo>>) -> Json<ModelInfo> {
+    Json(state.as_ref().clone())
 }
