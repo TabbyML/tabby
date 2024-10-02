24a329c332bc3a3a8b4562e6a7a4936a70759c11
Zhiming Ma <codes.icy@gmail.com>
2024-10-02 12:38:44 +0800

    chore(intellij): bump intellij plugin version to 1.8.1.

--- clients/intellij/build.gradle.kts
+++ clients/intellij/build.gradle.kts
@@ -39,7 +39,7 @@ tasks {
 
   intellijPlatform {
     pluginConfiguration {
-      version.set("1.8.0")
+      version.set("1.8.1")
       changeNotes.set(provider {
         changelog.renderItem(
           changelog.getLatest(),
