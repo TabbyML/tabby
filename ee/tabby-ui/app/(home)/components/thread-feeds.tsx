f8147ed6b676b9729d0f9da97608089c012caf40
aliang <1098486429@qq.com>
2024-10-16 19:43:33 +0800

    Update ee/tabby-ui/app/(home)/components/thread-feeds.tsx
    Co-authored-by: Meng Zhang <meng@tabbyml.com>

--- ee/tabby-ui/app/(home)/components/thread-feeds.tsx
+++ ee/tabby-ui/app/(home)/components/thread-feeds.tsx
@@ -136,7 +136,7 @@ export function ThreadFeeds({
     >
       <div className="w-full">
         <AnimationWrapper delay={0.3} style={{ width: '100%' }}>
-          <div className="mb-2.5 w-full text-lg font-semibold">Threads</div>
+          <div className="mb-2.5 w-full text-lg font-semibold">Recent Activities</div>
           <Separator className="mb-4 w-full" />
           <motion.div
             initial="initial"
