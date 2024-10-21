779f78582781ef4594e15a2ec54a78447d06bd6b
Wei Zhang <kweizh@gmail.com>
2024-10-21 13:34:03 +0800

    feat(download): allow fetching model files with multiple partitions (#3258)
    * finish main logic
    
    * add ut
    
    * [autofix.ci] apply automated fixes
    
    * feat: use indexed model name
    
    * chore: apply review from meng
    
    * chore: revert unnecessary downloader change
    
    * chore: fix ut
    
    * chore: donwload one file each time
    
    * [autofix.ci] apply automated fixes
    
    * chore: fix ut
    
    * chore: fix review from meng
    
    * chore: fix ci
    
    * chore: revert multibar
    
    * [autofix.ci] apply automated fixes
    
    * [autofix.ci] apply automated fixes (attempt 2/3)
    
    * chore: filter download address tests should be serial
    
    * chore: use the workspace dep for serial_test
    
    ---------
    
    Co-authored-by: leili <lilei@deeproute.ai>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- crates/tabby-download/src/lib.rs
+++ crates/tabby-download/src/lib.rs
@@ -1,24 +1,71 @@
 //! Responsible for downloading ML models for use with tabby.
-use std::{
-    fs::{self},
-    path::Path,
-};
+use std::fs;
 
 use aim_downloader::{bar::WrappedBar, error::DownloadError, hash::HashChecker, https};
-use anyhow::{anyhow, bail, Result};
-use tabby_common::registry::{parse_model_id, ModelRegistry};
+use anyhow::{bail, Result};
+use tabby_common::registry::{parse_model_id, ModelInfo, ModelRegistry};
 use tokio_retry::{
     strategy::{jitter, ExponentialBackoff},
     Retry,
 };
 use tracing::{info, warn};
 
-fn select_by_download_host(url: &String) -> bool {
-    if let Ok(host) = std::env::var("TABBY_DOWNLOAD_HOST") {
-        url.contains(&host)
-    } else {
-        true
-    }
+pub fn get_download_host() -> String {
+    std::env::var("TABBY_DOWNLOAD_HOST").unwrap_or_else(|_| "huggingface.co".to_string())
+}
+
+pub fn get_huggingface_mirror_host() -> Option<String> {
+    std::env::var("TABBY_HUGGINGFACE_HOST_OVERRIDE").ok()
+}
+
+pub fn filter_download_address(model_info: &ModelInfo) -> Vec<(String, String)> {
+    let download_host = get_download_host();
+    if let Some(urls) = &model_info.urls {
+        if !urls.is_empty() {
+            let url = model_info
+                .urls
+                .iter()
+                .flatten()
+                .find(|f| f.contains(&download_host));
+            if let Some(url) = url {
+                if let Some(mirror_host) = get_huggingface_mirror_host() {
+                    return vec![(
+                        url.replace("huggingface.co", &mirror_host),
+                        model_info.sha256.clone().unwrap_or_default(),
+                    )];
+                }
+                return vec![(
+                    url.to_owned(),
+                    model_info.sha256.clone().unwrap_or_default(),
+                )];
+            }
+        }
+    };
+
+    model_info
+        .partition_urls
+        .iter()
+        .flatten()
+        .map(|x| -> (String, String) {
+            let url = x.urls.iter().find(|f| f.contains(&download_host));
+            if let Some(url) = url {
+                if let Some(mirror_host) = get_huggingface_mirror_host() {
+                    return (
+                        url.replace("huggingface.co", &mirror_host),
+                        x.sha256.clone(),
+                    );
+                }
+                return (url.to_owned(), x.sha256.clone());
+            }
+            panic!("No download URLs available for <{}>", model_info.name);
+        })
+        .collect()
+}
+
+macro_rules! partitioned_file_name {
+    ($index:expr, $total:expr) => {
+        format!("model-{:05}-of-{:05}.gguf", $index + 1, $total)
+    };
 }
 
 async fn download_model_impl(
@@ -27,63 +74,79 @@ async fn download_model_impl(
     prefer_local_file: bool,
 ) -> Result<()> {
     let model_info = registry.get_model_info(name);
-    registry.save_model_info(name);
+    registry.migrate_legacy_model_path(name)?;
 
-    registry.migrate_model_path(name)?;
-    let model_path = registry.get_model_path(name);
-    if model_path.exists() {
-        if !prefer_local_file {
-            info!("Checking model integrity..");
-            if HashChecker::check(&model_path.display().to_string(), &model_info.sha256).is_ok() {
-                return Ok(());
-            }
-            warn!(
-                "Checksum doesn't match for <{}/{}>, re-downloading...",
-                registry.name, name
-            );
-            fs::remove_file(&model_path)?;
-        } else {
-            return Ok(());
-        }
+    let urls = filter_download_address(model_info);
+    if urls.is_empty() {
+        bail!(
+            "No download URLs available for <{}/{}>",
+            registry.name,
+            model_info.name
+        );
     }
 
-    let Some(model_url) = model_info
-        .urls
-        .iter()
-        .flatten()
-        .find(|x| select_by_download_host(x))
-    else {
-        return Err(anyhow!("No valid url for model <{}>", model_info.name));
-    };
+    if !prefer_local_file {
+        info!("Checking model integrity..");
 
-    // Replace the huggingface.co domain with the mirror host if it is set.
-    let model_url = if let Ok(host) = std::env::var("TABBY_HUGGINGFACE_HOST_OVERRIDE") {
-        model_url.replace("huggingface.co", &host)
-    } else {
-        model_url.to_owned()
-    };
+        let mut sha256_matched = true;
+        for (index, url) in urls.iter().enumerate() {
+            if HashChecker::check(
+                partitioned_file_name!(index + 1, urls.len()).as_str(),
+                &url.1,
+            )
+            .is_err()
+            {
+                sha256_matched = false;
+                break;
+            }
+        }
 
-    let strategy = ExponentialBackoff::from_millis(100).map(jitter).take(2);
-    let download_job = Retry::spawn(strategy, || {
-        download_file(&model_url, model_path.as_path(), &model_info.sha256)
-    });
-    download_job.await?;
+        if sha256_matched {
+            return Ok(());
+        }
+
+        warn!(
+            "Checksum doesn't match for <{}/{}>, re-downloading...",
+            registry.name, name
+        );
+
+        fs::remove_dir_all(registry.get_model_dir(name))?;
+    }
+
+    // prepare for download
+    let dir = registry.get_model_store_dir(name);
+    fs::create_dir_all(dir)?;
+    registry.save_model_info(name);
+
+    for (index, url) in urls.iter().enumerate() {
+        let dir = registry
+            .get_model_store_dir(name)
+            .to_string_lossy()
+            .into_owned();
+        let filename: String = partitioned_file_name!(index + 1, urls.len());
+        let strategy = ExponentialBackoff::from_millis(100).map(jitter).take(2);
+
+        Retry::spawn(strategy, move || {
+            let dir = dir.clone();
+            let filename = filename.clone();
+
+            download_file(&url.0, dir, filename, &url.1)
+        })
+        .await?;
+    }
+
     Ok(())
 }
 
-async fn download_file(url: &str, path: &Path, expected_sha256: &str) -> Result<()> {
-    let dir = path
-        .parent()
-        .ok_or_else(|| anyhow!("Must not be in root directory"))?;
-    fs::create_dir_all(dir)?;
-
-    let filename = path
-        .to_str()
-        .ok_or_else(|| anyhow!("Could not convert filename to UTF-8"))?;
-    let intermediate_filename = filename.to_owned() + ".tmp";
-
+async fn download_file(
+    url: &str,
+    dir: String,
+    filename: String,
+    expected_sha256: &str,
+) -> Result<()> {
+    let fullpath = format! {"{}{}{}", dir, std::path::MAIN_SEPARATOR, filename};
+    let intermediate_filename = fullpath.clone() + ".tmp";
     let mut bar = WrappedBar::new(0, url, false);
-
     if let Err(e) =
         https::HTTPSHandler::get(url, &intermediate_filename, &mut bar, expected_sha256).await
     {
@@ -97,7 +160,7 @@ async fn download_file(url: &str, path: &Path, expected_sha256: &str) -> Result<
         }
     }
 
-    fs::rename(intermediate_filename, filename)?;
+    fs::rename(intermediate_filename, fullpath)?;
     Ok(())
 }
 
@@ -110,4 +173,210 @@ pub async fn download_model(model_id: &str, prefer_local_file: bool) {
     download_model_impl(&registry, name, prefer_local_file)
         .await
         .unwrap_or_else(handler)
+}
+
+#[cfg(test)]
+mod tests {
+    // filter_download_address tests should be serial because they rely on environment variables
+    use serial_test::serial;
+    use tabby_common::registry::{ModelInfo, PartitionModelUrl};
+
+    #[test]
+    #[serial(filter_download_address)]
+    fn test_filter_download_address() {
+        // multiple urls
+        let model_info = ModelInfo {
+            name: "test".to_string(),
+            urls: Some(vec![
+                "https://huggingface.co/test".to_string(),
+                "https://huggingface.co/test2".to_string(),
+                "https://modelscope.co/test2".to_string(),
+            ]),
+            sha256: Some("test_sha256".to_string()),
+            prompt_template: None,
+            chat_template: None,
+            partition_urls: None,
+        };
+        let urls = super::filter_download_address(&model_info);
+        assert_eq!(urls.len(), 1);
+        assert_eq!(urls[0].0, "https://huggingface.co/test");
+
+        // single url
+        let model_info = ModelInfo {
+            name: "test".to_string(),
+            urls: Some(vec![
+                "https://huggingface.co/test".to_string(),
+                "https://modelscope.co/test2".to_string(),
+            ]),
+            sha256: Some("test_sha256".to_string()),
+            prompt_template: None,
+            chat_template: None,
+            partition_urls: None,
+        };
+        let urls = super::filter_download_address(&model_info);
+        assert_eq!(urls.len(), 1);
+        assert_eq!(urls[0].0, "https://huggingface.co/test");
+    }
+
+    #[test]
+    #[serial(filter_download_address)]
+    fn test_filter_download_address_multiple_partitions() {
+        let model_info = ModelInfo {
+            name: "test".to_string(),
+            urls: None,
+            sha256: Some("test_sha256".to_string()),
+            prompt_template: None,
+            chat_template: None,
+            partition_urls: Some(vec![
+                PartitionModelUrl {
+                    urls: vec![
+                        "https://huggingface.co/part1".to_string(),
+                        "https://modelscope.co/part1".to_string(),
+                    ],
+                    sha256: "test_sha256_1".to_string(),
+                },
+                PartitionModelUrl {
+                    urls: vec![
+                        "https://huggingface.co/part2".to_string(),
+                        "https://modelscope.co/part2".to_string(),
+                    ],
+                    sha256: "test_sha256_2".to_string(),
+                },
+            ]),
+        };
+        let urls = super::filter_download_address(&model_info);
+        assert_eq!(urls.len(), 2);
+        assert_eq!(urls[0].0, "https://huggingface.co/part1");
+        assert_eq!(urls[0].1, "test_sha256_1");
+        assert_eq!(urls[1].0, "https://huggingface.co/part2");
+        assert_eq!(urls[1].1, "test_sha256_2");
+    }
+
+    #[test]
+    #[serial(filter_download_address)]
+    fn test_filter_download_address_single_partition() {
+        let model_info = ModelInfo {
+            name: "test".to_string(),
+            urls: None,
+            sha256: Some("test_sha256".to_string()),
+            prompt_template: None,
+            chat_template: None,
+            partition_urls: Some(vec![PartitionModelUrl {
+                urls: vec!["https://huggingface.co/part1".to_string()],
+                sha256: "test_sha256_1".to_string(),
+            }]),
+        };
+        let urls = super::filter_download_address(&model_info);
+        assert_eq!(urls.len(), 1);
+        assert_eq!(urls[0].0, "https://huggingface.co/part1");
+        assert_eq!(urls[0].1, "test_sha256_1");
+    }
+
+    #[test]
+    #[serial(filter_download_address)]
+    fn test_filter_download_address_prefer_urls() {
+        let model_info = ModelInfo {
+            name: "test".to_string(),
+            urls: Some(vec!["https://huggingface.co/test".to_string()]),
+            sha256: Some("test_sha256".to_string()),
+            prompt_template: None,
+            chat_template: None,
+            partition_urls: Some(vec![PartitionModelUrl {
+                urls: vec!["https://modelscope.co/test".to_string()],
+                sha256: "test_sha256".to_string(),
+            }]),
+        };
+        let urls = super::filter_download_address(&model_info);
+        assert_eq!(urls.len(), 1);
+        assert_eq!(urls[0].0, "https://huggingface.co/test");
+        assert_eq!(urls[0].1, "test_sha256");
+    }
+
+    #[test]
+    #[serial(filter_download_address)]
+    fn test_filter_download_address_huggingface_override_urls() {
+        std::env::set_var("TABBY_HUGGINGFACE_HOST_OVERRIDE", "modelscope.co");
+        let model_info = ModelInfo {
+            name: "test".to_string(),
+            urls: Some(vec!["https://huggingface.co/test".to_string()]),
+            sha256: Some("test_sha256".to_string()),
+            prompt_template: None,
+            chat_template: None,
+            partition_urls: None,
+        };
+        let urls = super::filter_download_address(&model_info);
+        assert_eq!(urls.len(), 1);
+        assert_eq!(urls[0].0, "https://modelscope.co/test");
+        assert_eq!(urls[0].1, "test_sha256");
+        // must reset the env, or it will affect other tests
+        std::env::remove_var("TABBY_HUGGINGFACE_HOST_OVERRIDE");
+    }
+
+    #[test]
+    #[serial(filter_download_address)]
+    fn test_filter_download_address_huggingface_override_partitioned() {
+        std::env::set_var("TABBY_HUGGINGFACE_HOST_OVERRIDE", "modelscope.co");
+        let model_info = ModelInfo {
+            name: "test".to_string(),
+            urls: None,
+            sha256: Some("test_sha256".to_string()),
+            prompt_template: None,
+            chat_template: None,
+            partition_urls: Some(vec![
+                PartitionModelUrl {
+                    urls: vec!["https://huggingface.co/part1".to_string()],
+                    sha256: "test_sha256_1".to_string(),
+                },
+                PartitionModelUrl {
+                    urls: vec!["https://huggingface.co/part2".to_string()],
+                    sha256: "test_sha256_2".to_string(),
+                },
+            ]),
+        };
+        let urls = super::filter_download_address(&model_info);
+        assert_eq!(urls.len(), 2);
+        assert_eq!(urls[0].0, "https://modelscope.co/part1");
+        assert_eq!(urls[0].1, "test_sha256_1");
+        assert_eq!(urls[1].0, "https://modelscope.co/part2");
+        assert_eq!(urls[1].1, "test_sha256_2");
+        // must reset the env, or it will affect other tests
+        std::env::remove_var("TABBY_HUGGINGFACE_HOST_OVERRIDE");
+    }
+
+    #[test]
+    #[serial(filter_download_address)]
+    fn test_filter_download_address_download_host() {
+        std::env::set_var("TABBY_DOWNLOAD_HOST", "modelscope.co");
+        let model_info = ModelInfo {
+            name: "test".to_string(),
+            urls: None,
+            sha256: Some("test_sha256".to_string()),
+            prompt_template: None,
+            chat_template: None,
+            partition_urls: Some(vec![
+                PartitionModelUrl {
+                    urls: vec![
+                        "https://huggingface.co/part1".to_string(),
+                        "https://modelscope.co/part1".to_string(),
+                    ],
+                    sha256: "test_sha256_1".to_string(),
+                },
+                PartitionModelUrl {
+                    urls: vec![
+                        "https://huggingface.co/part2".to_string(),
+                        "https://modelscope.co/part2".to_string(),
+                    ],
+                    sha256: "test_sha256_2".to_string(),
+                },
+            ]),
+        };
+        let urls = super::filter_download_address(&model_info);
+        assert_eq!(urls.len(), 2);
+        assert_eq!(urls[0].0, "https://modelscope.co/part1");
+        assert_eq!(urls[0].1, "test_sha256_1");
+        assert_eq!(urls[1].0, "https://modelscope.co/part2");
+        assert_eq!(urls[1].1, "test_sha256_2");
+        // must reset the env, or it will affect other tests
+        std::env::remove_var("TABBY_DOWNLOAD_HOST");
+    }
 }
