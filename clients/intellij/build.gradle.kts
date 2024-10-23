a2640267e80019ade453ce8123dabb6f12f88163
Zhiming Ma <codes.icy@gmail.com>
2024-10-23 12:41:59 +0800

    chore(intellij): bump intellij plugin version to 1.9.0-dev.

--- clients/intellij/build.gradle.kts
+++ clients/intellij/build.gradle.kts
@@ -39,7 +39,7 @@ tasks {
 
   intellijPlatform {
     pluginConfiguration {
-      version.set("1.8.0-dev")
+      version.set("1.9.0-dev")
       changeNotes.set(provider {
         changelog.renderItem(
           changelog.getLatest(),
