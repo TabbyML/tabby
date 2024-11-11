5df031a62b745aca65dd872c544b8af4f1ae0078
Zhiming Ma <codes.icy@gmail.com>
2024-11-12 01:44:12 +0800

    fix(eclipse): use lineSpacing insteadof lineVerticalIndent for ghost text renderer.

--- clients/eclipse/plugin/src/com/tabbyml/tabby4eclipse/inlineCompletion/renderer/InlineCompletionItemRenderer.java
+++ clients/eclipse/plugin/src/com/tabbyml/tabby4eclipse/inlineCompletion/renderer/InlineCompletionItemRenderer.java
@@ -9,7 +9,23 @@ public class InlineCompletionItemRenderer {
 		private static final IInlineCompletionItemRenderer INSTANCE = createInstance();
 	}
 
+	private static String TABBY4ECLIPSE_EXPERIMENTAL_RENDERER_TEXTPAINTER = System
+			.getenv("TABBY4ECLIPSE_EXPERIMENTAL_RENDERER_TEXTPAINTER");
+	private static boolean EXPERIMENTAL_RENDERER_TEXTPAINTER = TABBY4ECLIPSE_EXPERIMENTAL_RENDERER_TEXTPAINTER != null
+			&& !TABBY4ECLIPSE_EXPERIMENTAL_RENDERER_TEXTPAINTER.isEmpty();
+
+	private static String TABBY4ECLIPSE_EXPERIMENTAL_RENDERER_TEXTPAINTER2 = System
+			.getenv("TABBY4ECLIPSE_EXPERIMENTAL_RENDERER_TEXTPAINTER2");
+	private static boolean EXPERIMENTAL_RENDERER_TEXTPAINTER2 = TABBY4ECLIPSE_EXPERIMENTAL_RENDERER_TEXTPAINTER2 != null
+			&& !TABBY4ECLIPSE_EXPERIMENTAL_RENDERER_TEXTPAINTER2.isEmpty();
+
 	public static IInlineCompletionItemRenderer createInstance() {
-		return new InlineCompletionItemTextPainter();
+		if (EXPERIMENTAL_RENDERER_TEXTPAINTER) {
+			return new InlineCompletionItemTextPainter();
+		}
+		if (EXPERIMENTAL_RENDERER_TEXTPAINTER2) {
+			return new InlineCompletionItemTextPainter2();
+		}
+		return new InlineCompletionItemTextPainter2();
 	}
 }
