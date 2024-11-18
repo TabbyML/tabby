d260d12579d778faf60d362207b813c22f915f8d
aliang <jueliang@tabbyml.com>
2024-11-18 07:35:57 +0700

    feat(ui): add pagination to recent activities (#3427)
    * feat(ui): add pagination to recent activities
    
    * update: animation
    
    * [autofix.ci] apply automated fixes
    
    * update
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- ee/tabby-ui/components/textarea-search.tsx
+++ ee/tabby-ui/components/textarea-search.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useMemo, useRef, useState } from 'react'
 import { Editor } from '@tiptap/react'
 import { Maybe } from 'graphql/jsutils/Maybe'
 
@@ -74,15 +74,9 @@ export default function TextAreaSearch({
   onValueChange?: (value: string | undefined) => void
   models: Maybe<Array<string>> | undefined
 }) {
-  const [isShow, setIsShow] = useState(false)
   const [isFocus, setIsFocus] = useState(false)
   const [value, setValue] = useState('')
   const editorRef = useRef<PromptEditorRef>(null)
-
-  useEffect(() => {
-    // Ensure the textarea height remains consistent during rendering
-    setIsShow(true)
-  }, [])
 
   const focusTextarea = () => {
     editorRef.current?.editor?.commands.focus()
@@ -171,7 +165,7 @@ export default function TextAreaSearch({
           'min-h-[2.5rem]': isFollowup
         })}
       >
-        <div className="mr-1 flex-1">
+        <div className="mr-1 flex-1 overflow-x-hidden">
           <PromptEditor
             editable
             contextInfo={contextInfo}
@@ -187,8 +181,6 @@ export default function TextAreaSearch({
             className={cn(
               'text-area-autosize resize-none rounded-lg !border-none bg-transparent !shadow-none !outline-none !ring-0 !ring-offset-0',
               {
-                '!h-[48px]': !isShow && !isFollowup,
-                '!h-[24px]': !isShow && isFollowup,
                 'py-3': !showBetaBadge,
                 'py-4': showBetaBadge
               }
@@ -247,7 +239,7 @@ export default function TextAreaSearch({
             delay={0}
             fallback={
               <div className="flex h-8 w-[40%] items-center">
-                <Skeleton className="h-5 w-full" />
+                <Skeleton className="h-4 w-full" />
               </div>
             }
           >
--- ee/tabby-ui/lib/stores/scroll-store.ts
+++ ee/tabby-ui/lib/stores/scroll-store.ts
@@ -1,62 +1,23 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
 
-const excludeFromState = ['_hasHydrated', 'setHasHydrated', 'activeChatId']
-
-export interface ChatState {
+export interface ScrollState {
   homePage: number | undefined
-  _hasHydrated: boolean
-  setHasHydrated: (state: boolean) => void
 }
 
-const initialState: Omit<ChatState, 'setHasHydrated' | 'deleteChat'> = {
-  _hasHydrated: false,
+const initialState: ScrollState = {
   homePage: undefined
 }
 
-export const useScrollStore = create<ChatState>()(
-  persist(
-    set => {
-      return {
-        ...initialState,
-        setHasHydrated: (state: boolean) => {
-          set({
-            _hasHydrated: state
-          })
-        }
-      }
-    },
-    {
-      name: 'tabby-scroll-storage',
-      partialize: state =>
-        Object.fromEntries(
-          Object.entries(state).filter(
-            ([key]) => !excludeFromState.includes(key)
-          )
-        ),
-      onRehydrateStorage() {
-        return state => {
-          if (state) {
-            state.setHasHydrated(true)
-          }
-        }
-      }
-    }
-  )
-)
+export const useScrollStore = create<ScrollState>()(() => {
+  return { ...initialState }
+})
 
 const set = useScrollStore.setState
 
 export const setHomeScrollPosition = (scrollTop: number) => {
-  return set(state => ({
-    ...state,
-    homePage: scrollTop
-  }))
+  return set(() => ({ homePage: scrollTop }))
 }
 
 export const clearHomeScrollPosition = () => {
-  return set(state => ({
-    ...state,
-    homePage: undefined
-  }))
+  return set(() => ({ homePage: undefined }))
 }
