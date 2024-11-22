83ab30c5053038bba39fdce04c5d2be871bc17db
Wei Zhang <kweizh@tabbyml.com>
2024-11-22 20:17:02 +0800

    chore: use HttpClient trait for rate limit and client

--- crates/http-api-bindings/src/embedding/llama.rs
+++ crates/http-api-bindings/src/embedding/llama.rs
@@ -2,28 +2,37 @@ use std::time;
 
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::sync::Arc;
 use tabby_inference::Embedding;
 
-use crate::RateLimitedClient;
+use crate::{create_reqwest_client, HttpClient, RateLimitedClient};
 
 pub struct LlamaCppEngine {
-    client: RateLimitedClient,
+    client: Arc<dyn HttpClient>,
     api_endpoint: String,
 }
 
 impl LlamaCppEngine {
     pub fn create(api_endpoint: &str, api_key: Option<String>, num_request: u64, per: u64) -> Self {
-        let client = RateLimitedClient::new(
-            api_endpoint,
-            api_key,
-            num_request,
-            time::Duration::from_secs(per),
-        )
-        .unwrap();
-
-        Self {
-            client,
-            api_endpoint: format!("{}/embedding", api_endpoint),
+        if num_request > 0 {
+            let client = RateLimitedClient::new(
+                api_endpoint,
+                api_key,
+                num_request,
+                time::Duration::from_secs(per),
+            )
+            .unwrap();
+            Self {
+                client: Arc::new(client),
+                api_endpoint: format!("{}/embedding", api_endpoint),
+            }
+        } else {
+            let client = create_reqwest_client(api_endpoint);
+            Self {
+                client: Arc::new(client),
+                api_endpoint: format!("{}/embedding", api_endpoint),
+            }
         }
     }
 }
@@ -45,7 +54,7 @@ impl Embedding for LlamaCppEngine {
             content: prompt.to_owned(),
         };
 
-        let response = self.client.post(&self.api_endpoint, request).await?;
+        let response = self.client.post(&self.api_endpoint, json!(request)).await?;
         if response.status().is_server_error() {
             let error = response.text().await?;
             return Err(anyhow::anyhow!(
--- crates/http-api-bindings/src/lib.rs
+++ crates/http-api-bindings/src/lib.rs
@@ -2,15 +2,19 @@ mod chat;
 mod completion;
 mod embedding;
 
-use std::{env, sync::Arc, time::Duration};
-
 pub use chat::create as create_chat;
 pub use completion::{build_completion_prompt, create};
 pub use embedding::create as create_embedding;
 use reqwest::{Response, Url};
-use serde::Serialize;
+use serde_json::Value;
+use std::{env, sync::Arc, time::Duration};
 use tokio::sync::Mutex;
 use tower::{limit::rate::RateLimit, Service, ServiceBuilder, ServiceExt};
+
+#[async_trait::async_trait]
+pub trait HttpClient: Send + Sync {
+    async fn post(&self, url: &str, body: Value) -> anyhow::Result<Response>;
+}
 
 fn create_reqwest_client(api_endpoint: &str) -> reqwest::Client {
     let builder = reqwest::Client::builder();
@@ -31,8 +35,6 @@ struct RateLimitedClient {
     rate_limit: Arc<Mutex<RateLimit<reqwest::Client>>>,
 
     api_key: Option<String>,
-
-    rate_limit_enabled: bool,
 }
 
 impl RateLimitedClient {
@@ -42,11 +44,9 @@ impl RateLimitedClient {
         num_request: u64,
         per: Duration,
     ) -> anyhow::Result<Self> {
-        if (num_request == 0) != (per.as_secs() == 0) {
-            anyhow::bail!("Both num_request and per must be zero or both must be non-zero");
+        if num_request == 0 || per.as_secs() == 0 {
+            anyhow::bail!("Both num_request and per must be non-zero");
         }
-
-        let rate_limit_enabled = num_request > 0;
 
         let builder = reqwest::Client::builder();
         let is_localhost = api_endpoint.starts_with("http://localhost")
@@ -68,11 +68,13 @@ impl RateLimitedClient {
                     .service(client),
             )),
             api_key,
-            rate_limit_enabled,
         })
     }
+}
 
-    async fn post<T: Serialize>(&self, url: &str, body: T) -> anyhow::Result<Response> {
+#[async_trait::async_trait]
+impl HttpClient for RateLimitedClient {
+    async fn post(&self, url: &str, body: Value) -> anyhow::Result<Response> {
         let url = Url::parse(url)?;
         let mut builder = self.client.post(url.clone()).json(&body);
         if let Some(api_key) = &self.api_key {
@@ -80,17 +82,23 @@ impl RateLimitedClient {
         }
         let request = builder.build()?;
 
-        if self.rate_limit_enabled {
-            let future = self.rate_limit.lock().await.ready().await?.call(request);
-            future
-                .await
-                .and_then(|response| response.error_for_status())
-                .map_err(|err| anyhow::anyhow!("Error from server: {}", err))
-        } else {
-            let response = self.client.execute(request).await?;
-            response
-                .error_for_status()
-                .map_err(|err| anyhow::anyhow!("Error from server: {}", err))
-        }
+        let future = self.rate_limit.lock().await.ready().await?.call(request);
+        let response = future.await?;
+
+        response
+            .error_for_status()
+            .map_err(|err| anyhow::anyhow!("Error from server: {}", err))
+    }
+}
+
+#[async_trait::async_trait]
+impl HttpClient for reqwest::Client {
+    async fn post(&self, url: &str, body: Value) -> anyhow::Result<Response> {
+        let url = Url::parse(url)?;
+        let response = self.post(url).json(&body).send().await?;
+
+        response
+            .error_for_status()
+            .map_err(|err| anyhow::anyhow!("Error from server: {}", err))
     }
 }
