451afa20951c7b79f65d1f1daf98a07f1412daaf
Meng Zhang <meng@tabbyml.com>
2024-11-26 14:52:36 +0800

    feat(db): add notification inbox tables
    - Added `notifications` table to store notification messages.
    - Added `readed_notifications` table to track which notifications have been read by users.

---
+++ ee/tabby-db/migrations/0039_add-notification-inbox.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE notifications (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+
+  created_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+  updated_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+
+  -- enum of ADMIN, ALL_USERS
+  kind: TEXT NOT NULL,
+
+  message: TEXT NOT NULL,
+)
+
+CREATE TABLE readed_notifications (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER NOT NULL,
+  notification_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (notification_id) REFERENCES notifications(id) ON DELETE CASCADE,
+)
