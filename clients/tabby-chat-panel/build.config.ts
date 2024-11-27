d73a5286c3fa61bd34d27b076653c7c1cc249fb7
Jackson Chen <541898146chen@gmail.com>
2024-11-27 04:14:52 -0500

    chore: remove custom thread

--- clients/tabby-chat-panel/build.config.ts
+++ clients/tabby-chat-panel/build.config.ts
@@ -4,8 +4,6 @@ export default defineBuildConfig({
   entries: [
     'src/index',
     'src/react',
-    'src/createThread',
-    'src/createThreadInsideIframe',
   ],
   declaration: true,
   clean: true,
