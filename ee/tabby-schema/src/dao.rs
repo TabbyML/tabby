c749141da9552563c87dbc90744f00fe4e143b47
Meng Zhang <meng@tabbyml.com>
2024-12-05 18:50:55 +0800

    update

--- ee/tabby-schema/src/dao.rs
+++ ee/tabby-schema/src/dao.rs
@@ -11,7 +11,7 @@ use tabby_db::{
 use crate::{
     integration::{Integration, IntegrationKind, IntegrationStatus},
     interface::UserValue,
-    notification::NotificationKind,
+    notification::NotificationRecipient,
     repository::RepositoryKind,
     schema::{
         auth::{self, OAuthCredential, OAuthProvider},
@@ -469,18 +469,18 @@ impl DbEnum for thread::Role {
     }
 }
 
-impl DbEnum for NotificationKind {
+impl DbEnum for NotificationRecipient {
     fn as_enum_str(&self) -> &'static str {
         match self {
-            NotificationKind::Admin => "admin",
-            NotificationKind::AllUser => "all_user",
+            NotificationRecipient::Admin => "admin",
+            NotificationRecipient::AllUser => "all_user",
         }
     }
 
     fn from_enum_str(s: &str) -> anyhow::Result<Self> {
         match s {
-            "admin" => Ok(NotificationKind::Admin),
-            "all_user" => Ok(NotificationKind::AllUser),
+            "admin" => Ok(NotificationRecipient::Admin),
+            "all_user" => Ok(NotificationRecipient::AllUser),
             _ => bail!("{s} is not a valid value for NotificationKind"),
         }
     }
