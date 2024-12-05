1e10d690772d70e400f8db468f2380705445d87e
Meng Zhang <meng@tabbyml.com>
2024-12-05 18:27:29 +0800

    update

--- ee/tabby-db/schema/schema.sql
+++ ee/tabby-db/schema/schema.sql
@@ -227,7 +227,7 @@ CREATE TABLE notifications(
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   created_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
   updated_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
-  -- enum of ADMIN, ALL_USERS
+  -- enum of admin, all_user
   kind VARCHAR(255) NOT NULL DEFAULT 'admin',
   -- content of notification, in markdown format.
   content TEXT NOT NULL
@@ -238,6 +238,7 @@ CREATE TABLE readed_notifications(
   notification_id INTEGER NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
   updated_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+  CONSTRAINT idx_unique_user_id_notification_id UNIQUE(user_id, notification_id),
   FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
   FOREIGN KEY(notification_id) REFERENCES notifications(id) ON DELETE CASCADE
 );
