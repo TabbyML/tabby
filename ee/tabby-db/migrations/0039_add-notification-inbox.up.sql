754dcec9a9ed048b2a7948d1c1bf783ecd53239a
Meng Zhang <meng@tabbyml.com>
2024-12-05 17:54:18 +0800

    update

--- ee/tabby-db/migrations/0039_add-notification-inbox.up.sql
+++ ee/tabby-db/migrations/0039_add-notification-inbox.up.sql
@@ -5,7 +5,7 @@ CREATE TABLE notifications (
   updated_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
 
   -- enum of ADMIN, ALL_USERS
-  kind TEXT NOT NULL,
+  kind VARCHAR(255) NOT NULL DEFAULT 'admin',
 
   -- content of notification, in markdown format.
   content TEXT NOT NULL
