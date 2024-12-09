5529c850840f016ef454ed76bcd1aa2655fb24f5
Wei Zhang <kweizh@tabbyml.com>
2024-12-09 15:46:05 +0800

    chore: fix style
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>

--- ee/tabby-schema/src/schema/thread/types.rs
+++ ee/tabby-schema/src/schema/thread/types.rs
@@ -3,7 +3,7 @@ use juniper::{GraphQLEnum, GraphQLInputObject, GraphQLObject, GraphQLUnion, ID};
 use serde::Serialize;
 use tabby_common::api::{
     code::{CodeSearchDocument, CodeSearchHit, CodeSearchScores},
-    structured_doc::{DocSearchDocument, DocSearchHit},
+    structured_doc::DocSearchDocument,
 };
 use validator::Validate;
 
