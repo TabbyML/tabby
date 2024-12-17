01d509609a19cc5edcd62d35e98d5504171a15fe
Zhiming Ma <codes.icy@gmail.com>
2024-12-17 13:43:41 +0800

    chore(intellij): bump intellij plugin version to 1.9.1-rc.0.

--- clients/intellij/build.gradle.kts
+++ clients/intellij/build.gradle.kts
@@ -44,7 +44,7 @@ tasks {
 
   intellijPlatform {
     pluginConfiguration {
-      version.set("1.9.0")
+      version.set("1.9.1-rc.0")
       changeNotes.set(provider {
         changelog.renderItem(
           changelog.getLatest(),
