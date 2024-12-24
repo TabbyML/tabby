91596d7cdcc9f80128ae11e85708816721265d56
Wei Zhang <kweizh@tabbyml.com>
2024-12-24 12:45:57 +0800

    chore: fix review
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>

--- ee/tabby-db/schema/schema.sql
+++ ee/tabby-db/schema/schema.sql
@@ -247,11 +247,12 @@ CREATE TABLE ldap_credential(
   id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255) NOT NULL,
   host STRING NOT NULL,
-  smtp_port INTEGER NOT NULL DEFAULT 389,
+  port INTEGER NOT NULL DEFAULT 389,
   bind_dn STRING NOT NULL,
   bind_password STRING NOT NULL,
   base_dn STRING NOT NULL,
   user_filter STRING NOT NULL,
+  -- enum of none, starttls, ldaps
   encryption STRING NOT NULL DEFAULT 'none',
   skip_tls_verify BOOLEAN NOT NULL DEFAULT FALSE,
   --- the attribute to be used as the Tabby user email address
