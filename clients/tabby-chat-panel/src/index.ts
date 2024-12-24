287bce053f06ab0d402bc5b013523479f8f0188c
liangfung <jueliang@tabbyml.com>
2024-12-24 15:49:52 +0800

    update cmts

--- clients/tabby-chat-panel/src/index.ts
+++ clients/tabby-chat-panel/src/index.ts
@@ -160,6 +160,7 @@ export interface FileLocation {
   /**
    * The location in the file.
    * It could be a 1-based line number, a line range, a position or a position range.
+   * If the range is not provided, the whole file is considered.
    */
   location?: Location
 }
