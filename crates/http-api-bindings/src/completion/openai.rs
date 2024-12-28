f9d48a946c5587c6fa721ca5631990f97c90a3f1
corbin <corbin@cc.com>
2024-12-28 17:16:13 +1030

    Add support for latest openai chat models e.g. o1,o1-mini,gpt-4o,gpt-4o-mini while doing completion

--- crates/http-api-bindings/src/completion/openai.rs
+++ crates/http-api-bindings/src/completion/openai.rs
@@ -4,6 +4,7 @@ use futures::{stream::BoxStream, StreamExt};
 use reqwest_eventsource::{Event, EventSource};
 use serde::{Deserialize, Serialize};
 use tabby_inference::{CompletionOptions, CompletionStream};
+use tracing::debug;
 use tracing::warn;
 
 use super::split_fim_prompt;
@@ -40,15 +41,29 @@ impl OpenAICompletionEngine {
     }
 }
 
-#[derive(Serialize)]
+#[derive(Debug, Default, Serialize)]
 struct CompletionRequest {
     model: String,
-    prompt: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    prompt: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
     suffix: Option<String>,
+
     max_tokens: i32,
     temperature: f32,
     stream: bool,
     presence_penalty: f32,
+
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    messages: Vec<Message>,
+}
+
+#[derive(Serialize, Debug)]
+struct Message {
+    role: String,
+    content: String,
 }
 
 #[derive(Deserialize)]
@@ -56,31 +71,67 @@ struct CompletionResponseChunk {
     choices: Vec<CompletionResponseChoice>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Default)]
 struct CompletionResponseChoice {
-    text: String,
+    text: Option<String>,
+    delta: Option<CompletionResponseDelta>,
     finish_reason: Option<String>,
+}
+
+#[derive(Clone, Deserialize, Default, Debug)]
+struct CompletionResponseDelta {
+    content: Option<String>,
 }
 
 #[async_trait]
 impl CompletionStream for OpenAICompletionEngine {
     async fn generate(&self, prompt: &str, options: CompletionOptions) -> BoxStream<String> {
-        let (prompt, suffix) = if self.support_fim {
-            split_fim_prompt(prompt)
-        } else {
-            (prompt, None)
-        };
+        const LEGACY_MODEL_NAME: &str = "gpt-3.5-turbo-instruct";
 
-        let request = CompletionRequest {
+        let mut request = CompletionRequest {
             model: self.model_name.clone(),
-            prompt: prompt.to_owned(),
-            suffix: suffix.map(|x| x.to_owned()),
             max_tokens: options.max_decoding_tokens,
-            temperature: options.sampling_temperature,
+            temperature: 1_f32,
             stream: true,
             presence_penalty: options.presence_penalty,
+            ..Default::default()
         };
 
+        if self.model_name == LEGACY_MODEL_NAME {
+            let (prompt, suffix) = if self.support_fim {
+                split_fim_prompt(prompt)
+            } else {
+                (prompt, None)
+            };
+            request.prompt = Some(prompt.into());
+            request.suffix = suffix.map(Into::into);
+        } else {
+            //FIXME: We may need to refine this prompt to achieve better completion results
+            const SYS_PMT: &str = r#"You are a code completion assistant that completes partial code snippets. Only provide the completion portion, not the full line.
+                            Examples:
+                            System.out.p ->rintln("Hello");
+                            Input: "String name = ->"John Doe";
+                            Input: "if(num > 0->) {
+                            Input: ArrayList<String> list = new -> ArrayList<>();"
+                            Rules:
+
+                            Return only the completion portion
+                            Include necessary closing syntax (parentheses, quotes, semicolons)
+                            Provide the most common completion for the context
+                            No explanations or alternatives
+
+                            Following is the code snippet you shouled work on:
+                            "#;
+
+            request.messages = vec![Message {
+                role: "user".to_string(),
+                content: format!("{SYS_PMT}\n{prompt}"),
+            }];
+
+            debug!("messages:{:?}", request.messages);
+        }
+
+        debug!("messages: {:?}", request.messages);
         let mut request = self.client.post(&self.api_endpoint).json(&request);
         if let Some(api_key) = &self.api_key {
             request = request.bearer_auth(api_key);
@@ -92,12 +143,22 @@ impl CompletionStream for OpenAICompletionEngine {
                 match event {
                     Ok(Event::Open) => {}
                     Ok(Event::Message(message)) => {
+
+                        //message.data example: {"id":"chatcmpl-AigtaSN","object":"chat.completion.chunk","created":1735214686,"model":"o1-mini-2024-09-12","system_fingerprint":"fp_e4","choices":[{"index":0,"delta":{"content":"hello"},"finish_reason":null}]}
                         let x: CompletionResponseChunk = serde_json::from_str(&message.data).expect("Failed to parse response");
                         if let Some(choice) = x.choices.first() {
-                            yield choice.text.clone();
-
                             if choice.finish_reason.is_some() {
                                 break;
+                            }
+
+                            if self.model_name ==  LEGACY_MODEL_NAME{
+                                if let Some(text) = choice.text.clone(){
+                                yield text;
+                                }
+                            }
+                            else if let Some(delta) =  choice.delta.clone(){
+                                debug!("delta:{:?}",delta.content);
+                                yield delta.content.unwrap_or_default();
                             }
                         }
                     }
