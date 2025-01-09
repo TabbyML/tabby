f514a7962f035afd168455482af6df60d4ea2cd5
Meng Zhang <meng@tabbyml.com>
2025-01-09 10:46:29 -0800

    chore: update ui

--- ee/tabby-webserver/ui/_next/static/chunks/app/(dashboard)/settings/(integrations)/providers/[kind]/layout-7fd8f4cfd414892f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2219,7915],{62265:function(e,n,s){Promise.resolve().then(s.bind(s,81565)),Promise.resolve().then(s.t.bind(s,54007,23))}},function(e){e.O(0,[8415,7430,7812,4007,7288,1565,3375,5289,1744],function(){return e(e.s=62265)}),_N_E=e.O()}]);
