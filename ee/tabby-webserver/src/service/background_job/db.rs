fc64e65c39838a26b607e7e70ba7d6c2a68c9dbf
Wei Zhang <kweizh@tabbyml.com>
2025-01-09 11:39:34 +0800

    feat(job): db job should retain job_runs and user_events for only last three months (#3640)
    * feat(job): add daily job to retain job_runs and user_events for only last three months
    
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>
    
    * [autofix.ci] apply automated fixes
    
    * chore: fix review
    
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>
    
    * chore: fix tests
    
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>
    
    * chore: return error directly if meet one for now
    
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>
    
    ---------
    
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- ee/tabby-webserver/src/service/background_job/db.rs
+++ ee/tabby-webserver/src/service/background_job/db.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use anyhow::Context;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use tabby_db::DbConn;
@@ -16,7 +17,7 @@ impl Job for DbMaintainanceJob {
 
 impl DbMaintainanceJob {
     pub async fn cron(
-        _now: DateTime<Utc>,
+        now: DateTime<Utc>,
         context: Arc<dyn ContextService>,
         db: DbConn,
     ) -> tabby_schema::Result<()> {
@@ -35,6 +36,159 @@ impl DbMaintainanceJob {
 
         db.delete_unused_source_id_read_access_policy(&active_source_ids)
             .await?;
+
+        Self::data_retention(now, &db).await?;
         Ok(())
+    }
+
+    async fn data_retention(now: DateTime<Utc>, db: &DbConn) -> tabby_schema::Result<()> {
+        db.delete_job_run_before_three_months(now)
+            .await
+            .context("Failed to clean up and retain only the last 3 months of jobs")?;
+
+        db.delete_user_events_before_three_months(now)
+            .await
+            .context("Failed to clean up and retain only the last 3 months of user events")?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::{DateTime, Utc};
+    use tabby_db::DbConn;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_retention_should_delete() {
+        let db = DbConn::new_in_memory().await.unwrap();
+        let cases = vec![
+            (
+                "2024-04-30T12:12:12Z".parse::<DateTime<Utc>>().unwrap(),
+                "2024-01-30T12:12:11Z".parse::<DateTime<Utc>>().unwrap(),
+            ),
+            (
+                "2024-04-30T12:12:12Z".parse::<DateTime<Utc>>().unwrap(),
+                "2024-01-29T12:12:12Z".parse::<DateTime<Utc>>().unwrap(),
+            ),
+            (
+                "2024-05-01T12:12:12Z".parse::<DateTime<Utc>>().unwrap(),
+                "2024-01-31T12:12:11Z".parse::<DateTime<Utc>>().unwrap(),
+            ),
+        ];
+
+        let user_id = db
+            .create_user("user@test.com".to_string(), None, true, None)
+            .await
+            .unwrap();
+        for (now, created) in cases {
+            db.create_user_event(
+                user_id,
+                "test".to_string(),
+                created.timestamp_millis() as u128,
+                "".to_string(),
+            )
+            .await
+            .unwrap();
+
+            let events = db
+                .list_user_events(
+                    None,
+                    None,
+                    false,
+                    vec![user_id],
+                    created.checked_sub_days(chrono::Days::new(1)).unwrap(),
+                    now,
+                )
+                .await
+                .unwrap();
+            assert_eq!(events.len(), 1);
+
+            DbMaintainanceJob::data_retention(now, &db).await.unwrap();
+
+            let events = db
+                .list_user_events(
+                    None,
+                    None,
+                    false,
+                    vec![user_id],
+                    created.checked_sub_days(chrono::Days::new(1)).unwrap(),
+                    now,
+                )
+                .await
+                .unwrap();
+            assert_eq!(events.len(), 0);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_retention_should_not_delete() {
+        let db = DbConn::new_in_memory().await.unwrap();
+        let cases = vec![
+            (
+                "2024-04-30T12:12:12Z".parse::<DateTime<Utc>>().unwrap(),
+                "2024-01-31T12:12:12Z".parse::<DateTime<Utc>>().unwrap(),
+            ),
+            (
+                "2024-04-30T12:12:12Z".parse::<DateTime<Utc>>().unwrap(),
+                "2024-01-30T12:12:12Z".parse::<DateTime<Utc>>().unwrap(),
+            ),
+            (
+                "2024-04-30T12:12:12Z".parse::<DateTime<Utc>>().unwrap(),
+                "2024-04-30T12:12:11Z".parse::<DateTime<Utc>>().unwrap(),
+            ),
+        ];
+
+        let user_id = db
+            .create_user("user@test.com".to_string(), None, true, None)
+            .await
+            .unwrap();
+        for (now, created) in cases {
+            db.create_user_event(
+                user_id,
+                "test".to_string(),
+                created.timestamp_millis() as u128,
+                "".to_string(),
+            )
+            .await
+            .unwrap();
+
+            let events = db
+                .list_user_events(
+                    None,
+                    None,
+                    false,
+                    vec![user_id],
+                    created.checked_sub_days(chrono::Days::new(1)).unwrap(),
+                    now,
+                )
+                .await
+                .unwrap();
+            assert_eq!(events.len(), 1);
+
+            DbMaintainanceJob::data_retention(now, &db).await.unwrap();
+
+            let events = db
+                .list_user_events(
+                    None,
+                    None,
+                    false,
+                    vec![user_id],
+                    created.checked_sub_days(chrono::Days::new(1)).unwrap(),
+                    now,
+                )
+                .await
+                .unwrap();
+            assert_eq!(events.len(), 1);
+
+            // clean up for next iteration
+            db.delete_user_events_before_three_months(
+                now.checked_add_months(chrono::Months::new(3)).unwrap(),
+            )
+            .await
+            .unwrap();
+        }
     }
 }
