854caade5e5cee700c69e0884b43c16368856562
Wei Zhang <kweizh@tabbyml.com>
2025-01-13 18:37:43 +0800

    feat(page): update db review
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>

--- ee/tabby-db/migrations/0043_page-section.up.sql
+++ ee/tabby-db/migrations/0043_page-section.up.sql
@@ -1,19 +1,13 @@
 CREATE TABLE pages(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 
-    -- Whether the page is draft
-    is_draft BOOLEAN NOT NULL,
-
     -- The user who created the page
-    user_id INTEGER NOT NULL,
-
-    -- Array of relevant questions, in format of `String`
-    relevant_questions BLOB,
+    author_id INTEGER NOT NULL,
 
     created_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
     updated_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
 
-    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY(author_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE page_sections(
@@ -25,18 +19,9 @@ CREATE TABLE page_sections(
     title TEXT NOT NULL,
     content TEXT NOT NULL,
 
-    -- TODO: link or updated image
-    media BLOB,
-
-    -- TODO: user input source
-    client_sources BLOB,
-
-    -- TODO: json format
-    sources BLOB,
-
     created_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
     updated_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
 
-    FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE,
+    FOREIGN KEY(page_id) REFERENCES pages(id) ON DELETE CASCADE,
     CONSTRAINT `page_id_order_index` UNIQUE(page_id, order_index)
 );
