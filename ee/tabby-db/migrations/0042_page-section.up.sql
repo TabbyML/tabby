40cfbc222a02eb7bf1fb153aa78204fbc83954b3
Wei Zhang <kweizh@tabbyml.com>
2025-01-13 20:55:22 +0800

    feat(graphQL): first versio of page API
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>

--- ee/tabby-db/migrations/0042_page-section.up.sql
+++ ee/tabby-db/migrations/0042_page-section.up.sql
@@ -4,6 +4,9 @@ CREATE TABLE pages(
     -- The user who created the page
     author_id INTEGER NOT NULL,
 
+    title TEXT NOT NULL,
+    summary TEXT NOT NULL,
+
     created_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
     updated_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
 
@@ -14,7 +17,7 @@ CREATE TABLE page_sections(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     page_id INTEGER NOT NULL,
 
-    order_index INTEGER NOT NULL,
+    position INTEGER NOT NULL,
 
     title TEXT NOT NULL,
     content TEXT NOT NULL,
@@ -23,5 +26,5 @@ CREATE TABLE page_sections(
     updated_at TIMESTAMP NOT NULL DEFAULT (DATETIME('now')),
 
     FOREIGN KEY(page_id) REFERENCES pages(id) ON DELETE CASCADE,
-    CONSTRAINT `page_id_order_index` UNIQUE(page_id, order_index)
+    CONSTRAINT `page_id_position` UNIQUE(page_id, position)
 );
---
+++ ee/tabby-schema/src/schema/page.rs
@@ -0,0 +1,25 @@
+mod types;
+pub use types::*;
+
+use async_trait::async_trait;
+use juniper::ID;
+
+use crate::schema::Result;
+
+#[async_trait]
+pub trait PageService: Send + Sync {
+    async fn convert_thread_to_page(&self, user_id: &ID, thread_id: ID) -> Result<ID>;
+
+    async fn delete(&self, id: &ID) -> Result<()>;
+    async fn generate_page_title(&self, id: &ID) -> Result<String>;
+    async fn generate_page_summary(&self, id: &ID) -> Result<String>;
+
+    async fn list(
+        &self,
+        ids: Option<&[ID]>,
+        after: Option<String>,
+        before: Option<String>,
+        first: Option<usize>,
+        last: Option<usize>,
+    ) -> Result<Vec<Page>>;
+}
