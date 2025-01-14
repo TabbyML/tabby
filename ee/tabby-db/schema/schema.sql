fd84d681cd142a4f3480b6fd937da18519a1a80f
Wei Zhang <kweizh@tabbyml.com>
2025-01-14 17:46:48 +0800

    chore: page title and summary could be empty
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>

--- ee/tabby-db/schema/schema.sql
+++ ee/tabby-db/schema/schema.sql
@@ -261,3 +261,24 @@ CREATE TABLE ldap_credential(
   created_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
   updated_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now'))
 );
+CREATE TABLE pages(
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  -- The user who created the page
+  author_id INTEGER NOT NULL,
+  title TEXT,
+  summary TEXT,
+  created_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+  updated_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+  FOREIGN KEY(author_id) REFERENCES users(id) ON DELETE CASCADE
+);
+CREATE TABLE page_sections(
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  page_id INTEGER NOT NULL,
+  position INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  content TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+  updated_at TIMESTAMP NOT NULL DEFAULT(DATETIME('now')),
+  FOREIGN KEY(page_id) REFERENCES pages(id) ON DELETE CASCADE,
+  CONSTRAINT `page_id_position` UNIQUE(page_id, position)
+);
