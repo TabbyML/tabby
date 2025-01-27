889750601da93e229ccdd23ddf6fc5f754cd677d
Zhiming Ma <codes.icy@gmail.com>
2025-01-27 16:38:32 +0800

    fix(vscode): exclude ignored files for chat panel at file list. (#3765)
    * fix(vscode): exclude ignored files for chat panel @file list.
    
    * fix: handle emplty gitignore line.
    
    * fix: typo in comments.

--- clients/vscode/src/chat/webview.ts
+++ clients/vscode/src/chat/webview.ts
@@ -51,6 +51,7 @@ import {
   localUriToListFileItem,
   escapeGlobPattern,
 } from "./utils";
+import { findFiles } from "../findFiles";
 import mainHtml from "./html/main.html";
 import errorHtml from "./html/error.html";
 
@@ -529,10 +530,8 @@ export class ChatWebview {
             .join("");
 
           const globPattern = `**/${caseInsensitivePattern}*`;
-
           this.logger.info(`Searching files with pattern: ${globPattern}, limit: ${maxResults}`);
-
-          const files = await workspace.findFiles(globPattern, undefined, maxResults);
+          const files = await findFiles(globPattern, { maxResults });
           this.logger.info(`Found ${files.length} files.`);
           return files.map((uri) => localUriToListFileItem(uri, this.gitProvider));
         } catch (error) {
