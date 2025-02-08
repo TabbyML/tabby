f73710412f116aa0d38afa3e23df70d6a9d69b7e
Wei Zhang <kweizh@tabbyml.com>
2025-02-08 21:50:08 +0800

    chore: update-ui for page (#3811)

--- ee/tabby-webserver/ui/_next/static/chunks/app/(dashboard)/settings/(integrations)/sso/layout-6947deb7547f4c4c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5174],{33888:function(n,e,u){Promise.resolve().then(u.bind(u,81565))}},function(n){n.O(0,[8415,7430,7812,7288,1565,3375,5289,1744],function(){return n(n.s=33888)}),_N_E=n.O()}]);
