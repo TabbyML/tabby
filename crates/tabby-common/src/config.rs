e9a0b144cc3802b920ed03b936fb403ab8c9135b
Wei Zhang <kweizh@tabbyml.com>
2025-02-10 19:30:22 +0800

    feat(embedding): add max input length in EmbeddingConfig and clip prompt before sent to server
    Signed-off-by: Wei Zhang <kweizh@tabbyml.com>

--- crates/tabby-common/src/config.rs
+++ crates/tabby-common/src/config.rs
@@ -29,6 +29,9 @@ pub struct Config {
     pub completion: CompletionConfig,
 
     #[serde(default)]
+    pub embedding: EmbeddingConfig,
+
+    #[serde(default)]
     pub answer: AnswerConfig,
 
     #[serde(default)]
@@ -66,7 +69,6 @@ impl Config {
         }
 
         if let Err(e) = cfg.validate_config() {
-            cfg = Default::default();
             InfoMessage::new(
                 "Parsing config failed",
                 HeaderFormat::BoldRed,
@@ -390,6 +392,19 @@ impl Default for CompletionConfig {
             code_search_params: CodeSearchParams::default(),
         }
     }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Default)]
+pub struct EmbeddingConfig {
+    #[serde(default = "default_embedding_max_input_length")]
+    pub max_input_length: usize,
+}
+
+// In the majority of instances, the ratio of tokens to words is 100:75,
+// The default_embedding_max_input_length configured to be 2048 characters,
+// as specified by the default `CompletionConfig.max_input_length` setting.
+fn default_embedding_max_input_length() -> usize {
+    2048
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
---
+++ ee/tabby-webserver/src/service/embedding.rs
@@ -0,0 +1,33 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use async_trait::async_trait;
+use tabby_common::config::EmbeddingConfig;
+use tabby_inference::Embedding;
+
+pub struct EmbeddingServiceImpl {
+    config: EmbeddingConfig,
+    embedding: Arc<dyn Embedding>,
+}
+
+pub fn create(config: &EmbeddingConfig, embedding: Arc<dyn Embedding>) -> Arc<dyn Embedding> {
+    Arc::new(EmbeddingServiceImpl {
+        config: config.clone(),
+        embedding,
+    })
+}
+
+#[async_trait]
+impl Embedding for EmbeddingServiceImpl {
+    async fn embed(&self, prompt: &str) -> Result<Vec<f32>> {
+        if self.config.max_input_length != 0
+            && prompt.chars().count() > self.config.max_input_length
+        {
+            self.embedding
+                .embed(&prompt[..self.config.max_input_length])
+                .await
+        } else {
+            self.embedding.embed(prompt).await
+        }
+    }
+}
