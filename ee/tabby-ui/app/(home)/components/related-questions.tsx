8a79aa4df57bda0540aee7a136bb3bcff597f716
aliang <jueliang@tabbyml.com>
2025-02-13 19:47:23 +0800

    chore(ui): implement carousel for related questions on homepage (#3836)
    * chore(ui): implement carousel for related questions on homepage
    
    * Update ee/tabby-ui/app/(home)/components/related-questions.tsx
    
    ---------
    
    Co-authored-by: Meng Zhang <meng@tabbyml.com>

--- ee/tabby-ui/app/(home)/components/related-questions.tsx
+++ ee/tabby-ui/app/(home)/components/related-questions.tsx
@@ -1,7 +1,15 @@
+import Autoplay from 'embla-carousel-autoplay'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useQuery } from 'urql'
 
 import { graphql } from '@/lib/gql/generates'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel'
 
 const readRepositoryRelatedQuestionsQuery = graphql(/* GraphQL */ `
   query readRepositoryRelatedQuestions($sourceId: String!) {
@@ -58,19 +66,38 @@ export function RelatedQuestions({
             delay: 0.1
           }}
         >
-          <div className="flex flex-wrap justify-center gap-2 pb-3 pt-5 align-middle text-xs">
-            {repositoryRelatedQuestions.map((x, idx) => {
-              return (
-                <div
-                  key={`${x}_${idx}`}
-                  className="cursor-pointer truncate rounded-lg bg-muted px-4 py-2 transition-opacity hover:bg-muted/70"
-                  onClick={e => onClickRelatedQuestion(x)}
-                >
-                  <span>{x}</span>
-                </div>
-              )
-            })}
-          </div>
+          <Carousel
+            orientation="vertical"
+            opts={{
+              align: 'start',
+              loop: true
+            }}
+            plugins={[
+              Autoplay({
+                delay: 5000,
+                stopOnInteraction: true,
+                stopOnMouseEnter: true
+              })
+            ]}
+            className="group mt-3 w-full"
+          >
+            <CarouselContent className="h-14 pt-1.5">
+              {repositoryRelatedQuestions.map((x, index) => (
+                <CarouselItem key={`${x}_${index}`}>
+                  <div className="mx-auto flex max-w-[80%] justify-center overflow-hidden p-1 text-sm">
+                    <span
+                      className="cursor-pointer truncate rounded-lg bg-muted px-4 py-1 transition-opacity hover:bg-muted/70"
+                      onClick={e => onClickRelatedQuestion(x)}
+                    >
+                      {x}
+                    </span>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious className="absolute left-auto right-10 top-1 z-10 h-6 w-6 opacity-0 group-hover:opacity-100" />
+            <CarouselNext className="absolute left-auto right-2 top-1 z-10 h-6 w-6 opacity-0 group-hover:opacity-100" />
+          </Carousel>
         </motion.div>
       ) : null}
     </AnimatePresence>
