82d2a846c1eb9b77dbf72e6d60b0107a5d047d59
Meng Zhang <meng@tabbyml.com>
2025-02-23 22:44:21 +0800

    chore(third_party): improve error handling and logging in repository fetching

--- ee/tabby-webserver/src/service/repository/third_party.rs
+++ ee/tabby-webserver/src/service/repository/third_party.rs
@@ -8,6 +8,7 @@ use strum::IntoEnumIterator;
 use tabby_common::config::{CodeRepository, RepositoryConfig};
 use tabby_db::{DbConn, ProvidedRepositoryDAO};
 use tabby_schema::{
+    bail,
     integration::{Integration, IntegrationKind, IntegrationService},
     job::{JobInfo, JobService},
     repository::{
@@ -167,15 +168,15 @@ impl ThirdPartyRepositoryService for ThirdPartyRepositoryServiceImpl {
         .await
         {
             Ok(repos) => repos,
-            Err(e) => {
+            Err(err) => {
                 self.integration
-                    .update_integration_sync_status(&provider.id, Some(e.to_string()))
+                    .update_integration_sync_status(&provider.id, Some(err.to_string()))
                     .await?;
-                error!(
-                    "Failed to fetch repositories from integration: {}",
-                    provider.display_name
+                bail!(
+                    "Failed to fetch repositories from integration: {}, error: {}",
+                    provider.display_name,
+                    err
                 );
-                return Err(e.into());
             }
         };
 
