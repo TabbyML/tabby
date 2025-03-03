36910ce5e01896b6b5f71e5aac588060ecc79009
Meng Zhang <meng@tabbyml.com>
2025-03-03 21:48:21 +0800

    update

--- ee/tabby-webserver/src/service/page/prompt_tools.rs
+++ ee/tabby-webserver/src/service/page/prompt_tools.rs
@@ -25,38 +25,42 @@ Do not include any additional text.
 }
 
 fn generate_page_prompt(title: &str, sections: &[Section]) -> String {
-    let sections = sections
-        .iter()
-        .map(|x| format!("## {}\n\n{}", x.title, x.content))
-        .collect::<Vec<_>>()
-        .join("\n");
-    format!(
-        r#"Here's some existing writings about the page.
+    if sections.is_empty() {
+        format!("You're writing a page named \"{}\".\n", title)
+    } else {
+        let sections = sections
+            .iter()
+            .map(|x| format!("## {}\n\n{}", x.title, x.content))
+            .collect::<Vec<_>>()
+            .join("\n");
+        format!(
+            r#"Here's some existing writings about the page.
+=== Page Content Start ===
 # {title}
 
 {sections}
+=== Page Content End ===
 "#,
-    )
+        )
+    }
 }
 
 pub fn prompt_page_section_titles(
     count: usize,
     title: &str,
     sections: &[Section],
-    new_section_prompt: &str,
+    new_section_prompt: Option<&str>,
 ) -> String {
     let page_prompt = generate_page_prompt(title, sections);
 
-    let new_section = if !new_section_prompt.is_empty() {
-        format!("The new section is about: {new_section_prompt}.")
+    let new_section = if let Some(new_section_prompt) = new_section_prompt {
+        format!("The new section is about: {new_section_prompt}.\n")
     } else {
         "".to_string()
     };
 
     format!(
-        r#"{page_prompt}{new_section}
-
-Please generate {count} section titles for the page based on above information.
+        r#"{page_prompt}{new_section}Please generate {count} section titles for the page based on above information.
 Please only generate the section title and nothing else. Do not include any additional text.
 Each section title should be on a new line.
 "#
