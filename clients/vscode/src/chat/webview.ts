b1e4752ab5d4d67f502efe069334027b36c14215
Sma1lboy <541898146chen@gmail.com>
2025-03-17 21:32:34 -0500

    feat(chat): improve file retrieval when fewer than 5 open tabs are present

--- clients/vscode/src/chat/webview.ts
+++ clients/vscode/src/chat/webview.ts
@@ -532,16 +532,22 @@ export class ChatWebview extends EventEmitter {
             .flatMap((group) => group.tabs)
             .filter((tab) => tab.input && (tab.input as TabInputText).uri);
 
-          if (openTabs.length > 0) {
-            this.logger.info(`No query provided, listing ${openTabs.length} opened editors.`);
-            return openTabs.map((tab) => localUriToListFileItem((tab.input as TabInputText).uri, this.gitProvider));
+          const openTabItems = openTabs.map((tab) =>
+            localUriToListFileItem((tab.input as TabInputText).uri, this.gitProvider),
+          );
+
+          // If we have less than 5 open tabs, fetch additional files to make total = 5
+          if (openTabs.length < 5) {
+            const additionalCount = 5 - openTabs.length;
+            this.logger.info(`Found ${openTabs.length} open tabs, fetching ${additionalCount} more files`);
+            const files = await this.findFiles("**/*", { maxResults: additionalCount });
+            this.logger.info(`Found ${files.length} additional files`);
+            const fileItems = files.map((uri) => localUriToListFileItem(uri, this.gitProvider));
+            return [...openTabItems, ...fileItems];
           }
 
-          // If no open tabs, fall back to findFiles with maxResults
-          this.logger.info(`No open editors found, searching workspace with limit: ${maxResults}`);
-          const files = await this.findFiles("**/*", { maxResults });
-          this.logger.info(`Found ${files.length} files.`);
-          return files.map((uri) => localUriToListFileItem(uri, this.gitProvider));
+          this.logger.info(`No query provided, listing ${openTabs.length} opened editors.`);
+          return openTabItems;
         }
 
         try {
