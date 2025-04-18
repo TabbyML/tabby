049a0381db6c736b7666b48f9d4b2a84f1d7b299
Wei Zhang <kweizh@tabbyml.com>
2025-04-18 11:16:20 +0800

    chore: update ui

---
+++ ee/tabby-webserver/ui/_next/static/eVX1ek4_N_TrzfxtyCOVI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e9871f0a501df54a.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
