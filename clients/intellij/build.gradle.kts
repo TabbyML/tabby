ca7d2b1891349d01a80233a038ae24da23b7221a
Zhiming Ma <codes.icy@gmail.com>
2025-05-14 20:04:32 +0800

    chore(intellij): bump intellij plugin version to 1.14.0-rc.2.

--- clients/intellij/build.gradle.kts
+++ clients/intellij/build.gradle.kts
@@ -44,7 +44,7 @@ tasks {
 
   intellijPlatform {
     pluginConfiguration {
-      version.set("1.14.0-rc.1")
+      version.set("1.14.0-rc.2")
       changeNotes.set(provider {
         changelog.renderItem(
           changelog.getLatest(),
