ef16e424a1113611fc650bc4ec6eec96bc4b806a
Wei Zhang <kweizh@tabbyml.com>
2025-05-16 15:40:40 +0800

    chore: optimise sync repositories error message

--- ee/tabby-webserver/src/service/background_job/third_party_integration/error.rs
+++ ee/tabby-webserver/src/service/background_job/third_party_integration/error.rs
@@ -3,7 +3,7 @@ use octocrab;
 pub fn octocrab_error_message(err: octocrab::Error) -> String {
     match err {
         octocrab::Error::GitHub { source, .. } => {
-            format!("GitHub error: {} {}", source.status_code, source.message)
+            format!("GitHub error: {}, {}", source.status_code, source.message)
         }
 
         // no need to print the Json error backtrace
