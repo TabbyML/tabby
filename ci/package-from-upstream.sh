7d468ce6e28e31d514484b00a8e8496b1ad6f22f
Wei Zhang <kweizh@tabbyml.com>
2025-08-01 21:17:47 +0800

    chore(ci): fix download llama from upstream and drop cuda11.7 release (#4331)

--- ci/package-from-upstream.sh
+++ ci/package-from-upstream.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # get current bash file directory
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 LLAMA_CPP_PATH="${PROJECT_ROOT}/crates/llama-cpp-server/llama.cpp"
@@ -7,11 +9,12 @@ LLAMA_CPP_PATH="${PROJECT_ROOT}/crates/llama-cpp-server/llama.cpp"
 # Input variables
 LLAMA_CPP_VERSION=${LLAMA_CPP_VERSION:-$(cd ${LLAMA_CPP_PATH} && git fetch --tags origin >/dev/null && git describe --tags --abbrev=0)}
 echo "LLAMA_CPP_VERSION=${LLAMA_CPP_VERSION}"
-LLAMA_CPP_PLATFORM=${LLAMA_CPP_PLATFORM:-win-cuda-cu11.7-x64}
-OUTPUT_NAME=${OUTPUT_NAME:-tabby_x86_64-windows-msvc-cuda117}
+LLAMA_CPP_PLATFORM=${LLAMA_CPP_PLATFORM:-win-cuda-12.4-x64}
 
 NAME=llama-${LLAMA_CPP_VERSION}-bin-${LLAMA_CPP_PLATFORM}
 ZIP_FILE=${NAME}.zip
+
+OUTPUT_NAME=${OUTPUT_NAME:-tabby_x86_64-windows-msvc-cuda124}
 
 if [[ ${LLAMA_CPP_PLATFORM} == win* ]]; then
     TABBY_BINARY=${TABBY_BINARY:-tabby_x86_64-windows-msvc.exe}
@@ -27,7 +30,7 @@ cp "./${TABBY_BINARY}"/${TABBY_BINARY} ${OUTPUT_NAME}/tabby${TABBY_EXTENSION}
 
 pushd ${OUTPUT_NAME}
 if [[ ${LLAMA_CPP_PLATFORM} == win* ]]; then
-    rm $(ls *.exe | grep -v -e "tabby" -e "llama-server")
+    rm -f $(ls *.exe | grep -v -e "tabby" -e "llama-server")
 
     popd
     zip -r ${OUTPUT_NAME}.zip ${OUTPUT_NAME}
@@ -36,7 +39,7 @@ else
     mv build/bin/* .
     rm -r build
 
-    rm $(ls . | grep -v -e "tabby" -e "llama-server" -e '.so$' -e "LICENSE")
+    rm -f $(ls . | grep -v -e "tabby" -e "llama-server" -e '.so$' -e "LICENSE")
     mv LICENSE LICENSE-llama-server
     chmod +x llama-server tabby
 
